8eaab3e05b32a21f1fddf4108956fb860e1c9f10
samdauwe <sam.dauwe@gmail.com>
2021-04-28 21:55:26 +0200

    Moved stanford-dragon mesh creation to meshes file

--- src/examples/meshes.h
+++ src/examples/meshes.h
@@ -14,3 +14,40 @@ typedef struct cube_mesh_t {
 } cube_mesh_t;
 
 void cube_mesh_init(cube_mesh_t* cube_mesh);
+
+/* -------------------------------------------------------------------------- *
+ * Stanford Dragon
+ * -------------------------------------------------------------------------- */
+
+#ifdef __GNUC__
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wparentheses"
+#endif
+#include <rply.h>
+#ifdef __GNUC__
+#pragma GCC diagnostic pop
+#endif
+
+#define POSITION_COUNT_RES_4 5205
+#define CELL_COUNT_RES_4 11102
+#define STANFORD_DRAGON_MESH_SCALE 500
+
+typedef struct stanford_dragon_mesh_t {
+  struct {
+    float data[POSITION_COUNT_RES_4][3];
+    uint64_t count; // number of vertices (should be 5205)
+  } vertices;
+  struct {
+    uint16_t data[CELL_COUNT_RES_4][3];
+    uint64_t count; // number of faces (should be 11102)
+  } triangles;      // triangles
+} stanford_dragon_mesh_t;
+
+/**
+ * @brief Loads the 'stanford-dragon' PLY file (quality level 4).
+ * @see https://github.com/hughsk/stanford-dragon
+ *
+ * Uses: ANSI C Library for PLY file format input and output
+ * @see http://w3.impa.br/~diego/software/rply/
+ */
+int stanford_dragon_mesh_init(stanford_dragon_mesh_t* stanford_dragon_mesh);
--- src/examples/shadow_mapping.c
+++ src/examples/shadow_mapping.c
@@ -1,170 +1,20 @@
 #include "example_base.h"
 #include "examples.h"
+#include "meshes.h"
 
 #include <string.h>
 
 #include "../webgpu/imgui_overlay.h"
 
 /* -------------------------------------------------------------------------- *
- * Stanford Dragon
- * -------------------------------------------------------------------------- */
-
-#ifdef __GNUC__
-#pragma GCC diagnostic push
-#pragma GCC diagnostic ignored "-Wparentheses"
-#endif
-#include <rply.h>
-#ifdef __GNUC__
-#pragma GCC diagnostic pop
-#endif
-
-/**
- * @brief Loads the 'stanford-dragon' PLY file (quality level 4).
- * @see https://github.com/hughsk/stanford-dragon
+ * WebGPU Example - Shadow Mapping
  *
- * Uses: ANSI C Library for PLY file format input and output
- * @see http://w3.impa.br/~diego/software/rply/
- */
-
-#define POSITION_COUNT_RES_4 5205
-#define CELL_COUNT_RES_4 11102
-#define STANFORD_DRAGON_MESH_SCALE 500
-#define STANFORD_DRAGON_MESH_DEBUG_PRINT 0
-
-typedef struct stanford_dragon_mesh_t {
-  struct {
-    float data[POSITION_COUNT_RES_4][3];
-    size_t count; // number of vertices (should be 5205)
-  } vertices;
-  struct {
-    uint16_t data[CELL_COUNT_RES_4][3];
-    size_t count; // number of faces (should be 11102)
-  } triangles;    // triangles
-} stanford_dragon_mesh_t;
-
-#if STANFORD_DRAGON_MESH_DEBUG_PRINT
-static void debug_print(stanford_dragon_mesh_t* stanford_dragon_mesh)
-{
-  ASSERT(stanford_dragon_mesh);
-  if (stanford_dragon_mesh->vertices.count == 0
-      || stanford_dragon_mesh->triangles.count == 0) {
-    return;
-  }
-
-  // Vertices and indices count
-  printf("nvertices=%ld\nntriangles=%ld\n",
-         stanford_dragon_mesh->vertices.count,
-         stanford_dragon_mesh->triangles.count);
-  // Vertices data
-  for (uint32_t i = 0; i < stanford_dragon_mesh->vertices.count; ++i) {
-    printf("%g ", stanford_dragon_mesh->vertices.data[i][0]);
-    printf("%g ", stanford_dragon_mesh->vertices.data[i][1]);
-    printf("%g\n", stanford_dragon_mesh->vertices.data[i][2]);
-  }
-  // indices data
-  for (uint32_t i = 0; i < stanford_dragon_mesh->triangles.count; ++i) {
-    printf("%hu ", stanford_dragon_mesh->triangles.data[i][0]);
-    printf("%hu ", stanford_dragon_mesh->triangles.data[i][1]);
-    printf("%hu\n", stanford_dragon_mesh->triangles.data[i][2]);
-  }
-}
-#endif
-
-static int vertex_cb(p_ply_argument argument)
-{
-  // Vertex index
-  long vertex_index;
-  ply_get_argument_element(argument, NULL, &vertex_index);
-
-  // x, y or z index
-  long index_data;
-  stanford_dragon_mesh_t* pdata;
-  ply_get_argument_user_data(argument, (void**)&pdata, &index_data);
-  ASSERT(pdata)
-
-  // Get value
-  float value = (float)ply_get_argument_value(argument);
-  value *= STANFORD_DRAGON_MESH_SCALE;
-
-  // Store value
-  ASSERT((size_t)vertex_index < pdata->vertices.count);
-  ASSERT(index_data >= 0 && index_data < 3);
-  pdata->vertices.data[vertex_index][index_data] = value;
-
-  return 1;
-}
-
-static int face_cb(p_ply_argument argument)
-{
-  // Face index
-  long face_index;
-  ply_get_argument_element(argument, NULL, &face_index);
-
-  // Data pointer
-  stanford_dragon_mesh_t* pdata;
-  ply_get_argument_user_data(argument, (void**)&pdata, NULL);
-  ASSERT(pdata);
-
-  // Value
-  long length, value_index;
-  ply_get_argument_property(argument, NULL, &length, &value_index);
-  if (value_index >= 0 && value_index < 3) {
-    // Get value
-    uint16_t value = (uint16_t)ply_get_argument_value(argument);
-
-    // Store value
-    ASSERT((size_t)face_index < pdata->triangles.count);
-    pdata->triangles.data[face_index][value_index] = value;
-  }
-
-  return 1;
-}
-
-static int
-stanford_dragon_mesh_init(stanford_dragon_mesh_t* stanford_dragon_mesh)
-{
-  ASSERT(stanford_dragon_mesh)
-
-  p_ply ply = ply_open("meshes/dragon_vrip_res4.ply", NULL, 0, NULL);
-  if (!ply) {
-    return 1;
-  }
-  if (!ply_read_header(ply)) {
-    return 1;
-  }
-
-  stanford_dragon_mesh->vertices.count
-    = ply_set_read_cb(ply, "vertex", "x", vertex_cb, stanford_dragon_mesh, 0);
-  ply_set_read_cb(ply, "vertex", "y", vertex_cb, stanford_dragon_mesh, 1);
-  ply_set_read_cb(ply, "vertex", "z", vertex_cb, stanford_dragon_mesh, 2);
-  stanford_dragon_mesh->triangles.count = ply_set_read_cb(
-    ply, "face", "vertex_indices", face_cb, stanford_dragon_mesh, 0);
-  if (!ply_read(ply)) {
-    return 1;
-  }
-  ply_close(ply);
-
-  ASSERT(stanford_dragon_mesh->vertices.count == POSITION_COUNT_RES_4);
-  ASSERT(stanford_dragon_mesh->triangles.count == CELL_COUNT_RES_4);
-
-#if STANFORD_DRAGON_MESH_DEBUG_PRINT
-  debug_print(stanford_dragon_mesh);
-#endif
-
-  return 0;
-}
-
-/* -------------------------------------------------------------------------- *
- * Shadow mapping example
- * -------------------------------------------------------------------------- */
-
-/**
- * @brief This example shows how to sample from a depth texture to render
- * shadows.
- * @see
+ * This example shows how to sample from a depth texture to render shadows.
+ *
+ * Ref:
  * https://github.com/austinEng/webgpu-samples/blob/main/src/pages/samples/shadowMapping.ts
- * @see stanford-dragon: https://github.com/hughsk/stanford-dragon
- */
+ * stanford-dragon: https://github.com/hughsk/stanford-dragon
+ * -------------------------------------------------------------------------- */
 
 static struct view_matrices_t {
   vec3 up_vector;
