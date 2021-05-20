8e798381995056a77a037268f68c63ab00613e72
samdauwe <sam.dauwe@gmail.com>
2021-05-20 21:37:33 +0200

    Removed comments

--- src/webgpu/api.h
+++ src/webgpu/api.h
@@ -1,10 +1,6 @@
 #ifndef WEBGPU_API_H
 #define WEBGPU_API_H
 
-// https://alain.xyz/blog/raw-webgpu
-// https://hackmd.io/szV68rOVQ56GYzPJMBko8A
-// https://hackmd.io/uH1MI9cnQBG9GGOZG20COw?view
-
 #include <dawn/webgpu.h>
 
 #include "buffer.h"
