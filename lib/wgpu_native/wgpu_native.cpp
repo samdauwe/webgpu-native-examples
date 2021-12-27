3af07dafe1125b1a144de16af59305c6172becb5
samdauwe <sam.dauwe@gmail.com>
2021-12-27 20:37:00 +0100

    Refactored wgpu_native libe for easier API and integration with GLFW

---
+++ lib/wgpu_native/wgpu_native.cpp
@@ -0,0 +1,237 @@
+#include "wgpu_native.h"
+
+#include <dawn/webgpu.h>
+
+#include <dawn/dawn_proc.h>
+#include <dawn/webgpu_cpp.h>
+#include <dawn_native/DawnNative.h>
+
+#include <memory>
+
+//****************************** Implementation *******************************/
+
+namespace WGPUImpl {
+
+#ifdef DEBUG
+#define dlog(format, ...)                                                      \
+  ({                                                                           \
+    fprintf(stderr, format " \e[2m(%s %d)\e[0m\n", ##__VA_ARGS__,              \
+            __FUNCTION__, __LINE__);                                           \
+    fflush(stderr);                                                            \
+  })
+#else
+#define dlog(...)                                                              \
+  do {                                                                         \
+  } while (0)
+#endif
+
+static const char* BackendTypeName(wgpu::BackendType);
+static const char* AdapterTypeName(wgpu::AdapterType);
+
+static struct {
+  struct {
+    DawnProcTable procTable;
+    std::unique_ptr<dawn_native::Instance> instance = nullptr;
+  } dawn_native;
+  struct {
+    dawn_native::Adapter handle;
+    wgpu::BackendType backendType;
+  } adapter;
+  bool initialized = false;
+} gpuContext = {};
+
+static void Initialize()
+{
+  if (gpuContext.initialized) {
+    return;
+  }
+
+  // Set up the native procs for the global proctable
+  gpuContext.dawn_native.procTable = dawn_native::GetProcs();
+  dawnProcSetProcs(&gpuContext.dawn_native.procTable);
+  gpuContext.dawn_native.instance = std::make_unique<dawn_native::Instance>();
+  gpuContext.dawn_native.instance->DiscoverDefaultAdapters();
+  gpuContext.dawn_native.instance->EnableBackendValidation(true);
+  gpuContext.dawn_native.instance->SetBackendValidationLevel(
+    dawn_native::BackendValidationLevel::Full);
+
+  // Dawn backend type.
+  // Default to D3D12, Metal, Vulkan, OpenGL in that order as D3D12 and Metal
+  // are the preferred on their respective platforms, and Vulkan is preferred to
+  // OpenGL
+  gpuContext.adapter.backendType =
+#if defined(DAWN_ENABLE_BACKEND_D3D12)
+    wgpu::BackendType::D3D12;
+#elif defined(DAWN_ENABLE_BACKEND_METAL)
+    wgpu::BackendType::Metal;
+#elif defined(DAWN_ENABLE_BACKEND_VULKAN)
+    wgpu::BackendType::Vulkan;
+#elif defined(DAWN_ENABLE_BACKEND_OPENGL)
+    wgpu::BackendType::OpenGL;
+#else
+#error
+#endif
+  gpuContext.adapter.handle = nullptr;
+
+  gpuContext.initialized = true;
+}
+
+static WGPUAdapter RequestAdapter(WGPURequestAdapterOptions* options)
+{
+  Initialize();
+
+  WGPUPowerPreference powerPreference
+    = options ?
+        (options->powerPreference == WGPUPowerPreference_HighPerformance ?
+           WGPUPowerPreference_HighPerformance :
+           WGPUPowerPreference_LowPower) :
+        WGPUPowerPreference_LowPower;
+
+  // search available adapters for a good match, in the following priority
+  // order
+  std::vector<wgpu::AdapterType> typePriority;
+  if (powerPreference == WGPUPowerPreference_LowPower) {
+    // low power
+    typePriority = std::vector<wgpu::AdapterType>{
+      wgpu::AdapterType::IntegratedGPU,
+      wgpu::AdapterType::DiscreteGPU,
+      wgpu::AdapterType::CPU,
+    };
+  }
+  else if (powerPreference == WGPUPowerPreference_HighPerformance) {
+    // high performance
+    typePriority = std::vector<wgpu::AdapterType>{
+      wgpu::AdapterType::DiscreteGPU,
+      wgpu::AdapterType::IntegratedGPU,
+      wgpu::AdapterType::CPU,
+    };
+  }
+
+  std::vector<dawn_native::Adapter> adapters
+    = gpuContext.dawn_native.instance->GetAdapters();
+  for (auto reqType : typePriority) {
+    for (const dawn_native::Adapter& adapter : adapters) {
+      wgpu::AdapterProperties ap;
+      adapter.GetProperties(&ap);
+      if (ap.adapterType == reqType
+          && (reqType == wgpu::AdapterType::CPU
+              || ap.backendType == gpuContext.adapter.backendType)) {
+        dlog("selected adapter %s (device=0x%x vendor=0x%x type=%s/%s)",
+             ap.name, ap.deviceID, ap.vendorID, AdapterTypeName(ap.adapterType),
+             BackendTypeName(ap.backendType));
+        gpuContext.adapter.handle = adapter;
+        return gpuContext.adapter.handle.Get();
+      }
+    }
+  }
+
+  return nullptr;
+}
+
+static void LogAvailableAdapters()
+{
+  Initialize();
+
+  fprintf(stderr, "Available adapters:\n");
+  for (auto&& a : gpuContext.dawn_native.instance->GetAdapters()) {
+    wgpu::AdapterProperties p;
+    a.GetProperties(&p);
+    fprintf(
+      stderr,
+      "  %s (%s)\n"
+      "    deviceID=%u, vendorID=0x%x, BackendType::%s, AdapterType::%s\n",
+      p.name, p.driverDescription, p.deviceID, p.vendorID,
+      BackendTypeName(p.backendType), AdapterTypeName(p.adapterType));
+  }
+}
+
+static std::unique_ptr<wgpu::ChainedStruct> SurfaceDescriptor(void* display,
+                                                              void* window)
+{
+#if defined(WIN32)
+  std::unique_ptr<wgpu::SurfaceDescriptorFromWindowsHWND> desc
+    = std::make_unique<wgpu::SurfaceDescriptorFromWindowsHWND>();
+  desc->hwnd      = window;
+  desc->hinstance = GetModuleHandle(nullptr);
+  return std::move(desc);
+#elif defined(__linux__) // X11
+  std::unique_ptr<wgpu::SurfaceDescriptorFromXlib> desc
+    = std::make_unique<wgpu::SurfaceDescriptorFromXlib>();
+  desc->display = display;
+  desc->window  = *((uint32_t*)window);
+  return std::move(desc);
+#endif
+
+  return nullptr;
+}
+
+static WGPUSurface CreateSurface(void* display, void* window)
+{
+  std::unique_ptr<wgpu::ChainedStruct> sd = SurfaceDescriptor(display, window);
+  wgpu::SurfaceDescriptor descriptor;
+  descriptor.nextInChain = sd.get();
+  wgpu::Surface surface = wgpu::Instance(gpuContext.dawn_native.instance->Get())
+                            .CreateSurface(&descriptor);
+  if (!surface) {
+    return nullptr;
+  }
+  WGPUSurface surf = surface.Get();
+  wgpuSurfaceReference(surf);
+  return surf;
+}
+
+static const char* BackendTypeName(wgpu::BackendType t)
+{
+  switch (t) {
+    case wgpu::BackendType::Null:
+      return "Null";
+    case wgpu::BackendType::WebGPU:
+      return "WebGPU";
+    case wgpu::BackendType::D3D11:
+      return "D3D11";
+    case wgpu::BackendType::D3D12:
+      return "D3D12";
+    case wgpu::BackendType::Metal:
+      return "Metal";
+    case wgpu::BackendType::Vulkan:
+      return "Vulkan";
+    case wgpu::BackendType::OpenGL:
+      return "OpenGL";
+    case wgpu::BackendType::OpenGLES:
+      return "OpenGLES";
+  }
+  return "?";
+}
+static const char* AdapterTypeName(wgpu::AdapterType t)
+{
+  switch (t) {
+    case wgpu::AdapterType::DiscreteGPU:
+      return "DiscreteGPU";
+    case wgpu::AdapterType::IntegratedGPU:
+      return "IntegratedGPU";
+    case wgpu::AdapterType::CPU:
+      return "CPU";
+    case wgpu::AdapterType::Unknown:
+      return "Unknown";
+  }
+  return "?";
+}
+
+} // namespace WGPUImpl
+
+//******************************** Public API *********************************/
+
+void wgpu_log_available_adapters()
+{
+  WGPUImpl::LogAvailableAdapters();
+}
+
+WGPUAdapter wgpu_request_adapter(WGPURequestAdapterOptions* options)
+{
+  return WGPUImpl::RequestAdapter(options);
+}
+
+WGPUSurface wgpu_create_surface(void* display, void* window_handle)
+{
+  return WGPUImpl::CreateSurface(display, window_handle);
+}
--- lib/wgpu_native/wgpu_native.h
+++ lib/wgpu_native/wgpu_native.h
@@ -8,11 +8,8 @@ extern "C" {
 #endif
 
 void wgpu_log_available_adapters();
-void wgpu_get_adapter_info(char (*adapter_info)[256]);
-void* wgpu_get_backend_instance(WGPUDevice device);
-WGPUDevice wgpu_create_device(WGPUBackendType type);
-WGPUSwapChain wgpu_create_swap_chain(WGPUDevice device, void* surface, int width, int height);
-WGPUTextureFormat wgpu_get_swap_chain_preferred_format(WGPUDevice device);
+WGPUAdapter wgpu_request_adapter(WGPURequestAdapterOptions* options);
+WGPUSurface wgpu_create_surface(void* display, void* window_handle);
 
 #ifdef __cplusplus
 } // extern "C"
