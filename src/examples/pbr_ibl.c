1cfcf113e7997b332e50f8c50ab24aa7a7ab0e08
samdauwe <sam.dauwe@gmail.com>
2022-02-03 21:01:31 +0100

    Continued work on PBR With Image Based Lighting example

--- src/examples/pbr_ibl.c
+++ src/examples/pbr_ibl.c
@@ -21,7 +21,8 @@
  * http://blog.selfshadow.com/publications/s2013-shading-course/karis/s2013_pbs_epic_notes_v2.pdf
  * -------------------------------------------------------------------------- */
 
-#define GRID_DIM 7
+#define SINGLE_ROW_OBJECT_COUNT 10
+#define ALIGNMENT 256 // 256-byte alignment
 
 static bool display_skybox = true;
 
@@ -31,13 +32,53 @@ static struct {
 
 static struct {
   struct gltf_model_t* skybox;
-} models;
+  struct {
+    const char* name;
+    const char* filelocation;
+    struct gltf_model_t* object;
+  } objects[4];
+  int32_t object_index;
+} models = {
+  // clang-format off
+  .objects = {
+    { .name = "Sphere",    .filelocation = "models/sphere.gltf" },
+    { .name = "Teapot",    .filelocation = "models/teapot.gltf" },
+    { .name = "Torusknot", .filelocation = "models/torusknot.gltf" },
+    { .name = "Venus",     .filelocation = "models/venus.gltf" },
+  },
+  // clang-format on
+  .object_index = 0,
+};
 
 static struct {
-  WGPUBuffer object;
-  WGPUBuffer skybox;
-  WGPUBuffer params;
+  // Object vertex shader uniform buffer
+  struct {
+    WGPUBuffer buffer;
+    uint64_t size;
+  } object;
+  // Skybox vertex shader uniform buffer
+  struct {
+    WGPUBuffer buffer;
+    uint64_t size;
+  } skybox;
+  // Shared parameter uniform buffer
+  struct {
+    WGPUBuffer buffer;
+    uint64_t size;
+  } ubo_params;
 } uniform_buffers;
+
+static struct matrial_params_dynamic_t {
+  float roughness;
+  float metallic;
+  vec3 color;
+  uint8_t padding[236];
+} material_params_dynamic[SINGLE_ROW_OBJECT_COUNT] = {0};
+
+static struct object_params_dynamic_t {
+  vec3 position;
+  uint8_t padding[244];
+} object_params_dynamic[SINGLE_ROW_OBJECT_COUNT] = {0};
 
 static struct {
   mat4 projection;
@@ -57,9 +98,11 @@ static struct {
 
 static struct {
   WGPURenderPipeline skybox;
+  WGPURenderPipeline pbr;
 } pipelines;
 
 static struct {
+  WGPUBindGroup object;
   WGPUBindGroup skybox;
 } bind_groups;
 
@@ -68,6 +111,46 @@ static struct {
 } bind_group_layouts;
 
 WGPUPipelineLayout pipeline_layout;
+
+// Default materials to select from
+static struct {
+  const char* name;
+  // Parameter block used as uniforms block
+  struct {
+    float roughness;
+    float metallic;
+    float specular;
+    vec3 color;
+  } params;
+} materials[12] = {
+  // clang-format off
+  // Setup some default materials (source:
+  // https://seblagarde.wordpress.com/2011/08/17/feeding-a-physical-based-lighting-mode/)
+  { .name = "Gold",     .params = { .roughness = 0.0f, .metallic = 0.0f, .specular = 0.0f, .color = { 1.0f, 0.765557f, 0.336057f } } },
+  { .name = "Copper",   .params = { .roughness = 0.0f, .metallic = 0.0f, .specular = 0.0f, .color = { 0.955008f, 0.637427f, 0.538163f } } },
+  { .name = "Chromium", .params = { .roughness = 0.0f, .metallic = 0.0f, .specular = 0.0f, .color = { 0.549585f, 0.556114f, 0.554256f } } },
+  { .name = "Nickel",   .params = { .roughness = 0.0f, .metallic = 0.0f, .specular = 0.0f, .color = { 0.659777f, 0.608679f, 0.525649f } } },
+  { .name = "Titanium", .params = { .roughness = 0.0f, .metallic = 0.0f, .specular = 0.0f, .color = { 0.541931f, 0.496791f, 0.449419f } } },
+  { .name = "Cobalt",   .params = { .roughness = 0.0f, .metallic = 0.0f, .specular = 0.0f, .color = { 0.662124f, 0.654864f, 0.633732f } } },
+  { .name = "Platinum", .params = { .roughness = 0.0f, .metallic = 0.0f, .specular = 0.0f, .color = { 0.672411f, 0.637331f, 0.585456f } } },
+  // Testing materials
+  { .name = "White", .params = { .roughness = 0.0f, .metallic = 0.0f, .specular = 0.0f, .color = { 1.0f, 1.0f, 1.0f } } },
+  { .name = "Dark",  .params = { .roughness = 0.0f, .metallic = 0.0f, .specular = 0.0f, .color = { 0.1f, 0.1f, 0.1f } } },
+  { .name = "Black", .params = { .roughness = 0.0f, .metallic = 0.0f, .specular = 0.0f, .color = { 0.0f, 0.0f, 0.0f } } },
+  { .name = "Red",   .params = { .roughness = 0.0f, .metallic = 0.0f, .specular = 0.0f, .color = { 1.0f, 0.0f, 0.0f } } },
+  { .name = "Blue",  .params = { .roughness = 0.0f, .metallic = 0.0f, .specular = 0.0f, .color = { 0.0f, 0.0f, 1.0f } } },
+  // clang-format on
+};
+static int32_t current_material_index = 9;
+
+// Arrays used for GUI
+static const char* material_names[12] = {
+  // Default materials
+  "Gold", "Copper", "Chromium", "Nickel", "Titanium", "Cobalt", "Platinum", //
+  // Testing materials
+  "White", "Dark", "Black", "Red", "Blue", //
+};
+static const char* object_names[4] = {"Sphere", "Teapot", "Torusknot", "Venus"};
 
 static const char* example_title = "PBR With Image Based Lighting";
 static bool prepared             = false;
@@ -89,7 +172,8 @@ static void load_assets(wgpu_context_t* wgpu_context)
 {
   // Load glTF models
   const uint32_t gltf_loading_flags
-    = WGPU_GLTF_FileLoadingFlags_PreTransformVertices;
+    = WGPU_GLTF_FileLoadingFlags_PreTransformVertices
+      | WGPU_GLTF_FileLoadingFlags_DontLoadImages;
   // Skybox
   models.skybox
     = wgpu_gltf_model_load_from_file(&(wgpu_gltf_model_load_options_t){
@@ -97,7 +181,16 @@ static void load_assets(wgpu_context_t* wgpu_context)
       .filename           = "models/cube.gltf",
       .file_loading_flags = gltf_loading_flags,
     });
-  // Load cube map
+  // Objects
+  for (uint8_t i = 0; i < (uint8_t)ARRAY_SIZE(models.objects); ++i) {
+    models.objects[i].object
+      = wgpu_gltf_model_load_from_file(&(wgpu_gltf_model_load_options_t){
+        .wgpu_context       = wgpu_context,
+        .filename           = models.objects[i].filelocation,
+        .file_loading_flags = gltf_loading_flags,
+      });
+  }
+  // Cube map
   static const char* cubemap[6] = {
     "textures/cubemaps/pisa_cube_px.jpg", // Right
     "textures/cubemaps/pisa_cube_nx.jpg", // Left
@@ -180,16 +273,16 @@ static void setup_bind_groups(wgpu_context_t* wgpu_context)
       [0] = (WGPUBindGroupEntry) {
         // Binding 0: Vertex shader uniform UBO
         .binding = 0,
-        .buffer = uniform_buffers.skybox,
+        .buffer = uniform_buffers.skybox.buffer,
         .offset = 0,
-        .size = sizeof(ubo_matrices),
+        .size = uniform_buffers.skybox.size,
       },
       [1] = (WGPUBindGroupEntry) {
         // Binding 1: Fragment uniform UBOParams
         .binding = 1,
-        .buffer = uniform_buffers.params,
+        .buffer = uniform_buffers.ubo_params.buffer,
         .offset = 0,
-        .size = sizeof(ubo_params),
+        .size = uniform_buffers.ubo_params.size,
       },
       [2] = (WGPUBindGroupEntry) {
         // Binding 2: Fragment shader image view
@@ -211,5 +304,124 @@ static void setup_bind_groups(wgpu_context_t* wgpu_context)
     bind_groups.skybox
       = wgpuDeviceCreateBindGroup(wgpu_context->device, &bg_desc);
     ASSERT(bind_groups.skybox != NULL)
+  }
+}
+
+static void update_uniform_buffers(wgpu_example_context_t* context)
+{
+  // 3D object
+  camera_t* camera = context->camera;
+  glm_mat4_copy(camera->matrices.perspective, ubo_matrices.projection);
+  glm_mat4_copy(camera->matrices.view, ubo_matrices.view);
+  glm_mat4_identity(ubo_matrices.model);
+  glm_rotate(ubo_matrices.model,
+             glm_rad(-90.0f + (models.object_index == 1 ? 45.0f : 0.0f)),
+             (vec3){0.0f, 1.0f, 0.0f});
+  glm_vec3_scale(camera->position, -1.0f, ubo_matrices.cam_pos);
+  wgpu_queue_write_buffer(context->wgpu_context, uniform_buffers.object.buffer,
+                          0, &ubo_matrices, uniform_buffers.object.size);
+
+  // Skybox
+  mat3 mat3_tmp = GLM_MAT3_ZERO_INIT;
+  glm_mat4_pick3(camera->matrices.view, mat3_tmp);
+  glm_mat4_ins3(mat3_tmp, ubo_matrices.model);
+  ubo_matrices.model[3][3] = 1.0f;
+  wgpu_queue_write_buffer(context->wgpu_context, uniform_buffers.skybox.buffer,
+                          0, &ubo_matrices, uniform_buffers.skybox.size);
+}
+
+static void update_params(wgpu_example_context_t* context)
+{
+  const float p = 15.0f;
+  glm_vec4_copy((vec4){-p, -p * 0.5f, -p, 1.0f}, ubo_params.lights[0]);
+  glm_vec4_copy((vec4){-p, -p * 0.5f, p, 1.0f}, ubo_params.lights[1]);
+  glm_vec4_copy((vec4){p, -p * 0.5f, p, 1.0f}, ubo_params.lights[2]);
+  glm_vec4_copy((vec4){p, -p * 0.5f, -p, 1.0f}, ubo_params.lights[3]);
+
+  wgpu_queue_write_buffer(context->wgpu_context,
+                          uniform_buffers.ubo_params.buffer, 0, &ubo_params,
+                          uniform_buffers.ubo_params.size);
+}
+
+// Prepare and initialize uniform buffer containing shader uniforms
+static void prepare_uniform_buffers(wgpu_example_context_t* context)
+{
+  // Object vertex shader uniform buffer
+  {
+    WGPUBufferDescriptor ubo_desc = {
+      .usage            = WGPUBufferUsage_CopyDst | WGPUBufferUsage_Uniform,
+      .size             = sizeof(ubo_matrices),
+      .mappedAtCreation = false,
+    };
+    uniform_buffers.object.size = ubo_desc.size;
+    uniform_buffers.object.buffer
+      = wgpuDeviceCreateBuffer(context->wgpu_context->device, &ubo_desc);
+  }
+
+  // Skybox vertex shader uniform buffer
+  {
+    WGPUBufferDescriptor ubo_desc = {
+      .usage            = WGPUBufferUsage_CopyDst | WGPUBufferUsage_Uniform,
+      .size             = sizeof(ubo_matrices),
+      .mappedAtCreation = false,
+    };
+    uniform_buffers.skybox.size = ubo_desc.size;
+    uniform_buffers.skybox.buffer
+      = wgpuDeviceCreateBuffer(context->wgpu_context->device, &ubo_desc);
+  }
+
+  // Shared parameter uniform buffer
+  {
+    WGPUBufferDescriptor ubo_desc = {
+      .usage            = WGPUBufferUsage_CopyDst | WGPUBufferUsage_Uniform,
+      .size             = sizeof(ubo_params),
+      .mappedAtCreation = false,
+    };
+    uniform_buffers.ubo_params.size = ubo_desc.size;
+    uniform_buffers.ubo_params.buffer
+      = wgpuDeviceCreateBuffer(context->wgpu_context->device, &ubo_desc);
+  }
+
+  update_uniform_buffers(context);
+  update_params(context);
+}
+
+static int example_initialize(wgpu_example_context_t* context)
+{
+  if (context) {
+    setup_camera(context);
+    load_assets(context->wgpu_context);
+    prepare_uniform_buffers(context);
+    setup_pipeline_layout(context->wgpu_context);
+    prepare_pipelines(context->wgpu_context);
+    setup_bind_group(context->wgpu_context);
+    setup_render_pass(context->wgpu_context);
+    prepared = true;
+    return 0;
+  }
+
+  return 1;
+}
+
+static void example_on_update_ui_overlay(wgpu_example_context_t* context)
+{
+  if (imgui_overlay_header("Settings")) {
+    if (imgui_overlay_combo_box(context->imgui_overlay, "Material",
+                                &current_material_index, material_names, 11)) {
+      update_dynamic_uniform_buffer(context->wgpu_context);
+    }
+    if (imgui_overlay_combo_box(context->imgui_overlay, "Object type",
+                                &models.object_index, object_names, 4)) {
+      update_dynamic_uniform_buffer(context->wgpu_context);
+    }
+    if (imgui_overlay_input_float(context->imgui_overlay, "Exposure",
+                                  &ubo_params.exposure, 0.1f, "%.2f")) {
+      update_params(context);
+    }
+    if (imgui_overlay_input_float(context->imgui_overlay, "Gamma",
+                                  &ubo_params.gamma, 0.1f, "%.2f")) {
+      update_params(context);
+    }
+    imgui_overlay_checkBox(context->imgui_overlay, "Skybox", &display_skybox);
   }
 }
