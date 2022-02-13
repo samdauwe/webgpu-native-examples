5bbb00bedaf0183df41eeaaf8f9e5aeb79a2b316
samdauwe <sam.dauwe@gmail.com>
2022-02-13 20:05:39 +0100

    Remove extra texture layer in deferredRendering example

--- src/examples/deferred_rendering.c
+++ src/examples/deferred_rendering.c
@@ -228,7 +228,7 @@ static void prepare_gbuffer_texture_render_targets(wgpu_context_t* wgpu_context)
       .size          = (WGPUExtent3D) {
         .width               = wgpu_context->surface.width,
         .height              = wgpu_context->surface.height,
-        .depthOrArrayLayers  = 3,
+        .depthOrArrayLayers  = 2,
       },
       .mipLevelCount = 1,
       .sampleCount   = 1,
