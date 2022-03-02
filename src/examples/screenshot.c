483a4ef9972592dd6e050103d813322a9cf68c04
samdauwe <sam.dauwe@gmail.com>
2022-03-02 21:59:11 +0100

    Applied formatting

--- src/examples/screenshot.c
+++ src/examples/screenshot.c
@@ -258,9 +258,9 @@ static void setup_pipeline_layout(wgpu_context_t* wgpu_context)
       .binding = 0,
       .visibility = WGPUShaderStage_Vertex,
       .buffer = (WGPUBufferBindingLayout) {
-        .type = WGPUBufferBindingType_Uniform,
+        .type             = WGPUBufferBindingType_Uniform,
         .hasDynamicOffset = false,
-        .minBindingSize = sizeof(ubo_vs),
+        .minBindingSize   = sizeof(ubo_vs),
       },
       .sampler = {0},
     }
