850b73f0d76d8acdc0870659c34db837ec9dfd3d
samdauwe <sam.dauwe@gmail.com>
2022-03-03 21:30:18 +0100

    Continued WebGPU Example - A Conway Game Of Life

--- src/examples/conway.c
+++ src/examples/conway.c
@@ -3,8 +3,6 @@
 
 #include <string.h>
 
-#include "../webgpu/imgui_overlay.h"
-
 /* -------------------------------------------------------------------------- *
  * WebGPU Example - A Conway Game Of Life
  *
@@ -14,6 +12,96 @@
  * https://github.com/Palats/webgpu/blob/main/src/demos/conway.ts
  * https://en.wikipedia.org/wiki/Conway%27s_Game_of_Life
  * -------------------------------------------------------------------------- */
+
+// Shaders
+// clang-format off
+static const char* compute_shader_wgsl =
+  "[[block]] struct UniformsDesc {\n"
+  "  computeWidth : u32;\n"
+  "  computeHeight : u32;\n"
+  "};\n"
+  "\n"
+  "@group(0) @binding(0) var<uniform> uniforms : UniformsDesc;\n"
+  "@group(0) @binding(1) var srcTexture : texture_2d<f32>;\n"
+  "@group(0) @binding(2) var dstTexture : texture_storage_2d<rgba8unorm, write>;\n"
+  "\n"
+  "fn isOn(x: i32, y: i32) -> i32 {\n"
+  "  let v = textureLoad(srcTexture, vec2<i32>(x, y), 0);\n"
+  "  if (v.r < 0.5) { return 0;}\n"
+  "  return 1;\n"
+  "}\n"
+  "\n"
+  "@stage(compute) @workgroup_size(8, 8)\n"
+  "fn main(@builtin(global_invocation_id) global_id : vec3<u32>) {\n"
+  "  // Guard against out-of-bounds work group sizes\n"
+  "  if (global_id.x >= uniforms.computeWidth || global_id.y >= uniforms.computeHeight) {\n"
+  "      return;\n"
+  "  }\n"
+  "\n"
+  "  let x = i32(global_id.x);\n"
+  "  let y = i32(global_id.y);\n"
+  "  let current = isOn(x, y);\n"
+  "  let neighbors =\n"
+  "      isOn(x - 1, y - 1)\n"
+  "    + isOn(x, y - 1)\n"
+  "    + isOn(x + 1, y - 1)\n"
+  "    + isOn(x - 1, y)\n"
+  "    + isOn(x + 1, y)\n"
+  "    + isOn(x - 1, y + 1)\n"
+  "    + isOn(x, y + 1)\n"
+  "    + isOn(x + 1, y + 1);\n"
+  "\n"
+  "  var s = 0.0;\n"
+  "  if (current != 0 && (neighbors == 2 || neighbors == 3)) {\n"
+  "    s = 1.0;\n"
+  "}\n"
+  " if (current == 0 && neighbors == 3) {\n"
+  "    s = 1.0;\n"
+  "  }\n"
+  "  textureStore(dstTexture, vec2<i32>(x, y), vec4<f32>(s, s, s, 1.0));\n"
+  "}";
+
+static const char* graphics_vertex_shader_wgsl =
+  "struct VSOut {\n"
+  "  @builtin(position) pos: vec4<f32>;\n"
+  "  @location(0) coord: vec2<f32>;\n"
+  "};\n"
+  "@stage(vertex)\n"
+  "fn main(@builtin(vertex_index) idx : u32) -> VSOut {\n"
+  "  var data = array<vec2<f32>, 6>(\n"
+  "    vec2<f32>(-1.0, -1.0),\n"
+  "    vec2<f32>(1.0, -1.0),\n"
+  "    vec2<f32>(1.0, 1.0),\n"
+  "\n"
+  "    vec2<f32>(-1.0, -1.0),\n"
+  "    vec2<f32>(-1.0, 1.0),\n"
+  "    vec2<f32>(1.0, 1.0),\n"
+  "  );\n"
+  "\n"
+  "  let pos = data[idx];\n"
+  "\n"
+  "  var out : VSOut;\n"
+  "  out.pos = vec4<f32>(pos, 0.0, 1.0);\n"
+  "  out.coord.x = (pos.x + 1.0) / 2.0;\n"
+  "  out.coord.y = (1.0 - pos.y) / 2.0;\n"
+  "\n"
+  "  return out;\n"
+  "}";
+
+static const char* graphics_fragment_shader_wgsl =
+  "struct VSOut {\n"
+  "  @builtin(position) pos: vec4<f32>;\n"
+  "  @location(0) coord: vec2<f32>;\n"
+  "};\n"
+  "\n"
+  "@group(0) @binding(0) var computeTexture : texture_2d<f32>;\n"
+  "@group(0) @binding(1) var dstSampler : sampler;\n"
+  "\n"
+  "@stage(fragment)\n"
+  "fn main(inp: VSOut) -> @location(0) vec4<f32> {\n"
+  "  return textureSample(computeTexture, dstSampler, inp.coord);\n"
+  "}";
+// clang-format on
 
 static struct {
   struct {
@@ -33,36 +121,48 @@ static struct {
 static WGPUTextureFormat COMPUTE_TEX_FORMAT = WGPUTextureFormat_RGBA8Unorm;
 static texture_t textures[2];
 
-// Bind group layouts
+// Resources for the graphics part of the example
 static struct {
-  WGPUBindGroupLayout compute;
-  WGPUBindGroupLayout render;
-} bind_group_layouts;
+  WGPUBindGroupLayout bind_group_layout;
+  WGPUBindGroup bind_groups[2];
+  WGPUPipelineLayout pipeline_layout;
+  WGPURenderPipeline pipeline;
+} graphics;
 
-// Bind groups
+// Resources for the compute part of the example
 static struct {
-  WGPUBindGroup compute[2];
-  WGPUBindGroup render[2];
-} bind_groups;
+  WGPUBindGroupLayout bind_group_layout;
+  WGPUBindGroup bind_groups[2];
+  WGPUPipelineLayout pipeline_layout;
+  WGPUComputePipeline pipeline;
+} compute;
 
-// Pipeline layouts
+// Render pass descriptor for frame buffer writes
 static struct {
-  WGPUPipelineLayout compute;
-  WGPUPipelineLayout render;
-} pipeline_layouts;
+  WGPURenderPassColorAttachment color_attachments[1];
+  WGPURenderPassDescriptor descriptor;
+} render_pass;
 
-// Pipelines
-static struct {
-  WGPUComputePipeline compute;
-  WGPURenderPipeline render;
-} pipelines;
+static bool is_forward = true;
 
 // Other variables
 static const char* example_title = "A Conway Game Of Life";
 static bool prepared             = false;
 
+static void update_uniform_buffers(wgpu_context_t* wgpu_context)
+{
+  // Update unfirms data
+  uniforms.desc.compute_height = wgpu_context->surface.width;
+  uniforms.desc.compute_width  = wgpu_context->surface.height;
+
+  // Uplaad buffer to the GPU
+  wgpu_queue_write_buffer(wgpu_context, uniforms.buffer.handle, 0,
+                          &uniforms.desc, uniforms.buffer.size);
+}
+
 static void prepare_uniform_buffers(wgpu_context_t* wgpu_context)
 {
+  // Create uniforms buffer
   uniforms.buffer.size                     = sizeof(uniforms.desc);
   WGPUBufferDescriptor texture_buffer_desc = {
     .label            = "Compute uniforms buffer",
@@ -73,6 +173,9 @@ static void prepare_uniform_buffers(wgpu_context_t* wgpu_context)
   uniforms.buffer.handle
     = wgpuDeviceCreateBuffer(wgpu_context->device, &texture_buffer_desc);
   ASSERT(uniforms.buffer.handle)
+
+  // Update unifroms buffer data
+  update_uniform_buffers(wgpu_context);
 }
 
 static void prepare_textures(wgpu_context_t* wgpu_context)
@@ -195,22 +298,22 @@ static void setup_pipeline_layouts(wgpu_context_t* wgpu_context)
       .entryCount = (uint32_t)ARRAY_SIZE(bgl_entries),
       .entries    = bgl_entries,
     };
-    bind_group_layouts.compute
+    compute.bind_group_layout
       = wgpuDeviceCreateBindGroupLayout(wgpu_context->device, &bgl_desc);
-    ASSERT(bind_group_layouts.compute != NULL)
+    ASSERT(compute.bind_group_layout != NULL)
 
     // Compute pipeline layout
     WGPUPipelineLayoutDescriptor compute_pipeline_layout_desc = {
       .label                = "compute pipeline layouts",
       .bindGroupLayoutCount = 1,
-      .bindGroupLayouts     = &bind_group_layouts.compute,
+      .bindGroupLayouts     = &compute.bind_group_layout,
     };
-    pipeline_layouts.compute = wgpuDeviceCreatePipelineLayout(
+    compute.pipeline_layout = wgpuDeviceCreatePipelineLayout(
       wgpu_context->device, &compute_pipeline_layout_desc);
-    ASSERT(pipeline_layouts.compute != NULL)
+    ASSERT(compute.pipeline_layout != NULL)
   }
 
-  /* Rendering pipeline layout */
+  /* Graphics pipeline layout */
   {
     WGPUBindGroupLayoutEntry bgl_entries[2] = {
       [0] = (WGPUBindGroupLayoutEntry) {
@@ -239,20 +342,44 @@ static void setup_pipeline_layouts(wgpu_context_t* wgpu_context)
       .entryCount = (uint32_t)ARRAY_SIZE(bgl_entries),
       .entries    = bgl_entries,
     };
-    bind_group_layouts.render
+    graphics.bind_group_layout
       = wgpuDeviceCreateBindGroupLayout(wgpu_context->device, &bgl_desc);
-    ASSERT(bind_group_layouts.render != NULL)
+    ASSERT(graphics.bind_group_layout != NULL)
 
     // Render pipeline layout
     WGPUPipelineLayoutDescriptor compute_pipeline_layout_desc = {
       .label                = "rendering pipeline layouts",
       .bindGroupLayoutCount = 1,
-      .bindGroupLayouts     = &bind_group_layouts.render,
+      .bindGroupLayouts     = &graphics.bind_group_layout,
     };
-    pipeline_layouts.render = wgpuDeviceCreatePipelineLayout(
+    graphics.pipeline_layout = wgpuDeviceCreatePipelineLayout(
       wgpu_context->device, &compute_pipeline_layout_desc);
-    ASSERT(pipeline_layouts.render != NULL)
+    ASSERT(graphics.pipeline_layout != NULL)
   }
+}
+
+static void setup_render_pass(wgpu_context_t* wgpu_context)
+{
+  UNUSED_VAR(wgpu_context);
+
+  // Color attachment
+  render_pass.color_attachments[0] = (WGPURenderPassColorAttachment) {
+      .view       = NULL, // Assigned later
+      .loadOp     = WGPULoadOp_Clear,
+      .storeOp    = WGPUStoreOp_Store,
+      .clearColor = (WGPUColor) {
+        .r = 0.0f,
+        .g = 0.0f,
+        .b = 0.0f,
+        .a = 1.0f,
+      },
+  };
+
+  // Render pass descriptor
+  render_pass.descriptor = (WGPURenderPassDescriptor){
+    .colorAttachmentCount = 1,
+    .colorAttachments     = render_pass.color_attachments,
+  };
 }
 
 static void setup_bind_groups(wgpu_context_t* wgpu_context)
@@ -277,14 +404,14 @@ static void setup_bind_groups(wgpu_context_t* wgpu_context)
         },
       };
 
-    bind_groups.compute[i] = wgpuDeviceCreateBindGroup(
+    compute.bind_groups[i] = wgpuDeviceCreateBindGroup(
       wgpu_context->device,
       &(WGPUBindGroupDescriptor){
-        .layout = wgpuComputePipelineGetBindGroupLayout(pipelines.compute, 0),
+        .layout = wgpuComputePipelineGetBindGroupLayout(compute.pipeline, 0),
         .entryCount = (uint32_t)ARRAY_SIZE(bg_entries),
         .entries    = bg_entries,
       });
-    ASSERT(bind_groups.compute[i] != NULL)
+    ASSERT(compute.bind_groups[i] != NULL)
   }
 
   // Create 2 bind group for the render pipeline, depending on what is the
@@ -302,13 +429,236 @@ static void setup_bind_groups(wgpu_context_t* wgpu_context)
         },
       };
 
-    bind_groups.render[i] = wgpuDeviceCreateBindGroup(
+    graphics.bind_groups[i] = wgpuDeviceCreateBindGroup(
       wgpu_context->device,
       &(WGPUBindGroupDescriptor){
-        .layout     = wgpuRenderPipelineGetBindGroupLayout(pipelines.render, 0),
+        .layout = wgpuRenderPipelineGetBindGroupLayout(graphics.pipeline, 0),
         .entryCount = (uint32_t)ARRAY_SIZE(bg_entries),
         .entries    = bg_entries,
       });
-    ASSERT(bind_groups.render[i] != NULL)
+    ASSERT(graphics.bind_groups[i] != NULL)
   }
+}
+
+static void prepare_pipelines(wgpu_context_t* wgpu_context)
+{
+  /* Compute pipeline */
+  {
+    // Compute shader
+    wgpu_shader_t conway_comp_shader = wgpu_shader_create(
+      wgpu_context, &(wgpu_shader_desc_t){
+                      // Compute shader WGSL
+                      .wgsl_code.source = compute_shader_wgsl,
+                      .entry            = "main",
+                    });
+
+    // Create compute pipeline
+    compute.pipeline = wgpuDeviceCreateComputePipeline(
+      wgpu_context->device,
+      &(WGPUComputePipelineDescriptor){
+        .layout  = compute.pipeline_layout,
+        .compute = conway_comp_shader.programmable_stage_descriptor,
+      });
+
+    // Partial cleanup
+    wgpu_shader_release(&conway_comp_shader);
+  }
+
+  /* Graphics pipeline */
+  {
+    // Primitive state
+    WGPUPrimitiveState primitive_state_desc = {
+      .topology  = WGPUPrimitiveTopology_TriangleList,
+      .frontFace = WGPUFrontFace_CCW,
+      .cullMode  = WGPUCullMode_Back,
+    };
+
+    // Color target state
+    WGPUBlendState blend_state = wgpu_create_blend_state(true);
+    WGPUColorTargetState color_target_state_desc = (WGPUColorTargetState){
+      .format    = wgpu_context->swap_chain.format,
+      .blend     = &blend_state,
+      .writeMask = WGPUColorWriteMask_All,
+    };
+
+    // Vertex state
+    WGPUVertexState vertex_state_desc = wgpu_create_vertex_state(
+          wgpu_context, &(wgpu_vertex_state_t){
+            .shader_desc = (wgpu_shader_desc_t){
+              // Vertex shader WGSL
+              .wgsl_code.source = graphics_vertex_shader_wgsl,
+              .entry            = "main",
+            },
+            .buffer_count = 0,
+            .buffers = NULL,
+          });
+
+    // Fragment state
+    WGPUFragmentState fragment_state_desc = wgpu_create_fragment_state(
+          wgpu_context, &(wgpu_fragment_state_t){
+            .shader_desc = (wgpu_shader_desc_t){
+              // Fragment shader WGSL
+              .wgsl_code.source = graphics_fragment_shader_wgsl,
+              .entry            = "main",
+            },
+            .target_count = 1,
+            .targets = &color_target_state_desc,
+          });
+
+    // Multisample state
+    WGPUMultisampleState multisample_state_desc
+      = wgpu_create_multisample_state_descriptor(
+        &(create_multisample_state_desc_t){
+          .sample_count = 1,
+        });
+
+    // Create rendering pipeline using the specified states
+    graphics.pipeline = wgpuDeviceCreateRenderPipeline(
+      wgpu_context->device, &(WGPURenderPipelineDescriptor){
+                              .label       = "conway_graphics_pipeline",
+                              .layout      = graphics.pipeline_layout,
+                              .primitive   = primitive_state_desc,
+                              .vertex      = vertex_state_desc,
+                              .fragment    = &fragment_state_desc,
+                              .multisample = multisample_state_desc,
+                            });
+
+    // Partial cleanup
+    WGPU_RELEASE_RESOURCE(ShaderModule, vertex_state_desc.module);
+    WGPU_RELEASE_RESOURCE(ShaderModule, fragment_state_desc.module);
+  }
+}
+
+static int example_initialize(wgpu_example_context_t* context)
+{
+  if (context) {
+    prepare_uniform_buffers(context->wgpu_context);
+    prepare_textures(context->wgpu_context);
+    setup_pipeline_layouts(context->wgpu_context);
+    prepare_pipelines(context->wgpu_context);
+    setup_bind_groups(context->wgpu_context);
+    setup_render_pass(context->wgpu_context);
+    prepared = true;
+    return 0;
+  }
+
+  return 1;
+}
+
+static WGPUCommandBuffer build_command_buffer(wgpu_example_context_t* context)
+{
+  wgpu_context_t* wgpu_context          = context->wgpu_context;
+  render_pass.color_attachments[0].view = wgpu_context->swap_chain.frame_buffer;
+
+  // Create command encoder
+  wgpu_context->cmd_enc
+    = wgpuDeviceCreateCommandEncoder(wgpu_context->device, NULL);
+
+  // -- Do compute pass, where the actual effect is -- //
+  {
+    wgpu_context->cpass_enc
+      = wgpuCommandEncoderBeginComputePass(wgpu_context->cmd_enc, NULL);
+    wgpuComputePassEncoderSetPipeline(wgpu_context->cpass_enc,
+                                      compute.pipeline);
+    wgpuComputePassEncoderSetBindGroup(
+      wgpu_context->cpass_enc, 0,
+      is_forward ? compute.bind_groups[0] : compute.bind_groups[1], 0, NULL);
+    wgpuComputePassEncoderDispatch(
+      wgpu_context->cpass_enc, ceil(uniforms.desc.compute_width / 8.0f),
+      ceil(uniforms.desc.compute_height / 8.0f), 1);
+    wgpuComputePassEncoderEndPass(wgpu_context->cpass_enc);
+    WGPU_RELEASE_RESOURCE(ComputePassEncoder, wgpu_context->cpass_enc)
+  }
+
+  // -- And do the frame rendering -- //
+  {
+    wgpu_context->rpass_enc = wgpuCommandEncoderBeginRenderPass(
+      wgpu_context->cmd_enc, &render_pass.descriptor);
+    wgpuRenderPassEncoderSetPipeline(wgpu_context->rpass_enc,
+                                     graphics.pipeline);
+    wgpuComputePassEncoderSetBindGroup(
+      wgpu_context->cpass_enc, 0,
+      is_forward ? graphics.bind_groups[0] : graphics.bind_groups[1], 0, NULL);
+    wgpuRenderPassEncoderDraw(wgpu_context->rpass_enc, 6, 1, 0, 0);
+    wgpuRenderPassEncoderEndPass(wgpu_context->rpass_enc);
+    WGPU_RELEASE_RESOURCE(RenderPassEncoder, wgpu_context->rpass_enc)
+  }
+
+  // Get command buffer
+  WGPUCommandBuffer command_buffer
+    = wgpu_get_command_buffer(wgpu_context->cmd_enc);
+  ASSERT(command_buffer != NULL)
+  WGPU_RELEASE_RESOURCE(CommandEncoder, wgpu_context->cmd_enc)
+
+  return command_buffer;
+}
+
+static int example_draw(wgpu_example_context_t* context)
+{
+  // Prepare frame
+  prepare_frame(context);
+
+  // Command buffer to be submitted to the queue
+  wgpu_context_t* wgpu_context                   = context->wgpu_context;
+  wgpu_context->submit_info.command_buffer_count = 1;
+  wgpu_context->submit_info.command_buffers[0] = build_command_buffer(context);
+
+  // Submit to queue
+  submit_command_buffers(context);
+
+  // Submit frame
+  submit_frame(context);
+
+  // Switch for next frame
+  is_forward = !is_forward;
+
+  return 0;
+}
+
+static int example_render(wgpu_example_context_t* context)
+{
+  if (!prepared) {
+    return 1;
+  }
+  return example_draw(context);
+}
+
+// Clean up used resources
+static void example_destroy(wgpu_example_context_t* context)
+{
+  UNUSED_VAR(context);
+
+  // Textures
+  wgpu_destroy_texture(&textures[0]);
+  wgpu_destroy_texture(&textures[1]);
+
+  // Graphics pipeline
+  WGPU_RELEASE_RESOURCE(BindGroupLayout, graphics.bind_group_layout)
+  WGPU_RELEASE_RESOURCE(BindGroup, graphics.bind_groups[0])
+  WGPU_RELEASE_RESOURCE(BindGroup, graphics.bind_groups[1])
+  WGPU_RELEASE_RESOURCE(PipelineLayout, graphics.pipeline_layout)
+  WGPU_RELEASE_RESOURCE(RenderPipeline, graphics.pipeline)
+
+  // Compute pipeline
+  WGPU_RELEASE_RESOURCE(Buffer, uniforms.buffer.handle)
+  WGPU_RELEASE_RESOURCE(BindGroupLayout, compute.bind_group_layout)
+  WGPU_RELEASE_RESOURCE(BindGroup, compute.bind_groups[0])
+  WGPU_RELEASE_RESOURCE(BindGroup, compute.bind_groups[1])
+  WGPU_RELEASE_RESOURCE(PipelineLayout, compute.pipeline_layout)
+  WGPU_RELEASE_RESOURCE(ComputePipeline, compute.pipeline)
+}
+
+void example_conway(int argc, char* argv[])
+{
+  // clang-format off
+  example_run(argc, argv, &(refexport_t){
+    .example_settings = (wgpu_example_settings_t){
+     .title   = example_title,
+     .vsync   = true,
+    },
+    .example_initialize_func      = &example_initialize,
+    .example_render_func          = &example_render,
+    .example_destroy_func         = &example_destroy,
+  });
+  // clang-format on
 }
