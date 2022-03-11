3a2fdf46990a2e449bb999a451c5d463741b71e0
samdauwe <sam.dauwe@gmail.com>
2022-03-11 21:27:12 +0100

    Updated fullscreen texture quad shader wgsl: remove [[block]] and [[strides]], elseif -> else if, [[]] -> @

--- src/examples/common_shaders.h
+++ src/examples/common_shaders.h
@@ -32,16 +32,16 @@ static const char* vertex_position_color_fragment_shader_wgsl =
   "}";
 
 static const char* fullscreen_textured_quad_wgsl =
-  "[[group(0), binding(0)]] var mySampler : sampler;\n"
-  "[[group(0), binding(1)]] var myTexture : texture_2d<f32>;\n"
+  "@group(0) @binding(0) var mySampler : sampler;\n"
+  "@group(0) @binding(1) var myTexture : texture_2d<f32>;\n"
   "\n"
   "struct VertexOutput {\n"
-  "  [[builtin(position)]] Position : vec4<f32>;\n"
-  "  [[location(0)]] fragUV : vec2<f32>;\n"
+  "  @builtin(position) Position : vec4<f32>;\n"
+  "  @location(0) fragUV : vec2<f32>;\n"
   "};\n"
   "\n"
-  "[[stage(vertex)]]\n"
-  "fn vert_main([[builtin(vertex_index)]] VertexIndex : u32) -> VertexOutput {\n"
+  "@stage(vertex)\n"
+  "fn vert_main(@builtin(vertex_index) VertexIndex : u32) -> VertexOutput {\n"
   "  var pos = array<vec2<f32>, 6>(\n"
   "      vec2<f32>( 1.0,  1.0),\n"
   "      vec2<f32>( 1.0, -1.0),\n"
@@ -64,8 +64,8 @@ static const char* fullscreen_textured_quad_wgsl =
   "  return output;\n"
   "}\n"
   "\n"
-  "[[stage(fragment)]]\n"
-  "fn frag_main([[location(0)]] fragUV : vec2<f32>) -> [[location(0)]] vec4<f32> {\n"
+  "@stage(fragment)\n"
+  "fn frag_main(@location(0) fragUV : vec2<f32>) -> @location(0) vec4<f32> {\n"
   "  return textureSample(myTexture, mySampler, fragUV);\n"
   "}";
 // clang-format on
