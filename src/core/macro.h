c966c2037baa862cb8b3a16b47b500018735d261
samdauwe <sam.dauwe@gmail.com>
2022-03-13 18:06:36 +0100

    Added math constants

--- src/core/macro.h
+++ src/core/macro.h
@@ -17,8 +17,21 @@
 #define NULL ((void*)0)
 #endif
 
-#define EPSILON 1e-5f              /* epsilon */
-#define PI 3.14159265358979323846f /* pi */
+/* Constants */
+#define E 2.71828182845904523536f        /* Euler's constant e */
+#define LOG2E 1.44269504088896340736f    /* log2(e) */
+#define LOG10E 0.434294481903251827651f  /* log10(e) */
+#define LN2 0.693147180559945309417f     /* ln(2) */
+#define LN10 2.30258509299404568402f     /* ln(10) */
+#define PI 3.14159265358979323846f       /* pi */
+#define PI2 6.28318530717958647692f      /* pi * 2 */
+#define PI_2 1.57079632679489661923f     /* pi/2 */
+#define PI_4 0.785398163397448309616f    /* pi/4 */
+#define SQRT2 1.41421356237309504880f    /* sqrt(2) */
+#define SQRT2_2 0.707106781186547524401f /* sqrt(2)/2 */
+
+/* Constant used to define the minimal number value */
+#define EPSILON 1e-5f /* epsilon */
 
 #define TO_RADIANS(degrees) ((PI / 180) * (degrees))
 #define TO_DEGREES(radians) ((180 / PI) * (radians))
