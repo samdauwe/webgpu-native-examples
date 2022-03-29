de2eb36816ffebbda541641afb6c7c56a4692c0b
samdauwe <sam.dauwe@gmail.com>
2022-03-29 22:29:15 +0200

    Updated comment

--- src/examples/texture_mipmap_gen.c
+++ src/examples/texture_mipmap_gen.c
@@ -569,7 +569,7 @@ static WGPUCommandBuffer build_command_buffer(wgpu_context_t* wgpu_context)
   wgpuRenderPassEncoderSetBindGroup(wgpu_context->rpass_enc, 0, bind_group, 0,
                                     0);
 
-  // Draw plane
+  // Draw model
   wgpu_gltf_model_draw(model, (wgpu_gltf_model_render_options_t){0});
 
   // End render pass
