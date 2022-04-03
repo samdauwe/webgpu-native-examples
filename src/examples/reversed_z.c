5e4074f59ee1f618e1030608bf6202ea8f6f3d9c
samdauwe <sam.dauwe@gmail.com>
2022-04-03 09:53:56 +0200

    Applied formatting

--- src/examples/reversed_z.c
+++ src/examples/reversed_z.c
@@ -1262,16 +1262,16 @@ void example_reversed_z(int argc, char* argv[])
   // clang-format off
   example_run(argc, argv, &(refexport_t){
     .example_settings = (wgpu_example_settings_t){
-     .title  = example_title,
+     .title   = example_title,
      .overlay = true,
     },
     .example_window_config = (window_config_t){
-      .width = default_canvas_width,
+      .width  = default_canvas_width,
       .height = default_canvas_height,
     },
-    .example_initialize_func      = &example_initialize,
-    .example_render_func          = &example_render,
-    .example_destroy_func         = &example_destroy
+    .example_initialize_func = &example_initialize,
+    .example_render_func     = &example_render,
+    .example_destroy_func    = &example_destroy
   });
   // clang-format on
 }
