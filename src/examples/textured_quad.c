f9929568bb132662a8cfdb651c4f57dde1f00b8c
samdauwe <sam.dauwe@gmail.com>
2022-04-20 21:01:07 +0200

    Applied formatting

--- src/examples/textured_quad.c
+++ src/examples/textured_quad.c
@@ -489,7 +489,7 @@ void example_textured_quad(int argc, char* argv[])
   // clang-format off
   example_run(argc, argv, &(refexport_t){
     .example_settings = (wgpu_example_settings_t){
-      .title = example_title,
+      .title   = example_title,
       .overlay = true,
     },
     .example_initialize_func      = &example_initialize,
