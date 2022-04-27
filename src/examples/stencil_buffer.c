8cd9ff3c10b56b77505f4fdd63c04eb9c7153b0d
samdauwe <sam.dauwe@gmail.com>
2022-04-27 21:18:24 +0200

    Renamed render pipeline state objects

--- src/examples/stencil_buffer.c
+++ src/examples/stencil_buffer.c
@@ -163,22 +163,22 @@ static void prepare_pipelines(wgpu_context_t* wgpu_context)
   // Construct the different states making up the pipeline
 
   // Primitive state
-  WGPUPrimitiveState primitive_state_desc = {
+  WGPUPrimitiveState primitive_state = {
     .topology  = WGPUPrimitiveTopology_TriangleList,
     .frontFace = WGPUFrontFace_CCW,
     .cullMode  = WGPUCullMode_Back,
   };
 
   // Color target state
-  WGPUBlendState blend_state                   = wgpu_create_blend_state(false);
-  WGPUColorTargetState color_target_state_desc = (WGPUColorTargetState){
+  WGPUBlendState blend_state              = wgpu_create_blend_state(false);
+  WGPUColorTargetState color_target_state = (WGPUColorTargetState){
     .format    = wgpu_context->swap_chain.format,
     .blend     = &blend_state,
     .writeMask = WGPUColorWriteMask_All,
   };
 
   // Depth stencil state
-  WGPUDepthStencilState depth_stencil_state_desc
+  WGPUDepthStencilState depth_stencil_state
     = wgpu_create_depth_stencil_state(&(create_depth_stencil_state_desc_t){
       .format              = WGPUTextureFormat_Depth24PlusStencil8,
       .depth_write_enabled = true,
@@ -196,19 +196,17 @@ static void prepare_pipelines(wgpu_context_t* wgpu_context)
 
   // Toon render and stencil fill pass
   {
-    depth_stencil_state_desc.depthWriteEnabled   = true;
-    depth_stencil_state_desc.stencilBack.compare = WGPUCompareFunction_Always;
-    depth_stencil_state_desc.stencilBack.failOp  = WGPUStencilOperation_Replace;
-    depth_stencil_state_desc.stencilBack.depthFailOp
-      = WGPUStencilOperation_Replace;
-    depth_stencil_state_desc.stencilBack.passOp = WGPUStencilOperation_Replace;
-    depth_stencil_state_desc.stencilReadMask    = 0xff;
-    depth_stencil_state_desc.stencilWriteMask   = 0xff;
-    depth_stencil_state_desc.stencilFront
-      = depth_stencil_state_desc.stencilBack;
+    depth_stencil_state.depthWriteEnabled       = true;
+    depth_stencil_state.stencilBack.compare     = WGPUCompareFunction_Always;
+    depth_stencil_state.stencilBack.failOp      = WGPUStencilOperation_Replace;
+    depth_stencil_state.stencilBack.depthFailOp = WGPUStencilOperation_Replace;
+    depth_stencil_state.stencilBack.passOp      = WGPUStencilOperation_Replace;
+    depth_stencil_state.stencilReadMask         = 0xff;
+    depth_stencil_state.stencilWriteMask        = 0xff;
+    depth_stencil_state.stencilFront = depth_stencil_state.stencilBack;
 
     // Vertex state
-    WGPUVertexState vertex_state_desc = wgpu_create_vertex_state(
+    WGPUVertexState vertex_state = wgpu_create_vertex_state(
           wgpu_context, &(wgpu_vertex_state_t){
             .shader_desc = (wgpu_shader_desc_t){
               // Vertex shader SPIR-V
@@ -219,18 +217,18 @@ static void prepare_pipelines(wgpu_context_t* wgpu_context)
           });
 
     // Fragment state
-    WGPUFragmentState fragment_state_desc = wgpu_create_fragment_state(
+    WGPUFragmentState fragment_state = wgpu_create_fragment_state(
           wgpu_context, &(wgpu_fragment_state_t){
             .shader_desc = (wgpu_shader_desc_t){
               // Fragment shader SPIR-V
               .file = "shaders/stencil_buffer/toon.frag.spv",
             },
             .target_count = 1,
-            .targets = &color_target_state_desc,
+            .targets = &color_target_state,
           });
 
     // Multisample state
-    WGPUMultisampleState multisample_state_desc
+    WGPUMultisampleState multisample_state
       = wgpu_create_multisample_state_descriptor(
         &(create_multisample_state_desc_t){
           .sample_count = 1,
@@ -241,33 +239,31 @@ static void prepare_pipelines(wgpu_context_t* wgpu_context)
       wgpu_context->device, &(WGPURenderPipelineDescriptor){
                               .label        = "stencil_render_pipeline",
                               .layout       = pipeline_layout,
-                              .primitive    = primitive_state_desc,
-                              .vertex       = vertex_state_desc,
-                              .fragment     = &fragment_state_desc,
-                              .depthStencil = &depth_stencil_state_desc,
-                              .multisample  = multisample_state_desc,
+                              .primitive    = primitive_state,
+                              .vertex       = vertex_state,
+                              .fragment     = &fragment_state,
+                              .depthStencil = &depth_stencil_state,
+                              .multisample  = multisample_state,
                             });
     ASSERT(pipelines.stencil != NULL);
 
     // Shader modules are no longer needed once the graphics pipeline has been
     // created
-    WGPU_RELEASE_RESOURCE(ShaderModule, vertex_state_desc.module);
-    WGPU_RELEASE_RESOURCE(ShaderModule, fragment_state_desc.module);
+    WGPU_RELEASE_RESOURCE(ShaderModule, vertex_state.module);
+    WGPU_RELEASE_RESOURCE(ShaderModule, fragment_state.module);
   }
 
   // Outline pass
   {
-    depth_stencil_state_desc.depthWriteEnabled   = false;
-    depth_stencil_state_desc.stencilBack.compare = WGPUCompareFunction_NotEqual;
-    depth_stencil_state_desc.stencilBack.failOp  = WGPUStencilOperation_Keep;
-    depth_stencil_state_desc.stencilBack.depthFailOp
-      = WGPUStencilOperation_Keep;
-    depth_stencil_state_desc.stencilBack.passOp = WGPUStencilOperation_Replace;
-    depth_stencil_state_desc.stencilFront
-      = depth_stencil_state_desc.stencilBack;
+    depth_stencil_state.depthWriteEnabled       = false;
+    depth_stencil_state.stencilBack.compare     = WGPUCompareFunction_NotEqual;
+    depth_stencil_state.stencilBack.failOp      = WGPUStencilOperation_Keep;
+    depth_stencil_state.stencilBack.depthFailOp = WGPUStencilOperation_Keep;
+    depth_stencil_state.stencilBack.passOp      = WGPUStencilOperation_Replace;
+    depth_stencil_state.stencilFront = depth_stencil_state.stencilBack;
 
     // Vertex state
-    WGPUVertexState vertex_state_desc = wgpu_create_vertex_state(
+    WGPUVertexState vertex_state = wgpu_create_vertex_state(
           wgpu_context, &(wgpu_vertex_state_t){
             .shader_desc = (wgpu_shader_desc_t){
               // Vertex shader SPIR-V
@@ -278,18 +274,18 @@ static void prepare_pipelines(wgpu_context_t* wgpu_context)
           });
 
     // Fragment state
-    WGPUFragmentState fragment_state_desc = wgpu_create_fragment_state(
+    WGPUFragmentState fragment_state = wgpu_create_fragment_state(
           wgpu_context, &(wgpu_fragment_state_t){
             .shader_desc = (wgpu_shader_desc_t){
               // Fragment shader SPIR-V
               .file = "shaders/stencil_buffer/outline.frag.spv",
             },
             .target_count = 1,
-            .targets = &color_target_state_desc,
+            .targets = &color_target_state,
           });
 
     // Multisample state
-    WGPUMultisampleState multisample_state_desc
+    WGPUMultisampleState multisample_state
       = wgpu_create_multisample_state_descriptor(
         &(create_multisample_state_desc_t){
           .sample_count = 1,
@@ -300,18 +296,18 @@ static void prepare_pipelines(wgpu_context_t* wgpu_context)
       wgpu_context->device, &(WGPURenderPipelineDescriptor){
                               .label        = "outline_render_pipeline",
                               .layout       = pipeline_layout,
-                              .primitive    = primitive_state_desc,
-                              .vertex       = vertex_state_desc,
-                              .fragment     = &fragment_state_desc,
-                              .depthStencil = &depth_stencil_state_desc,
-                              .multisample  = multisample_state_desc,
+                              .primitive    = primitive_state,
+                              .vertex       = vertex_state,
+                              .fragment     = &fragment_state,
+                              .depthStencil = &depth_stencil_state,
+                              .multisample  = multisample_state,
                             });
     ASSERT(pipelines.outline != NULL);
 
     // Shader modules are no longer needed once the graphics pipeline has been
     // created
-    WGPU_RELEASE_RESOURCE(ShaderModule, vertex_state_desc.module);
-    WGPU_RELEASE_RESOURCE(ShaderModule, fragment_state_desc.module);
+    WGPU_RELEASE_RESOURCE(ShaderModule, vertex_state.module);
+    WGPU_RELEASE_RESOURCE(ShaderModule, fragment_state.module);
   }
 }
 
