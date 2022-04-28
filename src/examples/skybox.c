485d365d1e373bc8f6bd997dd72f495d44e3ebb0
samdauwe <sam.dauwe@gmail.com>
2022-04-28 21:17:44 +0200

    Renamed render pipeline state objects

--- src/examples/skybox.c
+++ src/examples/skybox.c
@@ -199,15 +199,15 @@ static void prepare_uniform_buffers(wgpu_example_context_t* context)
 static void prepare_pipelines(wgpu_context_t* wgpu_context)
 {
   // Primitive state
-  WGPUPrimitiveState primitive_state_desc = {
+  WGPUPrimitiveState primitive_state = {
     .topology  = WGPUPrimitiveTopology_TriangleList,
     .frontFace = WGPUFrontFace_CCW,
     .cullMode  = WGPUCullMode_None,
   };
 
   // Color target state
-  WGPUBlendState blend_state                   = wgpu_create_blend_state(true);
-  WGPUColorTargetState color_target_state_desc = (WGPUColorTargetState){
+  WGPUBlendState blend_state              = wgpu_create_blend_state(true);
+  WGPUColorTargetState color_target_state = (WGPUColorTargetState){
     .format    = wgpu_context->swap_chain.format,
     .blend     = &blend_state,
     .writeMask = WGPUColorWriteMask_All,
@@ -217,7 +217,7 @@ static void prepare_pipelines(wgpu_context_t* wgpu_context)
   WGPUVertexBufferLayout skybox_vertex_buffer_layout = {0};
 
   // Vertex state
-  WGPUVertexState vertex_state_desc = wgpu_create_vertex_state(
+  WGPUVertexState vertex_state = wgpu_create_vertex_state(
                 wgpu_context, &(wgpu_vertex_state_t){
                 .shader_desc = (wgpu_shader_desc_t){
                   // Vertex shader SPIR-V
@@ -228,38 +228,38 @@ static void prepare_pipelines(wgpu_context_t* wgpu_context)
               });
 
   // Fragment state
-  WGPUFragmentState fragment_state_desc = wgpu_create_fragment_state(
+  WGPUFragmentState fragment_state = wgpu_create_fragment_state(
                 wgpu_context, &(wgpu_fragment_state_t){
                 .shader_desc = (wgpu_shader_desc_t){
                   // Fragment shader SPIR-V
                   .file = "shaders/skybox/shader.frag.spv",
                 },
                 .target_count = 1,
-                .targets      = &color_target_state_desc,
+                .targets      = &color_target_state,
               });
 
   // Multisample state
-  WGPUMultisampleState multisample_state_desc
+  WGPUMultisampleState multisample_state
     = wgpu_create_multisample_state_descriptor(
       &(create_multisample_state_desc_t){
         .sample_count = 1,
       });
 
   // Create rendering pipeline using the specified states
-  pipeline = wgpuDeviceCreateRenderPipeline(
-    wgpu_context->device, &(WGPURenderPipelineDescriptor){
-                            .label       = "skybox_render_pipeline",
-                            .layout      = pipeline_layout,
-                            .primitive   = primitive_state_desc,
-                            .vertex      = vertex_state_desc,
-                            .fragment    = &fragment_state_desc,
-                            .multisample = multisample_state_desc,
-                          });
+  pipeline = wgpuDeviceCreateRenderPipeline(wgpu_context->device,
+                                            &(WGPURenderPipelineDescriptor){
+                                              .label = "skybox_render_pipeline",
+                                              .layout      = pipeline_layout,
+                                              .primitive   = primitive_state,
+                                              .vertex      = vertex_state,
+                                              .fragment    = &fragment_state,
+                                              .multisample = multisample_state,
+                                            });
   ASSERT(pipeline != NULL);
 
   // Partial cleanup
-  WGPU_RELEASE_RESOURCE(ShaderModule, vertex_state_desc.module);
-  WGPU_RELEASE_RESOURCE(ShaderModule, fragment_state_desc.module);
+  WGPU_RELEASE_RESOURCE(ShaderModule, vertex_state.module);
+  WGPU_RELEASE_RESOURCE(ShaderModule, fragment_state.module);
 }
 
 static int example_initialize(wgpu_example_context_t* context)
