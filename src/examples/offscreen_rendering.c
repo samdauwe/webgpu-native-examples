02380265c82757600306f08b16f10d3704235e17
samdauwe <sam.dauwe@gmail.com>
2022-05-09 21:10:12 +0200

    Applied formatting

--- src/examples/offscreen_rendering.c
+++ src/examples/offscreen_rendering.c
@@ -253,10 +253,10 @@ static void setup_pipeline_layout(wgpu_context_t* wgpu_context)
   WGPUBindGroupLayoutEntry bgl_entries[3] = {
     [0] = (WGPUBindGroupLayoutEntry) {
       // Binding 0: Vertex shader uniform buffer
-      .binding = 0,
+      .binding    = 0,
       .visibility = WGPUShaderStage_Vertex,
       .buffer = (WGPUBufferBindingLayout) {
-        .type = WGPUBufferBindingType_Uniform,
+        .type             = WGPUBufferBindingType_Uniform,
         .hasDynamicOffset = false,
         .minBindingSize   = sizeof(ubo_shared_vs),
       },
