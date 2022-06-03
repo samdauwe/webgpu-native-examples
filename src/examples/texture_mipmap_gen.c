c5258dac6b691c93e09d89c8aafcaad3fb01b762
samdauwe <sam.dauwe@gmail.com>
2022-06-03 07:10:17 +0200

    Added labels for shader modules

--- src/examples/texture_mipmap_gen.c
+++ src/examples/texture_mipmap_gen.c
@@ -439,7 +439,8 @@ static void prepare_pipelines(wgpu_context_t* wgpu_context)
             wgpu_context, &(wgpu_vertex_state_t){
             .shader_desc = (wgpu_shader_desc_t){
               // Vertex shader SPIR-V
-              .file = "shaders/texture_mipmap_gen/texture.vert.spv",
+              .label = "texture_vertex_shader",
+              .file  = "shaders/texture_mipmap_gen/texture.vert.spv",
             },
             .buffer_count = 1,
             .buffers      = &tunnel_cylinder_vertex_buffer_layout,
@@ -450,7 +451,8 @@ static void prepare_pipelines(wgpu_context_t* wgpu_context)
             wgpu_context, &(wgpu_fragment_state_t){
             .shader_desc = (wgpu_shader_desc_t){
               // Fragment shader SPIR-V
-              .file = "shaders/texture_mipmap_gen/texture.frag.spv",
+              .label = "texture_fragment_shader",
+              .file  = "shaders/texture_mipmap_gen/texture.frag.spv",
             },
             .target_count = 1,
             .targets      = &color_target_state,
