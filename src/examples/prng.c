2716f89ba8290e8f25c316aff976f05e4be10707
samdauwe <sam.dauwe@gmail.com>
2022-06-16 21:04:04 +0200

    Applied formatting

--- src/examples/prng.c
+++ src/examples/prng.c
@@ -128,7 +128,7 @@ static void setup_pipeline_layout(wgpu_context_t* wgpu_context)
       .entryCount = 1,
       .entries = &(WGPUBindGroupLayoutEntry) {
         // Binding 0: Uniform buffer (Fragment shader)
-        .binding = 0,
+        .binding    = 0,
         .visibility = WGPUShaderStage_Fragment,
         .buffer = (WGPUBufferBindingLayout){
           .type             = WGPUBufferBindingType_Uniform,
