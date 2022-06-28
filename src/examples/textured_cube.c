8c175a1222caffbbb4c7f3185fd1d7ed62090642
samdauwe <sam.dauwe@gmail.com>
2022-06-28 21:55:05 +0200

    Using CODE macro for vertex and fragment shader

--- src/examples/textured_cube.c
+++ src/examples/textured_cube.c
@@ -19,15 +19,16 @@
 
 // Shaders
 // clang-format off
-static const char* sampled_texture_mix_color_fragment_shader_wgsl =
-  "@group(0) @binding(1) var mySampler: sampler;\n"
-  "@group(0) @binding(2) var myTexture: texture_2d<f32>;\n"
-  "\n"
-  "@stage(fragment)\n"
-  "fn main(@location(0) fragUV: vec2<f32>,\n"
-  "        @location(1) fragPosition: vec4<f32>) -> @location(0) vec4<f32> {\n"
-  "  return textureSample(myTexture, mySampler, fragUV) * fragPosition;\n"
-  "}";
+static const char* sampled_texture_mix_color_fragment_shader_wgsl = CODE(
+  @group(0) @binding(1) var mySampler: sampler;
+  @group(0) @binding(2) var myTexture: texture_2d<f32>;
+
+  @stage(fragment)
+  fn main(@location(0) fragUV: vec2<f32>,
+          @location(1) fragPosition: vec4<f32>) -> @location(0) vec4<f32> {
+    return textureSample(myTexture, mySampler, fragUV) * fragPosition;
+  };
+);
 // clang-format on
 
 // Cube mesh
