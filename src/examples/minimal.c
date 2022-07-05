43d3192504785bb78fec879bcc06d65917d55f54
samdauwe <sam.dauwe@gmail.com>
2022-07-05 21:26:23 +0200

    Using CODE macro for vertex and fragment shader code

--- src/examples/minimal.c
+++ src/examples/minimal.c
@@ -15,39 +15,41 @@
 
 // Shaders
 // clang-format off
-static const char* vertex_shader_wgsl =
-  "struct VSOut {\n"
-  "  @builtin(position) pos: vec4<f32>;\n"
-  "  @location(0) coord: vec2<f32>;\n"
-  "};\n"
-  "\n"
-  "@stage(vertex)\n"
-  "fn main(@builtin(vertex_index) idx : u32) -> VSOut {\n"
-  "  var data = array<vec2<f32>, 6>(\n"
-  "    vec2<f32>(-1.0, -1.0),\n"
-  "    vec2<f32>(1.0, -1.0),\n"
-  "    vec2<f32>(1.0, 1.0),\n"
-  "\n"
-  "    vec2<f32>(-1.0, -1.0),\n"
-  "    vec2<f32>(-1.0, 1.0),\n"
-  "    vec2<f32>(1.0, 1.0),\n"
-  "  );\n"
-  "\n"
-  "  let pos = data[idx];\n"
-  "\n"
-  "  var out : VSOut;\n"
-  "  out.pos = vec4<f32>(pos, 0.0, 1.0);\n"
-  "  out.coord.x = (pos.x + 1.0) / 2.0;\n"
-  "  out.coord.y = (1.0 - pos.y) / 2.0;\n"
-  "\n"
-  "  return out;\n"
-  "}";
+static const char* vertex_shader_wgsl = CODE(
+  struct VSOut {
+    @builtin(position) pos: vec4<f32>;
+    @location(0) coord: vec2<f32>;
+  };
 
-static const char* fragment_shader_wgsl =
-  "@stage(fragment)\n"
-  "fn main(@location(0) coord: vec2<f32>) -> @location(0) vec4<f32> {\n"
-  "  return vec4<f32>(coord.x, coord.y, 0.5, 1.0);\n"
-  "};";
+  @stage(vertex)
+  fn main(@builtin(vertex_index) idx : u32) -> VSOut {
+    var data = array<vec2<f32>, 6>(
+      vec2<f32>(-1.0, -1.0),
+      vec2<f32>(1.0, -1.0),
+      vec2<f32>(1.0, 1.0),
+
+      vec2<f32>(-1.0, -1.0),
+      vec2<f32>(-1.0, 1.0),
+      vec2<f32>(1.0, 1.0),
+    );
+
+    let pos = data[idx];
+
+    var out : VSOut;
+    out.pos = vec4<f32>(pos, 0.0, 1.0);
+    out.coord.x = (pos.x + 1.0) / 2.0;
+    out.coord.y = (1.0 - pos.y) / 2.0;
+
+    return out;
+  }
+);
+
+static const char* fragment_shader_wgsl = CODE(
+  @stage(fragment)
+  fn main(@location(0) coord: vec2<f32>) -> @location(0) vec4<f32> {
+    return vec4<f32>(coord.x, coord.y, 0.5, 1.0);
+  }
+);
 // clang-format on
 
 static WGPURenderPipeline pipeline;
