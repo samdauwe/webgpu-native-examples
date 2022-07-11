91684acb8c5a224469568775660ad3af35eb4493
samdauwe <sam.dauwe@gmail.com>
2022-07-11 20:34:55 +0200

    Using CODE macro for vertex and fragment shader code Updated WGSL code to latest spec

--- src/examples/common_shaders.h
+++ src/examples/common_shaders.h
@@ -29,50 +29,56 @@ static const char* basic_vertex_shader_wgsl = CODE(
   }
 );
 
-static const char* vertex_position_color_fragment_shader_wgsl =
-  "@stage(fragment)\n"
-  "fn main(@location(0) fragUV: vec2<f32>,\n"
-  "        @location(1) fragPosition: vec4<f32>) -> @location(0) vec4<f32> {\n"
-  "  return fragPosition;\n"
-  "}";
+static const char* vertex_position_color_fragment_shader_wgsl = CODE(
+  @fragment
+  fn main(
+    @location(0) fragUV: vec2<f32>,
+    @location(1) fragPosition: vec4<f32>
+  ) -> @location(0) vec4<f32> {
+    return fragPosition;
+  }
+);
 
-static const char* fullscreen_textured_quad_wgsl =
-  "@group(0) @binding(0) var mySampler : sampler;\n"
-  "@group(0) @binding(1) var myTexture : texture_2d<f32>;\n"
-  "\n"
-  "struct VertexOutput {\n"
-  "  @builtin(position) Position : vec4<f32>,\n"
-  "  @location(0) fragUV : vec2<f32>\n"
-  "};\n"
-  "\n"
-  "@stage(vertex)\n"
-  "fn vert_main(@builtin(vertex_index) VertexIndex : u32) -> VertexOutput {\n"
-  "  var pos = array<vec2<f32>, 6>(\n"
-  "      vec2<f32>( 1.0,  1.0),\n"
-  "      vec2<f32>( 1.0, -1.0),\n"
-  "      vec2<f32>(-1.0, -1.0),\n"
-  "      vec2<f32>( 1.0,  1.0),\n"
-  "      vec2<f32>(-1.0, -1.0),\n"
-  "      vec2<f32>(-1.0,  1.0));\n"
-  "\n"
-  "  var uv = array<vec2<f32>, 6>(\n"
-  "      vec2<f32>(1.0, 0.0),\n"
-  "      vec2<f32>(1.0, 1.0),\n"
-  "      vec2<f32>(0.0, 1.0),\n"
-  "      vec2<f32>(1.0, 0.0),\n"
-  "      vec2<f32>(0.0, 1.0),\n"
-  "      vec2<f32>(0.0, 0.0));\n"
-  "\n"
-  "  var output : VertexOutput;\n"
-  "  output.Position = vec4<f32>(pos[VertexIndex], 0.0, 1.0);\n"
-  "  output.fragUV = uv[VertexIndex];\n"
-  "  return output;\n"
-  "}\n"
-  "\n"
-  "@stage(fragment)\n"
-  "fn frag_main(@location(0) fragUV : vec2<f32>) -> @location(0) vec4<f32> {\n"
-  "  return textureSample(myTexture, mySampler, fragUV);\n"
-  "}";
+static const char* fullscreen_textured_quad_wgsl = CODE(
+  @group(0) @binding(0) var mySampler : sampler;
+  @group(0) @binding(1) var myTexture : texture_2d<f32>;
+
+  struct VertexOutput {
+    @builtin(position) Position : vec4<f32>,
+    @location(0) fragUV : vec2<f32>,
+  }
+
+  @vertex
+  fn vert_main(@builtin(vertex_index) VertexIndex : u32) -> VertexOutput {
+    var pos = array<vec2<f32>, 6>(
+      vec2<f32>( 1.0,  1.0),
+      vec2<f32>( 1.0, -1.0),
+      vec2<f32>(-1.0, -1.0),
+      vec2<f32>( 1.0,  1.0),
+      vec2<f32>(-1.0, -1.0),
+      vec2<f32>(-1.0,  1.0)
+    );
+
+    var uv = array<vec2<f32>, 6>(
+      vec2<f32>(1.0, 0.0),
+      vec2<f32>(1.0, 1.0),
+      vec2<f32>(0.0, 1.0),
+      vec2<f32>(1.0, 0.0),
+      vec2<f32>(0.0, 1.0),
+      vec2<f32>(0.0, 0.0)
+    );
+
+    var output : VertexOutput;
+    output.Position = vec4<f32>(pos[VertexIndex], 0.0, 1.0);
+    output.fragUV = uv[VertexIndex];
+    return output;
+  }
+
+  @fragment
+  fn frag_main(@location(0) fragUV : vec2<f32>) -> @location(0) vec4<f32> {
+    return textureSample(myTexture, mySampler, fragUV);
+  }
+);
 // clang-format on
 
 #endif /* COMMON_SHADERS_H */
