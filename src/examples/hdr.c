358a47e0faa86665acdda28aad769012b1c01677
samdauwe <sam.dauwe@gmail.com>
2022-07-21 21:33:39 +0200

    Added labels for pipeline layouts

--- src/examples/hdr.c
+++ src/examples/hdr.c
@@ -516,6 +516,7 @@ static void setup_pipeline_layout(wgpu_context_t* wgpu_context)
       pipeline_layouts.bloom_filter = wgpuDeviceCreatePipelineLayout(
         wgpu_context->device,
         &(WGPUPipelineLayoutDescriptor){
+          .label                = "Bloom filter pipeline layout",
           .bindGroupLayoutCount = 1,
           .bindGroupLayouts     = &bind_group_layouts.bloom_filter,
         });
@@ -536,6 +537,7 @@ static void setup_pipeline_layout(wgpu_context_t* wgpu_context)
       pipeline_layouts.composition = wgpuDeviceCreatePipelineLayout(
         wgpu_context->device,
         &(WGPUPipelineLayoutDescriptor){
+          .label                = "G-Buffer composition pipeline layout",
           .bindGroupLayoutCount = 1,
           .bindGroupLayouts     = &bind_group_layouts.composition,
         });
