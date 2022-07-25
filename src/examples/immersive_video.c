4e9255a1a3740a8d9a452d30b75234fc401bbfbb
samdauwe <sam.dauwe@gmail.com>
2022-07-25 21:50:29 +0200

    Added bind group layout label

--- src/examples/immersive_video.c
+++ src/examples/immersive_video.c
@@ -168,6 +168,7 @@ static void setup_pipeline_layout(wgpu_context_t* wgpu_context)
   };
   bind_group_layout = wgpuDeviceCreateBindGroupLayout(
     wgpu_context->device, &(WGPUBindGroupLayoutDescriptor){
+                            .label = "Immersive video render pipeline layout",
                             .entryCount = (uint32_t)ARRAY_SIZE(bgl_entries),
                             .entries    = bgl_entries,
                           });
