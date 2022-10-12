7b0cc4c063c6467961686fe8f221d321d3eedbfc
samdauwe <sam.dauwe@gmail.com>
2022-10-12 20:03:59 +0200

    Added init_pressure_program function

--- src/examples/fluid_simulation.c
+++ src/examples/fluid_simulation.c
@@ -592,6 +592,23 @@ static void init_gradient_subtract_program(program_t* this,
                settings.grid_w, settings.grid_h);
 }
 
+static void init_pressure_program(program_t* this, wgpu_context_t* wgpu_context)
+{
+  dynamic_buffer_t* program_buffers[3] = {
+    &dynamic_buffers.pressure,   /* in_pressure */
+    &dynamic_buffers.divergence, /* in_divergence */
+    &dynamic_buffers.pressure0,  /* out_pressure */
+  };
+  uniform_t* program_uniforms[1] = {
+    &uniforms.grid, /* */
+  };
+  const char* shader_wgsl_path = "pressure_shader.wgsl";
+  program_init(this, wgpu_context, program_buffers,
+               (uint32_t)ARRAY_SIZE(program_buffers), program_uniforms,
+               (uint32_t)ARRAY_SIZE(program_uniforms), shader_wgsl_path,
+               settings.grid_w, settings.grid_h);
+}
+
 static void init_update_program(program_t* this, wgpu_context_t* wgpu_context)
 {
   dynamic_buffer_t* program_buffers[2] = {
