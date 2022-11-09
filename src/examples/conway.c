aeaa719f7b5847887eaf7e2aa10d33e7468b652a
samdauwe <sam.dauwe@gmail.com>
2022-11-09 21:21:46 +0100

    Added bind group layout label

--- src/examples/conway.c
+++ src/examples/conway.c
@@ -406,6 +406,7 @@ static void setup_bind_groups(wgpu_context_t* wgpu_context)
     compute.bind_groups[i] = wgpuDeviceCreateBindGroup(
       wgpu_context->device,
       &(WGPUBindGroupDescriptor){
+        .label  = "Compute bind group layout",
         .layout = wgpuComputePipelineGetBindGroupLayout(compute.pipeline, 0),
         .entryCount = (uint32_t)ARRAY_SIZE(bg_entries),
         .entries    = bg_entries,
