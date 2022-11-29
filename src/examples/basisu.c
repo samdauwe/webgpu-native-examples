73925b5a05a1ca0436335df037b25557ebe5f79e
samdauwe <sam.dauwe@gmail.com>
2022-11-29 21:47:00 +0100

    Added vertex and fragment shader labels

--- src/examples/basisu.c
+++ src/examples/basisu.c
@@ -315,7 +315,8 @@ static void prepare_pipelines(wgpu_context_t* wgpu_context)
                 wgpu_context, &(wgpu_vertex_state_t){
                 .shader_desc = (wgpu_shader_desc_t){
                   /* Vertex shader SPIR-V */
-                  .file = "shaders/basisu/texture.vert.spv",
+                  .label = "Vertex shader",
+                  .file  = "shaders/basisu/texture.vert.spv",
                 },
                 .buffer_count = 1,
                 .buffers      = &quad_vertex_buffer_layout,
@@ -326,7 +327,8 @@ static void prepare_pipelines(wgpu_context_t* wgpu_context)
                 wgpu_context, &(wgpu_fragment_state_t){
                 .shader_desc = (wgpu_shader_desc_t){
                   /* Fragment shader SPIR-V */
-                  .file = "shaders/basisu/texture.frag.spv",
+                  .label = "Fragment shader",
+                  .file  = "shaders/basisu/texture.frag.spv",
                 },
                 .target_count = 1,
                 .targets      = &color_target_state_desc,
