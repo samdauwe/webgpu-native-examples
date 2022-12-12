9444fec8ca31fd384e73c2c94efa910a7ad3e8be
samdauwe <sam.dauwe@gmail.com>
2022-12-12 21:05:53 +0100

    Updated comment

--- src/examples/multi_sampling.c
+++ src/examples/multi_sampling.c
@@ -125,7 +125,7 @@ static void setup_render_pass(wgpu_context_t* wgpu_context)
 
   // Color attachment
   rp_color_att_descriptors[0] = (WGPURenderPassColorAttachment) {
-      .view          = NULL, // Assigned later
+      .view          = NULL, /* Assigned later */
       .resolveTarget = NULL,
       .loadOp        = WGPULoadOp_Clear,
       .storeOp       = WGPUStoreOp_Store,
