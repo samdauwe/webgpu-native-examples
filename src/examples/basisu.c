c572452fd0492043f2517e1187d99c8284652f21
samdauwe <sam.dauwe@gmail.com>
2022-12-17 20:12:05 +0100

    Updated formatting

--- src/examples/basisu.c
+++ src/examples/basisu.c
@@ -498,7 +498,7 @@ void example_basisu(int argc, char* argv[])
   // clang-format off
   example_run(argc, argv, &(refexport_t){
     .example_settings = (wgpu_example_settings_t){
-      .title = example_title,
+      .title   = example_title,
       .overlay = true,
     },
     .example_initialize_func      = &example_initialize,
