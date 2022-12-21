b48749d1e253ee2330331b1034f4ab97e2321567
samdauwe <sam.dauwe@gmail.com>
2022-12-21 19:31:02 +0100

    Updated comment

--- src/examples/instanced_cube.c
+++ src/examples/instanced_cube.c
@@ -110,7 +110,7 @@ static void setup_render_pass(wgpu_context_t* wgpu_context)
 {
   // Color attachment
   render_pass.color_attachments[0] = (WGPURenderPassColorAttachment) {
-      .view       = NULL, /* view is acquired in render loop. */
+      .view       = NULL, /* View is acquired in render loop. */
       .loadOp     = WGPULoadOp_Clear,
       .storeOp    = WGPUStoreOp_Store,
       .clearColor = (WGPUColor) {
