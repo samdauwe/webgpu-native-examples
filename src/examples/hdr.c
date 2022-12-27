32724a461c240876fe4683e9fc4ee47cae4812e7
samdauwe <sam.dauwe@gmail.com>
2022-12-27 20:38:44 +0100

    Updated comment

--- src/examples/hdr.c
+++ src/examples/hdr.c
@@ -185,7 +185,7 @@ static void load_assets(wgpu_context_t* wgpu_context)
   textures.envmap = wgpu_create_texture_cubemap_from_files(
     wgpu_context, cubemap,
     &(struct wgpu_texture_load_options_t){
-      .flip_y = true, // Flip y to match uffizi_cube_nz.ktx hdr cubemap
+      .flip_y = true, /* Flip y to match uffizi_cube_nz.ktx hdr cubemap */
     });
 }
 
