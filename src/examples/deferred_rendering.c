f011f6904723e7a76aa32b8e7304e5d8b4ad6e1f
samdauwe <sam.dauwe@gmail.com>
2023-01-08 19:44:28 +0100

    Added compute pipeline label

--- src/examples/deferred_rendering.c
+++ src/examples/deferred_rendering.c
@@ -989,6 +989,7 @@ static void prepare_light_update_compute_pipeline(wgpu_context_t* wgpu_context)
   light_update_compute_pipeline = wgpuDeviceCreateComputePipeline(
     wgpu_context->device,
     &(WGPUComputePipelineDescriptor){
+      .label   = "Light update compute pipeline",
       .layout  = light_update_compute_pipeline_layout,
       .compute = light_update_comp_shader.programmable_stage_descriptor,
     });
