3e03b5a80f497b08ca6b3f97a4104201eed68b14
samdauwe <sam.dauwe@gmail.com>
2023-01-25 20:49:15 +0100

    Updated render pipeline label

--- src/examples/compute_boids.c
+++ src/examples/compute_boids.c
@@ -373,7 +373,7 @@ static void prepare_pipelines(wgpu_context_t* wgpu_context)
   // Create rendering pipeline using the specified states
   render_pipeline = wgpuDeviceCreateRenderPipeline(
     wgpu_context->device, &(WGPURenderPipelineDescriptor){
-                            .label       = "boids_render_pipeline",
+                            .label       = "Boids render pipeline",
                             .layout      = render_pipeline_layout,
                             .primitive   = primitive_state,
                             .vertex      = vertex_state,
