dca49a9c7166627a441f13c155a61b30ea891d0f
samdauwe <sam.dauwe@gmail.com>
2023-01-27 21:49:30 +0100

    Updated shader comment

--- src/examples/cubemap.c
+++ src/examples/cubemap.c
@@ -25,9 +25,9 @@ static const char* sample_cubemap_fragment_shader_wgsl = CODE(
     @location(0) fragUV: vec2<f32>,
     @location(1) fragPosition: vec4<f32>
   ) -> @location(0) vec4<f32> {
-    // Our camera and the skybox cube are both centered at (0, 0, 0)
-    // so we can use the cube geomtry position to get viewing vector to sample the cube texture.
-    // The magnitude of the vector doesn't matter.
+    // Our camera and the skybox cube are both centered at (0, 0, 0) so we can
+    // use the cube geomtry position to get viewing vector to sample the cube
+    // texture. The magnitude of the vector doesn't matter.
     var cubemapVec = fragPosition.xyz - vec3(0.5);
     return textureSample(myTexture, mySampler, cubemapVec);
   }
