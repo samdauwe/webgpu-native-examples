2b3062a4ca1da25e5ee84c2cbe2a27abc23c8d64
samdauwe <sam.dauwe@gmail.com>
2023-02-05 19:51:54 +0100

    Updated render pipeline label

--- src/examples/parallax_mapping.c
+++ src/examples/parallax_mapping.c
@@ -339,7 +339,7 @@ static void prepare_pipelines(wgpu_context_t* wgpu_context)
   // Create rendering pipeline using the specified states
   pipeline = wgpuDeviceCreateRenderPipeline(
     wgpu_context->device, &(WGPURenderPipelineDescriptor){
-                            .label        = "parallax_render_pipeline",
+                            .label        = "Parallax render pipeline",
                             .layout       = pipeline_layout,
                             .primitive    = primitive_state,
                             .vertex       = vertex_state,
