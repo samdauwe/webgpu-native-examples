73b1e06b088db93123aabd5b3bc7e3ebc6833380
samdauwe <sam.dauwe@gmail.com>
2023-02-06 20:43:36 +0100

    Using define for exit code

--- src/examples/coordinate_system.c
+++ src/examples/coordinate_system.c
@@ -452,7 +452,7 @@ static int example_draw(wgpu_example_context_t* context)
   // Submit frame
   submit_frame(context);
 
-  return 0;
+  return EXIT_SUCCESS;
 }
 
 static int example_initialize(wgpu_example_context_t* context)
@@ -464,16 +464,16 @@ static int example_initialize(wgpu_example_context_t* context)
     prepare_pipelines(context->wgpu_context);
     setup_render_pass(context->wgpu_context);
     prepared = true;
-    return 0;
+    return EXIT_SUCCESS;
   }
 
-  return 1;
+  return EXIT_FAILURE;
 }
 
 static int example_render(wgpu_example_context_t* context)
 {
   if (!prepared) {
-    return 1;
+    return EXIT_FAILURE;
   }
   return example_draw(context);
 }
