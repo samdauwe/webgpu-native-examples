93f22fe8955d9ed5a14078cab842fb2f7cbc3784
samdauwe <sam.dauwe@gmail.com>
2023-02-12 21:39:07 +0100

    Added bind group label

--- src/examples/screenshot.c
+++ src/examples/screenshot.c
@@ -305,6 +305,7 @@ static void setup_bind_groups(wgpu_context_t* wgpu_context)
 
   bind_group = wgpuDeviceCreateBindGroup(
     wgpu_context->device, &(WGPUBindGroupDescriptor){
+                            .label      = "Bind group",
                             .layout     = bind_group_layout,
                             .entryCount = (uint32_t)ARRAY_SIZE(bg_entries),
                             .entries    = bg_entries,
