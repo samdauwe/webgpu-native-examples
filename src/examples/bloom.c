a6aa5085a7fbba51a09d3b359f99a7039b24ae91
samdauwe <sam.dauwe@gmail.com>
2023-03-05 19:13:27 +0100

    Updated blur_horz pipeline label

--- src/examples/bloom.c
+++ src/examples/bloom.c
@@ -707,7 +707,7 @@ static void prepare_pipelines(wgpu_context_t* wgpu_context)
       // Create rendering pipeline using the specified states
       pipelines.blur_horz = wgpuDeviceCreateRenderPipeline(
         wgpu_context->device, &(WGPURenderPipelineDescriptor){
-                                .label     = "horizontal_blur_render_pipeline",
+                                .label     = "Horizontal blur render pipeline",
                                 .layout    = pipeline_layouts.blur,
                                 .primitive = primitive_state,
                                 .vertex    = vertex_state,
