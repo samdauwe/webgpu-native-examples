3e5e6ee7e1817cdf6ffd368f81fe71f1cca4e403
samdauwe <sam.dauwe@gmail.com>
2023-03-15 20:51:19 +0100

    Updated vertex and fragment shader labels

--- src/examples/equirectangular_image.c
+++ src/examples/equirectangular_image.c
@@ -279,7 +279,7 @@ static void prepare_pipelines(wgpu_context_t* wgpu_context)
                     wgpu_context, &(wgpu_vertex_state_t){
                     .shader_desc = (wgpu_shader_desc_t){
                       // Vertex shader SPIR-V
-                      .label = "vertex_shader",
+                      .label = "Vertex shader",
                       .file  = "shaders/equirectangular_image/main.vert.spv",
                     },
                     .buffer_count = 0,
@@ -291,7 +291,7 @@ static void prepare_pipelines(wgpu_context_t* wgpu_context)
                     wgpu_context, &(wgpu_fragment_state_t){
                     .shader_desc = (wgpu_shader_desc_t){
                       // Fragment shader SPIR-V
-                      .label = "fragment_shader",
+                      .label = "Fragment shader",
                       .file  = "shaders/equirectangular_image/main.frag.spv",
                     },
                     .target_count = 1,
