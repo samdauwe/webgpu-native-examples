8c70e5c0ce76a25c671f8cc7e1aeb128586139f0
samdauwe <sam.dauwe@gmail.com>
2023-03-21 18:40:19 +0100

    Enabled vsync

--- src/examples/cubemap.c
+++ src/examples/cubemap.c
@@ -511,6 +511,7 @@ void example_cubemap(int argc, char* argv[])
     .example_settings = (wgpu_example_settings_t){
      .title   = example_title,
      .overlay = true,
+     .vsync   = true,
     },
     .example_initialize_func = &example_initialize,
     .example_render_func     = &example_render,
