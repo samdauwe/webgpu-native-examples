bb8346073b2605c3a1c5921f9f286204020d80ef
samdauwe <sam.dauwe@gmail.com>
2023-03-27 21:19:47 +0200

    Enabled stencil buffer example

--- src/examples/examples.c
+++ src/examples/examples.c
@@ -124,7 +124,7 @@ static examplecase_t g_example_cases[] = {
   {"shadow_mapping", example_shadow_mapping},
   {"skybox", example_skybox},
   {"square", example_square},
-  // {"stencil_buffer", example_stencil_buffer},
+  {"stencil_buffer", example_stencil_buffer},
   {"terrain_mesh", example_terrain_mesh},
   {"text_overlay", example_text_overlay},
   {"texture_3d", example_texture_3d},
