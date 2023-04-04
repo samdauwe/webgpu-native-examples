5dd19b8cee8871792142181a43e225d391e661ff
samdauwe <sam.dauwe@gmail.com>
2023-04-04 21:16:38 +0200

    Updated vertex shader label

--- src/examples/coordinate_system.c
+++ src/examples/coordinate_system.c
@@ -310,7 +310,7 @@ static void prepare_pipelines(wgpu_context_t* wgpu_context)
                     wgpu_context, &(wgpu_vertex_state_t){
                     .shader_desc = (wgpu_shader_desc_t){
                       // Vertex shader SPIR-V
-                      .label = "Quad vertex shader",
+                      .label = "Quad vertex shader SPIR-V",
                       .file = "shaders/coordinate_system/quad.vert.spv",
                     },
                     .buffer_count = 1,
