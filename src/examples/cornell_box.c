a22376e53eaca4d9862b56d060ad2d41e7a409e8
samdauwe <sam.dauwe@gmail.com>
2023-04-09 11:22:53 +0200

    Added tonemapper_t struct to  convert a linear-light framebuffer to a gamma-correct

--- src/examples/cornell_box.c
+++ src/examples/cornell_box.c
@@ -137,130 +137,141 @@ static void radiosity_create(radiosity_t* this, wgpu_context_t* wgpu_context,
   this->scene        = scene;
 
   /* Lightmap */
-  WGPUTextureDescriptor texture_desc = {
-    .label         = "Radiosity.lightmap",
-    .size          = (WGPUExtent3D) {
-      .width              = this->lightmap_width,
-      .height             = this->lightmap_height,
-      .depthOrArrayLayers = this->scene->quads.length,
-    },
-    .mipLevelCount = 1,
-    .sampleCount   = 1,
-    .dimension     = WGPUTextureDimension_2D,
-    .format        = this->lightmap_format,
-    .usage
-    = WGPUTextureUsage_TextureBinding | WGPUTextureUsage_StorageBinding,
-  };
-  this->lightmap.texture
-    = wgpuDeviceCreateTexture(this->wgpu_context->device, &texture_desc);
-  ASSERT(this->lightmap.texture != NULL);
-  this->lightmap_depth_or_array_layers = this->scene->quads.length;
+  {
+    // Texture
+    WGPUTextureDescriptor texture_desc = {
+      .label         = "Radiosity.lightmap",
+      .size          = (WGPUExtent3D) {
+        .width              = this->lightmap_width,
+        .height             = this->lightmap_height,
+        .depthOrArrayLayers = this->scene->quads.length,
+      },
+      .mipLevelCount = 1,
+      .sampleCount   = 1,
+      .dimension     = WGPUTextureDimension_2D,
+      .format        = this->lightmap_format,
+      .usage
+      = WGPUTextureUsage_TextureBinding | WGPUTextureUsage_StorageBinding,
+    };
+    this->lightmap.texture
+      = wgpuDeviceCreateTexture(this->wgpu_context->device, &texture_desc);
+    ASSERT(this->lightmap.texture != NULL);
+    this->lightmap_depth_or_array_layers = this->scene->quads.length;
 
-  // Create the texture view
-  WGPUTextureViewDescriptor texture_view_dec = {
-    .dimension       = WGPUTextureViewDimension_2D,
-    .format          = texture_desc.format,
-    .baseMipLevel    = 0,
-    .mipLevelCount   = 1,
-    .baseArrayLayer  = 0,
-    .arrayLayerCount = 1,
-    .aspect          = WGPUTextureAspect_All,
-  };
-  this->lightmap.view
-    = wgpuTextureCreateView(this->lightmap.texture, &texture_view_dec);
-  ASSERT(this->lightmap.view != NULL);
+    // Texture view
+    WGPUTextureViewDescriptor texture_view_dec = {
+      .dimension       = WGPUTextureViewDimension_2D,
+      .format          = texture_desc.format,
+      .baseMipLevel    = 0,
+      .mipLevelCount   = 1,
+      .baseArrayLayer  = 0,
+      .arrayLayerCount = 1,
+      .aspect          = WGPUTextureAspect_All,
+    };
+    this->lightmap.view
+      = wgpuTextureCreateView(this->lightmap.texture, &texture_view_dec);
+    ASSERT(this->lightmap.view != NULL);
+  }
 
   /* Accumulation buffer */
-  this->accumulation_buffer = wgpu_create_buffer(
-    this->wgpu_context, &(wgpu_buffer_desc_t){
-                          .label = "Radiosity.accumulationBuffer",
-                          .size  = this->lightmap_width * this->lightmap_height
-                                  * this->scene->quads.length * 16,
-                          .usage = WGPUBufferUsage_Storage,
-                        });
-  this->total_lightmap_texels
-    = this->lightmap_width * this->lightmap_height * this->scene->quads.length;
+  {
+    this->accumulation_buffer = wgpu_create_buffer(
+      this->wgpu_context, &(wgpu_buffer_desc_t){
+                            .label = "Radiosity.accumulationBuffer",
+                            .size = this->lightmap_width * this->lightmap_height
+                                    * this->scene->quads.length * 16,
+                            .usage = WGPUBufferUsage_Storage,
+                          });
+    this->total_lightmap_texels = this->lightmap_width * this->lightmap_height
+                                  * this->scene->quads.length;
+  }
 
   /* Uniform buffer */
-  this->accumulation_buffer = wgpu_create_buffer(
-    this->wgpu_context,
-    &(wgpu_buffer_desc_t){
-      .label = "Radiosity.uniformBuffer",
-      .size  = 8 * 4, /* 8 x f32 */
-      .usage = WGPUBufferUsage_Uniform | WGPUBufferUsage_CopyDst,
-    });
+  {
+    this->accumulation_buffer = wgpu_create_buffer(
+      this->wgpu_context,
+      &(wgpu_buffer_desc_t){
+        .label = "Radiosity.uniformBuffer",
+        .size  = 8 * 4, /* 8 x f32 */
+        .usage = WGPUBufferUsage_Uniform | WGPUBufferUsage_CopyDst,
+      });
+  }
 
-  /* Bindgroup layout */
-  WGPUBindGroupLayoutEntry bgl_entries[3] = {
-    [0] = (WGPUBindGroupLayoutEntry) {
-      // Binding 0: accumulation buffer
-      .binding    = 0,
-      .visibility = WGPUShaderStage_Compute,
-      .buffer = (WGPUBufferBindingLayout) {
-        .type           = WGPUBufferBindingType_Storage,
-        .minBindingSize = this->accumulation_buffer.size,
+  /* Bind group layout */
+  {
+    WGPUBindGroupLayoutEntry bgl_entries[3] = {
+      [0] = (WGPUBindGroupLayoutEntry) {
+        // Binding 0: accumulation buffer
+        .binding    = 0,
+        .visibility = WGPUShaderStage_Compute,
+        .buffer = (WGPUBufferBindingLayout) {
+          .type           = WGPUBufferBindingType_Storage,
+          .minBindingSize = this->accumulation_buffer.size,
+        },
+        .sampler = {0},
       },
-      .sampler = {0},
-    },
-    [1] = (WGPUBindGroupLayoutEntry) {
-      // Binding 1: lightmap
-      .binding    = 1,
-      .visibility = WGPUShaderStage_Compute,
-      .storageTexture = {
-        .access = WGPUStorageTextureAccess_WriteOnly,
-        .format = this->lightmap_format,
-        .viewDimension = WGPUTextureViewDimension_2DArray,
+      [1] = (WGPUBindGroupLayoutEntry) {
+        // Binding 1: lightmap
+        .binding    = 1,
+        .visibility = WGPUShaderStage_Compute,
+        .storageTexture = {
+          .access = WGPUStorageTextureAccess_WriteOnly,
+          .format = this->lightmap_format,
+          .viewDimension = WGPUTextureViewDimension_2DArray,
+        },
       },
-    },
-    [2] = (WGPUBindGroupLayoutEntry) {
-      // Binding 2: radiosity_uniforms
-      .binding    = 2,
-      .visibility = WGPUShaderStage_Compute,
-      .buffer = (WGPUBufferBindingLayout) {
-        .type           = WGPUBufferBindingType_Uniform,
-        .minBindingSize = this->uniform_buffer.size,
-      },
-      .sampler = {0},
-    }
-  };
-  this->bind_group_layout = wgpuDeviceCreateBindGroupLayout(
-    wgpu_context->device, &(WGPUBindGroupLayoutDescriptor){
-                            .label      = "Radiosity.bindGroupLayout",
-                            .entryCount = (uint32_t)ARRAY_SIZE(bgl_entries),
-                            .entries    = bgl_entries,
-                          });
-  ASSERT(this->bind_group_layout != NULL);
+      [2] = (WGPUBindGroupLayoutEntry) {
+        // Binding 2: radiosity_uniforms
+        .binding    = 2,
+        .visibility = WGPUShaderStage_Compute,
+        .buffer = (WGPUBufferBindingLayout) {
+          .type           = WGPUBufferBindingType_Uniform,
+          .minBindingSize = this->uniform_buffer.size,
+        },
+        .sampler = {0},
+      }
+    };
+    this->bind_group_layout = wgpuDeviceCreateBindGroupLayout(
+      wgpu_context->device, &(WGPUBindGroupLayoutDescriptor){
+                              .label      = "Radiosity.bindGroupLayout",
+                              .entryCount = (uint32_t)ARRAY_SIZE(bgl_entries),
+                              .entries    = bgl_entries,
+                            });
+    ASSERT(this->bind_group_layout != NULL);
+  }
 
-  /* Bindgroup layout */
-  WGPUBindGroupEntry bg_entries[3] = {
-    [0] = (WGPUBindGroupEntry) {
-      // Binding 0: accumulation buffer
-      .binding = 0,
-      .buffer  = this->accumulation_buffer.buffer,
-      .offset  = 0,
-      .size    = this->accumulation_buffer.size,
-    },
-    [1] = (WGPUBindGroupEntry) {
-      // Binding 1: lightmap
-      .binding     = 1,
-      .textureView = this->lightmap.view,
-    },
-    [2] = (WGPUBindGroupEntry) {
-      // Binding 2: radiosity_uniforms
-      .binding = 2,
-      .buffer  = this->uniform_buffer.buffer,
-      .offset  = 0,
-      .size    = this->uniform_buffer.size,
-    },
-  };
-  this->bind_group = wgpuDeviceCreateBindGroup(
-    wgpu_context->device, &(WGPUBindGroupDescriptor){
-                            .label      = "Radiosity.bindGroup",
-                            .layout     = this->bind_group_layout,
-                            .entryCount = (uint32_t)ARRAY_SIZE(bg_entries),
-                            .entries    = bg_entries,
-                          });
-  ASSERT(this->bind_group != NULL);
+  /* Bind group */
+  {
+    WGPUBindGroupEntry bg_entries[3] = {
+      [0] = (WGPUBindGroupEntry) {
+        // Binding 0: accumulation buffer
+        .binding = 0,
+        .buffer  = this->accumulation_buffer.buffer,
+        .offset  = 0,
+        .size    = this->accumulation_buffer.size,
+      },
+      [1] = (WGPUBindGroupEntry) {
+        // Binding 1: lightmap
+        .binding     = 1,
+        .textureView = this->lightmap.view,
+      },
+      [2] = (WGPUBindGroupEntry) {
+        // Binding 2: radiosity_uniforms
+        .binding = 2,
+        .buffer  = this->uniform_buffer.buffer,
+        .offset  = 0,
+        .size    = this->uniform_buffer.size,
+      },
+    };
+    this->bind_group = wgpuDeviceCreateBindGroup(
+      wgpu_context->device, &(WGPUBindGroupDescriptor){
+                              .label      = "Radiosity.bindGroup",
+                              .layout     = this->bind_group_layout,
+                              .entryCount = (uint32_t)ARRAY_SIZE(bg_entries),
+                              .entries    = bg_entries,
+                            });
+    ASSERT(this->bind_group != NULL);
+  }
 
   /* Compute pipeline layout */
   {
@@ -420,4 +431,176 @@ static void radiosity_run(radiosity_t* this,
     ceil(this->lightmap_height
          / (float)this->accumulation_to_lightmap_workgroup_size_y),
     this->lightmap_depth_or_array_layers);
+}
+
+/* -------------------------------------------------------------------------- *
+ * Tonemapper implements a tonemapper to convert a linear-light framebuffer to
+ * a gamma-correct, tonemapped framebuffer used for presentation.
+ * -------------------------------------------------------------------------- */
+
+typedef struct {
+  wgpu_context_t* wgpu_context;
+  common_t* common;
+  WGPUBindGroupLayout bind_group_layout;
+  WGPUBindGroup bind_group;
+  WGPUPipelineLayout pipeline_layout;
+  WGPUComputePipeline pipeline;
+  float width;
+  float height;
+  uint32_t workgroup_size_x;
+  uint32_t workgroup_size_y;
+} tonemapper_t;
+
+static void tonemapper_init_defaults(tonemapper_t* this)
+{
+  memset(this, 0, sizeof(*this));
+
+  this->workgroup_size_x = 16;
+  this->workgroup_size_y = 16;
+}
+
+static void tonemapper_create(tonemapper_t* this, wgpu_context_t* wgpu_context,
+                              common_t* common, texture_t* input,
+                              texture_t* output)
+{
+  tonemapper_init_defaults(this);
+
+  this->wgpu_context = wgpu_context;
+  this->common       = common;
+  this->width        = input->size.width;
+  this->height       = input->size.height;
+
+  /* Bind group layout */
+  {
+    WGPUBindGroupLayoutEntry bgl_entries[2] = {
+      [0] = (WGPUBindGroupLayoutEntry) {
+        // Binding 0: input
+        .binding    = 0,
+        .visibility = WGPUShaderStage_Compute,
+        .texture = (WGPUTextureBindingLayout) {
+          .sampleType    = WGPUTextureSampleType_Float,
+          .viewDimension = WGPUTextureViewDimension_2D,
+          .multisampled  = false,
+        },
+        .storageTexture = {0},
+      },
+      [1] = (WGPUBindGroupLayoutEntry) {
+        // Binding 1: output
+        .binding    = 1,
+        .visibility = WGPUShaderStage_Compute,
+        .storageTexture = {
+          .access = WGPUStorageTextureAccess_WriteOnly,
+          .format = output->format,
+          .viewDimension = WGPUTextureViewDimension_2D,
+        },
+      },
+    };
+    this->bind_group_layout = wgpuDeviceCreateBindGroupLayout(
+      wgpu_context->device, &(WGPUBindGroupLayoutDescriptor){
+                              .label      = "Tonemapper.bindGroupLayout",
+                              .entryCount = (uint32_t)ARRAY_SIZE(bgl_entries),
+                              .entries    = bgl_entries,
+                            });
+    ASSERT(this->bind_group_layout != NULL);
+  }
+
+  /* Bind group */
+  {
+    WGPUBindGroupEntry bg_entries[2] = {
+      [0] = (WGPUBindGroupEntry) {
+        // Binding 0: input
+        .binding     = 0,
+        .textureView = input->view,
+      },
+      [1] = (WGPUBindGroupEntry) {
+        // Binding 1: output
+        .binding     = 1,
+        .textureView = output->view,
+      },
+    };
+    this->bind_group = wgpuDeviceCreateBindGroup(
+      wgpu_context->device, &(WGPUBindGroupDescriptor){
+                              .label      = "Tonemapper.bindGroup",
+                              .layout     = this->bind_group_layout,
+                              .entryCount = (uint32_t)ARRAY_SIZE(bg_entries),
+                              .entries    = bg_entries,
+                            });
+    ASSERT(this->bind_group != NULL);
+  }
+
+  /* Compute pipeline layout */
+  {
+    WGPUBindGroupLayout bind_group_layouts[1] = {
+      this->bind_group_layout, /* Group 0 */
+    };
+    WGPUPipelineLayoutDescriptor compute_pipeline_layout_desc = {
+      .label                = "Tonemap.pipelineLayout",
+      .bindGroupLayoutCount = (uint32_t)ARRAY_SIZE(bind_group_layouts),
+      .bindGroupLayouts     = bind_group_layouts,
+    };
+    this->pipeline_layout = wgpuDeviceCreatePipelineLayout(
+      wgpu_context->device, &compute_pipeline_layout_desc);
+    ASSERT(this->pipeline_layout != NULL);
+  }
+
+  /* Tonemap compute pipeline */
+  {
+    /* Constants */
+    WGPUConstantEntry constant_entries[2] = {
+      [0] = (WGPUConstantEntry) {
+        .key   = "WorkgroupSizeX",
+        .value = this->workgroup_size_x,
+      },
+      [1] = (WGPUConstantEntry) {
+        .key   = "WorkgroupSizeY",
+        .value = this->workgroup_size_y,
+      },
+    };
+
+    /* Compute shader */
+    wgpu_shader_t tonemapper_comp_shader = wgpu_shader_create(
+      wgpu_context, &(wgpu_shader_desc_t){
+                      // Compute shader WGSL
+                      .label           = "tonemapper_comp_shader",
+                      .file            = "shaders/cornell_box/tonemapper.wgsl",
+                      .entry           = "main",
+                      .constants.count = (uint32_t)ARRAY_SIZE(constant_entries),
+                      .constants.entries = constant_entries,
+                    });
+
+    /* Compute pipeline*/
+    this->pipeline = wgpuDeviceCreateComputePipeline(
+      wgpu_context->device,
+      &(WGPUComputePipelineDescriptor){
+        .label   = "Tonemap.pipeline",
+        .layout  = this->pipeline_layout,
+        .compute = tonemapper_comp_shader.programmable_stage_descriptor,
+      });
+
+    /* Cleanup */
+    wgpu_shader_release(&tonemapper_comp_shader);
+  }
+}
+
+static void tonemapper_destroy(tonemapper_t* this)
+{
+  WGPU_RELEASE_RESOURCE(PipelineLayout, this->pipeline_layout)
+  WGPU_RELEASE_RESOURCE(ComputePipeline, this->pipeline)
+  WGPU_RELEASE_RESOURCE(BindGroupLayout, this->bind_group_layout)
+  WGPU_RELEASE_RESOURCE(BindGroup, this->bind_group)
+}
+
+static void tonemapper_run(tonemapper_t* this,
+                           WGPUCommandEncoder command_encoder)
+{
+  WGPUComputePassEncoder pass_encoder
+    = wgpuCommandEncoderBeginComputePass(command_encoder, NULL);
+  wgpuComputePassEncoderSetBindGroup(pass_encoder, 0, this->bind_group, 0,
+                                     NULL);
+  wgpuComputePassEncoderSetPipeline(pass_encoder, this->pipeline);
+  wgpuComputePassEncoderDispatchWorkgroups(
+    pass_encoder, ceil(this->width / (float)this->workgroup_size_x),
+    ceil(this->height / (float)this->workgroup_size_y), 1);
+  wgpuComputePassEncoderEnd(pass_encoder);
+  WGPU_RELEASE_RESOURCE(ComputePassEncoder, pass_encoder)
 }
