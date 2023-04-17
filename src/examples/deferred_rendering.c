8229a8a2a3ecb4ab5c2f8dfb5c37ccba022461f0
samdauwe <sam.dauwe@gmail.com>
2023-04-17 21:36:15 +0200

    Updated pipeline label

--- src/examples/deferred_rendering.c
+++ src/examples/deferred_rendering.c
@@ -731,7 +731,7 @@ static void prepare_deferred_render_pipeline(wgpu_context_t* wgpu_context)
   // Create rendering pipeline using the specified states
   deferred_render_pipeline = wgpuDeviceCreateRenderPipeline(
     wgpu_context->device, &(WGPURenderPipelineDescriptor){
-                            .label       = "deferred_render_pipeline",
+                            .label       = "Deferred render pipeline",
                             .layout      = deferred_render_pipeline_layout,
                             .primitive   = primitive_state,
                             .vertex      = vertex_state,
