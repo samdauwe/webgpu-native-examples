c3ca6030a534ae591084db204374c72f0cbd2b13
samdauwe <sam.dauwe@gmail.com>
2023-04-25 21:02:38 +0200

    Updated graphics pipeline label

--- src/examples/compute_particles.c
+++ src/examples/compute_particles.c
@@ -318,7 +318,7 @@ static void prepare_pipelines(wgpu_context_t* wgpu_context)
   // Create rendering pipeline using the specified states
   graphics.pipeline = wgpuDeviceCreateRenderPipeline(
     wgpu_context->device, &(WGPURenderPipelineDescriptor){
-                            .label        = "particle_render_pipeline",
+                            .label        = "Particle render pipeline",
                             .layout       = graphics.pipeline_layout,
                             .primitive    = primitive_state,
                             .vertex       = vertex_state,
