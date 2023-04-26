c8dc0f92e8f45a969aa5dd8fe60692b2f950d447
samdauwe <sam.dauwe@gmail.com>
2023-04-26 20:46:39 +0200

    Fixed comment issue

--- src/examples/gerstner_waves.c
+++ src/examples/gerstner_waves.c
@@ -737,7 +737,7 @@ static WGPUCommandBuffer build_command_buffer(wgpu_context_t* wgpu_context)
   wgpu_context->rpass_enc = wgpuCommandEncoderBeginRenderPass(
     wgpu_context->cmd_enc, &render_pass.descriptor);
 
-  // Record record render pass
+  // Record render pass
   wgpuRenderPassEncoderSetPipeline(wgpu_context->rpass_enc, pipeline);
   wgpuRenderPassEncoderSetVertexBuffer(wgpu_context->rpass_enc, 0,
                                        vertices.buffer, 0, WGPU_WHOLE_SIZE);
