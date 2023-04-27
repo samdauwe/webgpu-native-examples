ab0188733e053a56db28c2c87a1a3e9d330cb021
samdauwe <sam.dauwe@gmail.com>
2023-04-27 21:46:50 +0200

    Using define for exit code

--- src/examples/imgui_overlay.c
+++ src/examples/imgui_overlay.c
@@ -140,7 +140,7 @@ static int example_draw(wgpu_example_context_t* context)
 static int example_render(wgpu_example_context_t* context)
 {
   if (!prepared) {
-    return 1;
+    return EXIT_FAILURE;
   }
   return example_draw(context);
 }
