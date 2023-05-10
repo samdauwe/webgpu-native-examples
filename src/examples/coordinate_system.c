622884e93ff3a4e2dd5ad90b3d150fb303ef4c8c
samdauwe <sam.dauwe@gmail.com>
2023-05-10 21:17:17 +0200

    Added assert for pipeline check

--- src/examples/coordinate_system.c
+++ src/examples/coordinate_system.c
@@ -347,6 +347,7 @@ static void prepare_pipelines(wgpu_context_t* wgpu_context)
                             .depthStencil = &depth_stencil_state,
                             .multisample  = multisample_state,
                           });
+  ASSERT(pipeline != NULL);
 
   // Partial cleanup
   WGPU_RELEASE_RESOURCE(ShaderModule, vertex_state.module);
