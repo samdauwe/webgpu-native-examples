bce5ab50f6985ea7a13a7b69356512504efa8022
samdauwe <sam.dauwe@gmail.com>
2023-06-05 20:54:44 +0200

    Updated formatting

--- src/examples/sampler_parameters.c
+++ src/examples/sampler_parameters.c
@@ -696,17 +696,17 @@ void example_sampler_parameters(int argc, char* argv[])
 {
   // clang-format off
   example_run(argc, argv, &(refexport_t){
-    .example_settings = (wgpu_example_settings_t){
-      .title   = example_title,
-      .overlay = true,
-  },
-  .example_window_config = (window_config_t){
-    .width  = CANVAS_SIZE,
-    .height = CANVAS_SIZE,
-  },
-  .example_initialize_func = &example_initialize,
-  .example_render_func     = &example_render,
-  .example_destroy_func    = &example_destroy
+      .example_settings = (wgpu_example_settings_t){
+        .title   = example_title,
+        .overlay = true,
+    },
+    .example_window_config = (window_config_t){
+      .width  = CANVAS_SIZE,
+      .height = CANVAS_SIZE,
+    },
+    .example_initialize_func = &example_initialize,
+    .example_render_func     = &example_render,
+    .example_destroy_func    = &example_destroy
   });
   // clang-format on
 }
