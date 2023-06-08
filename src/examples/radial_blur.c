0c657ebb85627a7e761eb19e93a618bf25feec4f
samdauwe <sam.dauwe@gmail.com>
2023-06-08 20:16:22 +0200

    Added texture label

--- src/examples/radial_blur.c
+++ src/examples/radial_blur.c
@@ -143,6 +143,7 @@ static void prepare_offscreen(wgpu_context_t* wgpu_context)
   // Color attachment
   {
     WGPUTextureDescriptor texture_desc = {
+      .label         = "Offscreen texture",
       .size          = texture_extent,
       .mipLevelCount = 1,
       .sampleCount   = 1,
