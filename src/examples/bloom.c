d59cf5fcbde2efa2e2d4d6d30dc52d08cbe36104
samdauwe <sam.dauwe@gmail.com>
2023-06-10 21:15:22 +0200

    Updated offscreen frame buffer color texture view label

--- src/examples/bloom.c
+++ src/examples/bloom.c
@@ -195,7 +195,7 @@ static void prepare_offscreen_frame_buffer(wgpu_context_t* wgpu_context,
 
     // Create the texture view
     WGPUTextureViewDescriptor texture_view_dec = {
-      .label           = "offscreen_frame_buffer_color_texture_view",
+      .label           = "Offscreen frame buffer color texture view",
       .dimension       = WGPUTextureViewDimension_2D,
       .format          = texture_desc.format,
       .baseMipLevel    = 0,
