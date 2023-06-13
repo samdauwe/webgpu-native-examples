b99b6bd074abf09c04d5c51f6382487968acc8be
samdauwe <sam.dauwe@gmail.com>
2023-06-13 21:27:50 +0200

    Added GBuffer texture view label

--- src/examples/deferred_rendering.c
+++ src/examples/deferred_rendering.c
@@ -244,7 +244,8 @@ static void prepare_gbuffer_texture_render_targets(wgpu_context_t* wgpu_context)
 
   {
     WGPUTextureDescriptor texture_desc = {
-      .size          = (WGPUExtent3D) {
+      .label = "GBuffer texture view",
+      .size  = (WGPUExtent3D) {
         .width               = wgpu_context->surface.width,
         .height              = wgpu_context->surface.height,
         .depthOrArrayLayers  = 2,
