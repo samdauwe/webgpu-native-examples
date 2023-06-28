c949500a4fd11749713c6d1740f59f9a32138387
samdauwe <sam.dauwe@gmail.com>
2023-06-28 20:51:00 +0200

    Added skybox bind group label

--- src/examples/pbr_ibl.c
+++ src/examples/pbr_ibl.c
@@ -534,6 +534,7 @@ static void setup_bind_groups(wgpu_context_t* wgpu_context)
     };
 
     WGPUBindGroupDescriptor bg_desc = {
+      .label      = "Skybox bind group",
       .layout     = bind_group_layouts.skybox,
       .entryCount = (uint32_t)ARRAY_SIZE(bg_entries),
       .entries    = bg_entries,
