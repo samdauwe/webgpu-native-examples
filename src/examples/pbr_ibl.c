87557851fc1ef18ffd0aaead89510ced3fb2476b
samdauwe <sam.dauwe@gmail.com>
2023-06-30 20:39:19 +0200

    Updated skybox vertex shader label

--- src/examples/pbr_ibl.c
+++ src/examples/pbr_ibl.c
@@ -625,7 +625,7 @@ static void prepare_pipelines(wgpu_context_t* wgpu_context)
             wgpu_context, &(wgpu_vertex_state_t){
             .shader_desc = (wgpu_shader_desc_t){
               // Vertex shader SPIR-V
-              .label = "skybox_vertex_shader",
+              .label = "Skybox vertex shader",
               .file  = "shaders/pbr_ibl/skybox.vert.spv",
             },
             .buffer_count = 1,
