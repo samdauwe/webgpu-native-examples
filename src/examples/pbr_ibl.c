1127655c8caca77c5d81954707343c57e8e2d69f
samdauwe <sam.dauwe@gmail.com>
2023-06-30 20:41:30 +0200

    Updated PBR IBL fragment shader label

--- src/examples/pbr_ibl.c
+++ src/examples/pbr_ibl.c
@@ -686,7 +686,7 @@ static void prepare_pipelines(wgpu_context_t* wgpu_context)
             wgpu_context, &(wgpu_fragment_state_t){
             .shader_desc = (wgpu_shader_desc_t){
               // Fragment shader SPIR-V
-              .label = "pbribl_fragment_shader",
+              .label = "PBR IBL fragment shader",
               .file  = "shaders/pbr_ibl/pbribl.frag.spv",
             },
             .target_count = 1,
