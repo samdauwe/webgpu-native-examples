c2fbc1f1062237ee655cd717e6939dd606880dfc
samdauwe <sam.dauwe@gmail.com>
2023-07-07 21:08:54 +0200

    Added skybox vertex shader uniform buffer label

--- src/examples/pbr_ibl.c
+++ src/examples/pbr_ibl.c
@@ -1971,6 +1971,7 @@ static void prepare_uniform_buffers(wgpu_example_context_t* context)
   uniform_buffers.skybox = wgpu_create_buffer(
     context->wgpu_context,
     &(wgpu_buffer_desc_t){
+      .label = "Skybox vertex shader uniform buffer",
       .usage = WGPUBufferUsage_CopyDst | WGPUBufferUsage_Uniform,
       .size  = sizeof(ubo_matrices),
     });
