85bb7ef0eb24a5e1a32529f5bf6afed581b367c1
samdauwe <sam.dauwe@gmail.com>
2023-07-10 21:02:43 +0200

    Enabled vsync

--- src/examples/pbr_basic.c
+++ src/examples/pbr_basic.c
@@ -649,6 +649,7 @@ void example_pbr_basic(int argc, char* argv[])
     .example_settings = (wgpu_example_settings_t){
       .title   = example_title,
       .overlay = true,
+      .vsync   = true,
     },
     .example_initialize_func      = &example_initialize,
     .example_render_func          = &example_render,
