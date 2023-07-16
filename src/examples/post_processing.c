9f6ed5c76df04a6717148458e8066ab3a673c26d
samdauwe <sam.dauwe@gmail.com>
2023-07-16 20:57:24 +0200

    Added fullscreen quad transform uniform block label

--- src/examples/post_processing.c
+++ src/examples/post_processing.c
@@ -1183,6 +1183,7 @@ static void prepare_uniform_buffers(wgpu_context_t* wgpu_context)
   transform_init(&quad_transform);
   uniform_buffers.quad_transform = wgpu_create_buffer(
     wgpu_context, &(wgpu_buffer_desc_t){
+                    .label = "Fullscreen quad transform uniform block",
                     .usage = WGPUBufferUsage_CopyDst | WGPUBufferUsage_Uniform,
                     .size  = 16 * sizeof(float),
                   });
