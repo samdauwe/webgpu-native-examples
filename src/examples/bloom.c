efe7b9365066706588f6119a4700e1cdd065f0e9
samdauwe <sam.dauwe@gmail.com>
2023-07-22 21:11:52 +0200

    Updated gaussian blur vertex shader label

--- src/examples/bloom.c
+++ src/examples/bloom.c
@@ -650,7 +650,7 @@ static void prepare_pipelines(wgpu_context_t* wgpu_context)
                 wgpu_context, &(wgpu_vertex_state_t){
                 .shader_desc = (wgpu_shader_desc_t){
                   // Vertex shader SPIR-V
-                  .label = "Gaussian blur vertex shader",
+                  .label = "Vertical gaussian blur vertex shader",
                   .file  = "shaders/bloom/gaussblur.vert.spv",
                 },
                 // Empty vertex input state
