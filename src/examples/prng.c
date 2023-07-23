7ab67251685b13d68fa129ad6d1da91fed968316
samdauwe <sam.dauwe@gmail.com>
2023-07-23 20:55:42 +0200

    Updated commenting style

--- src/examples/prng.c
+++ src/examples/prng.c
@@ -323,7 +323,7 @@ static void example_on_update_ui_overlay(wgpu_example_context_t* context)
 
 static WGPUCommandBuffer build_command_buffer(wgpu_context_t* wgpu_context)
 {
-  // Set target frame buffer
+  /* Set target frame buffer */
   rp_color_att_descriptors[0].view = wgpu_context->swap_chain.frame_buffer;
 
   wgpu_context->cmd_enc
@@ -339,14 +339,14 @@ static WGPUCommandBuffer build_command_buffer(wgpu_context_t* wgpu_context)
                                     uniform_bind_group, 0, 0);
   wgpuRenderPassEncoderDraw(wgpu_context->rpass_enc, vertices.count, 1, 0, 0);
 
-  // End render pass
+  /* End render pass */
   wgpuRenderPassEncoderEnd(wgpu_context->rpass_enc);
   WGPU_RELEASE_RESOURCE(RenderPassEncoder, wgpu_context->rpass_enc)
 
-  // Draw ui overlay
+  // Draw ui overlay */
   draw_ui(wgpu_context->context, example_on_update_ui_overlay);
 
-  // Get command buffer
+  /* Get command buffer */
   WGPUCommandBuffer command_buffer
     = wgpu_get_command_buffer(wgpu_context->cmd_enc);
   WGPU_RELEASE_RESOURCE(CommandEncoder, wgpu_context->cmd_enc)
