81b516b24bc58385790e791b429fc3c20cffe0b2
samdauwe <sam.dauwe@gmail.com>
2023-07-29 20:51:24 +0200

    Added vertex shader uniform buffer label

--- src/examples/parallax_mapping.c
+++ src/examples/parallax_mapping.c
@@ -390,6 +390,7 @@ static void prepare_uniform_buffers(wgpu_example_context_t* context)
   uniform_buffers.vertex_shader = wgpu_create_buffer(
     context->wgpu_context,
     &(wgpu_buffer_desc_t){
+      .label = "Vertex shader uniform buffer",
       .usage = WGPUBufferUsage_CopyDst | WGPUBufferUsage_Uniform,
       .size  = sizeof(ubos.vertex_shader),
     });
