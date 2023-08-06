5a2b9440ae3964e126c00f0732d56315800e2585
samdauwe <sam.dauwe@gmail.com>
2023-08-06 20:05:43 +0200

    Made identation consistent

--- src/examples/procedural_mesh.c
+++ src/examples/procedural_mesh.c
@@ -79,10 +79,10 @@ static const char* fragment_shader_wgsl = CODE(
 
   // Trowbridge-Reitz GGX normal distribution function.
   fn distributionGgx(n: vec3<f32>, h: vec3<f32>, alpha: f32) -> f32 {
-      let alpha_sq = alpha * alpha;
-      let n_dot_h = saturate(dot(n, h));
-      let k = n_dot_h * n_dot_h * (alpha_sq - 1.0) + 1.0;
-      return alpha_sq / (pi * k * k);
+    let alpha_sq = alpha * alpha;
+    let n_dot_h = saturate(dot(n, h));
+    let k = n_dot_h * n_dot_h * (alpha_sq - 1.0) + 1.0;
+    return alpha_sq / (pi * k * k);
   }
 
   fn geometrySchlickGgx(x: f32, k: f32) -> f32 {
@@ -90,9 +90,9 @@ static const char* fragment_shader_wgsl = CODE(
   }
 
   fn geometrySmith(n: vec3<f32>, v: vec3<f32>, l: vec3<f32>, k: f32) -> f32 {
-      let n_dot_v = saturate(dot(n, v));
-      let n_dot_l = saturate(dot(n, l));
-      return geometrySchlickGgx(n_dot_v, k) * geometrySchlickGgx(n_dot_l, k);
+    let n_dot_v = saturate(dot(n, v));
+    let n_dot_l = saturate(dot(n, l));
+    return geometrySchlickGgx(n_dot_v, k) * geometrySchlickGgx(n_dot_l, k);
   }
 
   fn fresnelSchlick(h_dot_v: f32, f0: vec3<f32>) -> vec3<f32> {
@@ -121,43 +121,43 @@ static const char* fragment_shader_wgsl = CODE(
     f0 = mix(f0, base_color, metallic);
 
     let light_positions = array<vec3<f32>, 4>(
-        vec3(25.0, 15.0, 25.0),
-        vec3(-25.0, 15.0, 25.0),
-        vec3(25.0, 15.0, -25.0),
-        vec3(-25.0, 15.0, -25.0),
+      vec3(25.0, 15.0, 25.0),
+      vec3(-25.0, 15.0, 25.0),
+      vec3(25.0, 15.0, -25.0),
+      vec3(-25.0, 15.0, -25.0),
     );
     let light_radiance = array<vec3<f32>, 4>(
-        4.0 * vec3(0.0, 100.0, 250.0),
-        8.0 * vec3(200.0, 150.0, 250.0),
-        3.0 * vec3(200.0, 0.0, 0.0),
-        9.0 * vec3(200.0, 150.0, 0.0),
+      4.0 * vec3(0.0, 100.0, 250.0),
+      8.0 * vec3(200.0, 150.0, 250.0),
+      3.0 * vec3(200.0, 0.0, 0.0),
+      9.0 * vec3(200.0, 150.0, 0.0),
     );
 
     var lo = vec3(0.0);
     for (var light_index: i32 = 0; light_index < 4; light_index = light_index + 1) {
-        let lvec = light_positions[light_index] - position;
+      let lvec = light_positions[light_index] - position;
 
-        let l = normalize(lvec);
-        let h = normalize(l + v);
+      let l = normalize(lvec);
+      let h = normalize(l + v);
 
-        let distance_sq = dot(lvec, lvec);
-        let attenuation = 1.0 / distance_sq;
-        let radiance = light_radiance[light_index] * attenuation;
+      let distance_sq = dot(lvec, lvec);
+      let attenuation = 1.0 / distance_sq;
+      let radiance = light_radiance[light_index] * attenuation;
 
-        let f = fresnelSchlick(saturate(dot(h, v)), f0);
+      let f = fresnelSchlick(saturate(dot(h, v)), f0);
 
-        let ndf = distributionGgx(n, h, alpha);
-        let g = geometrySmith(n, v, l, k);
+      let ndf = distributionGgx(n, h, alpha);
+      let g = geometrySmith(n, v, l, k);
 
-        let numerator = ndf * g * f;
-        let denominator = 4.0 * saturate(dot(n, v)) * saturate(dot(n, l));
-        let specular = numerator / max(denominator, 0.001);
+      let numerator = ndf * g * f;
+      let denominator = 4.0 * saturate(dot(n, v)) * saturate(dot(n, l));
+      let specular = numerator / max(denominator, 0.001);
 
-        let ks = f;
-        let kd = (vec3(1.0) - ks) * (1.0 - metallic);
+      let ks = f;
+      let kd = (vec3(1.0) - ks) * (1.0 - metallic);
 
-        let n_dot_l = saturate(dot(n, l));
-        lo = lo + (kd * base_color / pi + specular) * radiance * n_dot_l;
+      let n_dot_l = saturate(dot(n, l));
+      lo = lo + (kd * base_color / pi + specular) * radiance * n_dot_l;
     }
 
     let ambient = vec3(0.03) * base_color * ao;
