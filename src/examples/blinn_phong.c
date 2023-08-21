51fe01cfa23deaf2588718aa5b04beeed49fbde2
samdauwe <sam.dauwe@gmail.com>
2023-08-21 21:22:38 +0200

    Added Blinn-Phong Lighting vertex shader

--- src/examples/blinn_phong.c
+++ src/examples/blinn_phong.c
@@ -319,6 +319,11 @@ static WGPUTextureView create_texture_view(WGPUTexture texture,
 }
 
 /* -------------------------------------------------------------------------- *
+ * WGSL Shaders
+ * -------------------------------------------------------------------------- */
+static const char* blinn_phong_lighting_vertex_shader_wgsl;
+
+/* -------------------------------------------------------------------------- *
  * Blinn-Phong Lighting example
  * -------------------------------------------------------------------------- */
 
@@ -408,3 +413,35 @@ static iPipeline_t prepare_render_pipelines(iweb_gpu_init_t* init,
     .depth_texture_views = {depth_texture_view},
   };
 }
+
+/* -------------------------------------------------------------------------- *
+ * WGSL Shaders
+ * -------------------------------------------------------------------------- */
+
+// clang-format off
+static const char* blinn_phong_lighting_vertex_shader_wgsl = CODE(
+  // vertex shader
+  struct Uniforms {
+    viewProjectMat : mat4x4f,
+    modelMat : mat4x4f,
+    normalMat : mat4x4f,
+  };
+  @binding(0) @group(0) var<uniform> uniforms : Uniforms;
+
+  struct Output {
+    @builtin(position) position : vec4f,
+    @location(0) vPosition : vec4f,
+    @location(1) vNormal : vec4f,
+  };
+
+  @vertex
+  fn vs_main(@location(0) pos: vec3f, @location(1) normal: vec3f) -> Output {
+    var output: Output;
+    let mPosition = uniforms.modelMat * vec4(pos, 1.0);
+    output.vPosition = mPosition;
+    output.vNormal =  uniforms.normalMat * vec4(normal, 1.0);
+    output.position = uniforms.viewProjectMat * mPosition;
+    return output;
+  }
+);
+// clang-format on
