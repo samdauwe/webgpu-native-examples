5a1bf36dcbcb439ce1380b15bceddc75bfc59504
samdauwe <sam.dauwe@gmail.com>
2023-08-21 21:24:08 +0200

    Added Blinn-Phong Lighting fragment shader

--- src/examples/blinn_phong.c
+++ src/examples/blinn_phong.c
@@ -322,6 +322,7 @@ static WGPUTextureView create_texture_view(WGPUTexture texture,
  * WGSL Shaders
  * -------------------------------------------------------------------------- */
 static const char* blinn_phong_lighting_vertex_shader_wgsl;
+static const char* blinn_phong_lighting_fragment_shader_wgsl;
 
 /* -------------------------------------------------------------------------- *
  * Blinn-Phong Lighting example
@@ -442,6 +443,46 @@ static const char* blinn_phong_lighting_vertex_shader_wgsl = CODE(
     output.vNormal =  uniforms.normalMat * vec4(normal, 1.0);
     output.position = uniforms.viewProjectMat * mPosition;
     return output;
+  }
+);
+
+static const char* blinn_phong_lighting_fragment_shader_wgsl = CODE(
+  // fragment shader
+  struct LightUniforms {
+    lightPosition: vec4f,
+    eyePosition: vec4f,
+    color: vec4f,
+    specularColor: vec4f,
+  };
+  @group(1) @binding(0) var<uniform> light : LightUniforms;
+
+  struct MaterialUniforms {
+    ambient: f32,
+    diffuse: f32,
+    specular: f32,
+    shininess: f32,
+  };
+  @group(1) @binding(1) var<uniform> material : MaterialUniforms;
+
+  fn blinnPhong(N:vec3f, L:vec3f, V:vec3f) -> vec2f{
+    let H = normalize(L + V);
+    var diffuse = material.diffuse * max(dot(N, L), 0.0);
+    diffuse += material.diffuse * max(dot(-N, L), 0.0);
+    var specular = material.specular * pow(max(dot(N, H), 0.0), material.shininess);
+    specular += material.specular * pow(max(dot(-N, H),0.0), material.shininess);
+    return vec2(diffuse, specular);
+  }
+
+  @fragment
+  fn fs_main(@location(0) vPosition:vec4f, @location(1) vNormal:vec4f) ->  @location(0) vec4f {
+    var N = normalize(vNormal.xyz);
+    let L = normalize(light.lightPosition.xyz - vPosition.xyz);
+    let V = normalize(light.eyePosition.xyz - vPosition.xyz);
+
+    let bp = blinnPhong(N, L, V);
+
+    let finalColor = light.color*(material.ambient + bp[0]) + light.specularColor * bp[1];
+    return vec4(finalColor.rgb, 1.0);
   }
 );
 // clang-format on
