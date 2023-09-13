5ec9c5483f112fdfa9a600eec1a90990977b7e2b
samdauwe <sam.dauwe@gmail.com>
2023-09-13 21:25:21 +0200

    Embedded WGSL code in the 'sampler_parameters' example

--- src/examples/sampler_parameters.c
+++ src/examples/sampler_parameters.c
@@ -16,6 +16,17 @@
  * https://github.com/webgpu/webgpu-samples/tree/main/src/sample/samplerParameters
  * -------------------------------------------------------------------------- */
 
+/* -------------------------------------------------------------------------- *
+ * WGSL Shaders
+ * -------------------------------------------------------------------------- */
+
+static const char* show_texture_wgsl;
+static const char* textured_square_wgsl;
+
+/* -------------------------------------------------------------------------- *
+ * Sampler Parameters example
+ * -------------------------------------------------------------------------- */
+
 #define CANVAS_SIZE 600u
 
 static struct {
@@ -420,9 +431,9 @@ static void prepare_debug_view_render_pipeline(wgpu_context_t* wgpu_context)
       wgpu_context, &(wgpu_vertex_state_t){
     .shader_desc = (wgpu_shader_desc_t){
       // Vertex shader WGSL
-      .label = "Debug view vertex shader WGSL",
-      .file  = "shaders/sampler_parameters/showTexture.wgsl",
-      .entry = "vmain"
+      .label            = "Debug view vertex shader WGSL",
+      .wgsl_code.source = show_texture_wgsl,
+      .entry            = "vmain"
     },
   });
 
@@ -431,9 +442,9 @@ static void prepare_debug_view_render_pipeline(wgpu_context_t* wgpu_context)
     wgpu_context, &(wgpu_fragment_state_t){
     .shader_desc = (wgpu_shader_desc_t){
       // Fragment shader WGSL
-      .label = "Debug view fragment shader WGSL",
-      .file  = "shaders/sampler_parameters/showTexture.wgsl",
-      .entry = "fmain"
+      .label            = "Debug view fragment shader WGSL",
+      .wgsl_code.source = show_texture_wgsl,
+      .entry            = "fmain"
     },
     .target_count = 1,
     .targets = &color_target_state,
@@ -517,9 +528,9 @@ prepare_textured_square_render_pipeline(wgpu_context_t* wgpu_context)
       wgpu_context, &(wgpu_vertex_state_t){
     .shader_desc = (wgpu_shader_desc_t){
       // Vertex shader WGSL
-      .label = "Textured square vertex shader WGSL",
-      .file  = "shaders/sampler_parameters/texturedSquare.wgsl",
-      .entry = "vmain"
+      .label            = "Textured square vertex shader WGSL",
+      .wgsl_code.source = textured_square_wgsl,
+      .entry            = "vmain"
     },
     .constant_count = (uint32_t)ARRAY_SIZE(constant_entries),
     .constants      = constant_entries,
@@ -530,9 +541,9 @@ prepare_textured_square_render_pipeline(wgpu_context_t* wgpu_context)
     wgpu_context, &(wgpu_fragment_state_t){
     .shader_desc = (wgpu_shader_desc_t){
       // Fragment shader WGSL
-      .label = "Textured square fragment shader WGSL",
-      .file  = "shaders/sampler_parameters/texturedSquare.wgsl",
-      .entry = "fmain"
+      .label            = "Textured square fragment shader WGSL",
+      .wgsl_code.source = textured_square_wgsl,
+      .entry            = "fmain"
     },
     .target_count   = 1,
     .targets        = &color_target_state,
@@ -717,3 +728,101 @@ void example_sampler_parameters(int argc, char* argv[])
   });
   // clang-format on
 }
+
+/* -------------------------------------------------------------------------- *
+ * WGSL Shaders
+ * -------------------------------------------------------------------------- */
+
+// clang-format off
+static const char* show_texture_wgsl = CODE(
+  @group(0) @binding(0) var tex: texture_2d<f32>;
+
+  struct Varying {
+    @builtin(position) pos: vec4f,
+    @location(0) texelCoord: vec2f,
+    @location(1) mipLevel: f32,
+  }
+
+  const kMipLevels = 4;
+  const baseMipSize: u32 = 16;
+
+  @vertex
+  fn vmain(
+    @builtin(instance_index) instance_index: u32, // used as mipLevel
+    @builtin(vertex_index) vertex_index: u32,
+  ) -> Varying {
+    var square = array(
+      vec2f(0, 0), vec2f(0, 1), vec2f(1, 0),
+      vec2f(1, 0), vec2f(0, 1), vec2f(1, 1),
+    );
+    let uv = square[vertex_index];
+    let pos = vec4(uv * 2 - vec2(1, 1), 0.0, 1.0);
+
+    let mipLevel = instance_index;
+    let mipSize = f32(1 << (kMipLevels - mipLevel));
+    let texelCoord = uv * mipSize;
+    return Varying(pos, texelCoord, f32(mipLevel));
+  }
+
+  @fragment
+  fn fmain(vary: Varying) -> @location(0) vec4f {
+    return textureLoad(tex, vec2u(vary.texelCoord), u32(vary.mipLevel));
+  }
+);
+
+static const char* textured_square_wgsl = CODE(
+  struct Config {
+    viewProj: mat4x4f,
+    animationOffset: vec2f,
+    flangeSize: f32,
+    highlightFlange: f32,
+  };
+  @group(0) @binding(0) var<uniform> config: Config;
+  @group(0) @binding(1) var<storage, read> matrices: array<mat4x4f>;
+  @group(0) @binding(2) var samp: sampler;
+  @group(0) @binding(3) var tex: texture_2d<f32>;
+
+  struct Varying {
+    @builtin(position) pos: vec4f,
+    @location(0) uv: vec2f,
+  }
+
+  override kTextureBaseSize: f32;
+  override kViewportSize: f32;
+
+  @vertex
+  fn vmain(
+    @builtin(instance_index) instance_index: u32,
+    @builtin(vertex_index) vertex_index: u32,
+  ) -> Varying {
+    let flange = config.flangeSize;
+    var uvs = array(
+      vec2(-flange, -flange), vec2(-flange, 1 + flange), vec2(1 + flange, -flange),
+      vec2(1 + flange, -flange), vec2(-flange, 1 + flange), vec2(1 + flange, 1 + flange),
+    );
+    // Default size (if matrix is the identity) makes 1 texel = 1 pixel.
+    let radius = (1 + 2 * flange) * kTextureBaseSize / kViewportSize;
+    var positions = array(
+      vec2(-radius, -radius), vec2(-radius, radius), vec2(radius, -radius),
+      vec2(radius, -radius), vec2(-radius, radius), vec2(radius, radius),
+    );
+
+    let modelMatrix = matrices[instance_index];
+    let pos = config.viewProj * modelMatrix * vec4f(positions[vertex_index] + config.animationOffset, 0, 1);
+    return Varying(pos, uvs[vertex_index]);
+  }
+
+  @fragment
+  fn fmain(vary: Varying) -> @location(0) vec4f {
+    let uv = vary.uv;
+    var color = textureSample(tex, samp, uv);
+
+    let outOfBounds = uv.x < 0 || uv.x > 1 || uv.y < 0 || uv.y > 1;
+    if config.highlightFlange > 0 && outOfBounds {
+      color += vec4(0.7, 0, 0, 0);
+    }
+
+    return color;
+  }
+);
+// clang-format on
