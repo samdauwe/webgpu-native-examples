72a67debf4c7c8a85e1bd50ce1775c21d7555435
samdauwe <sam.dauwe@gmail.com>
2023-09-28 20:38:03 +0200

    Added cube bind group layout label

--- src/examples/wireframe_vertex_pulling.c
+++ src/examples/wireframe_vertex_pulling.c
@@ -314,6 +314,7 @@ static void setup_bind_groups(wgpu_context_t* wgpu_context)
     }
   };
   WGPUBindGroupDescriptor bg_desc = {
+    .label      = "Cube bind group layout",
     .layout     = cube.bind_group_layout,
     .entryCount = (uint32_t)ARRAY_SIZE(bg_entries),
     .entries    = bg_entries,
