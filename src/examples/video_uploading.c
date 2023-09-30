7b23699babe511ab41c1c093ca1a9cd562c01f76
samdauwe <sam.dauwe@gmail.com>
2023-09-30 20:55:57 +0200

    Updated render pipeline label

--- src/examples/video_uploading.c
+++ src/examples/video_uploading.c
@@ -245,7 +245,7 @@ static void prepare_pipelines(wgpu_context_t* wgpu_context)
   // Create rendering pipeline using the specified states
   pipeline = wgpuDeviceCreateRenderPipeline(
     wgpu_context->device, &(WGPURenderPipelineDescriptor){
-                            .label       = "video_uploading_render_pipeline",
+                            .label       = "Video uploading render pipeline",
                             .primitive   = primitive_state,
                             .vertex      = vertex_state,
                             .fragment    = &fragment_state,
