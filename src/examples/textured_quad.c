ff0b30dbc0660a5557509b7e001fae471b94603b
samdauwe <sam.dauwe@gmail.com>
2023-10-21 21:06:03 +0200

    Updated comment

--- src/examples/textured_quad.c
+++ src/examples/textured_quad.c
@@ -444,7 +444,7 @@ static int example_draw(wgpu_example_context_t* context)
   /* Prepare frame */
   prepare_frame(context);
 
-  /* Command buffer to be submitted to the */
+  /* Command buffer to be submitted to the queue */
   wgpu_context_t* wgpu_context                   = context->wgpu_context;
   wgpu_context->submit_info.command_buffer_count = 1;
   wgpu_context->submit_info.command_buffers[0]
