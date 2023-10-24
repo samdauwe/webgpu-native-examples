c0bdad57230eefa1b80a95ae7ac7ec8069f48df1
samdauwe <sam.dauwe@gmail.com>
2023-10-24 21:19:08 +0200

    Updated vertex shader label

--- src/examples/textured_quad.c
+++ src/examples/textured_quad.c
@@ -314,7 +314,7 @@ static void prepare_pipelines(wgpu_context_t* wgpu_context)
                 wgpu_context, &(wgpu_vertex_state_t){
                 .shader_desc = (wgpu_shader_desc_t){
                   /* Vertex shader SPIR-V */
-                  .label = "texture_vertex_shader",
+                  .label = "Texture vertex shader",
                   .file  = "shaders/textured_quad/texture.vert.spv",
                 },
                 .buffer_count = 1,
