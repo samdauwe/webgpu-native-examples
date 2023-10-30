2b2f0bd63d1c3adc1bfc7b12149f0df15d835ba7
samdauwe <sam.dauwe@gmail.com>
2023-10-30 20:55:32 +0100

    Added render pass descriptor label

--- src/examples/triangle.c
+++ src/examples/triangle.c
@@ -214,6 +214,7 @@ static void setup_render_pass(wgpu_context_t* wgpu_context)
 
   // Render pass descriptor
   render_pass.descriptor = (WGPURenderPassDescriptor){
+    .label                  = "Render pass descriptor",
     .colorAttachmentCount   = 1,
     .colorAttachments       = render_pass.color_attachments,
     .depthStencilAttachment = &wgpu_context->depth_stencil.att_desc,
