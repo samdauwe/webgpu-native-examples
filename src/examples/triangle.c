3ba587e4afedb192c159d254294dc2b0a4e25596
samdauwe <sam.dauwe@gmail.com>
2023-10-31 20:18:37 +0100

    Updated triangle fragment shader label

--- src/examples/triangle.c
+++ src/examples/triangle.c
@@ -301,7 +301,7 @@ static void prepare_pipelines(wgpu_context_t* wgpu_context)
     wgpu_context, &(wgpu_fragment_state_t){
     .shader_desc = (wgpu_shader_desc_t){
       // Fragment shader WGSL
-      .label             = "triangle_fragment_shader_wgsl",
+      .label             = "Triangle fragment shader",
       .wgsl_code.source  = triangle_fragment_shader_wgsl,
       .entry             = "main",
     },
