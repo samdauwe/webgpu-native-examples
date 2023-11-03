8036cde7c6601fdfc93ec3664db72daab5e21614
samdauwe <sam.dauwe@gmail.com>
2023-11-03 21:15:13 +0100

    Updated vertex shader label

--- src/examples/stencil_buffer.c
+++ src/examples/stencil_buffer.c
@@ -214,7 +214,7 @@ static void prepare_pipelines(wgpu_context_t* wgpu_context)
           wgpu_context, &(wgpu_vertex_state_t){
             .shader_desc = (wgpu_shader_desc_t){
               // Vertex shader SPIR-V
-              .label = "toon_vertex_shader",
+              .label = "Toon vertex shader",
               .file  = "shaders/stencil_buffer/toon.vert.spv",
             },
             .buffer_count = 1,
