91d055701e8c736dbed5d64dd95fd2e3bb5c24c8
samdauwe <sam.dauwe@gmail.com>
2023-11-05 19:36:32 +0100

    Added uniform buffer label

--- src/examples/stencil_buffer.c
+++ src/examples/stencil_buffer.c
@@ -334,6 +334,7 @@ static void prepare_uniform_buffers(wgpu_example_context_t* context)
   uniform_buffer_vs = wgpu_create_buffer(
     context->wgpu_context,
     &(wgpu_buffer_desc_t){
+      .label = "Uniform buffer",
       .usage = WGPUBufferUsage_CopyDst | WGPUBufferUsage_Uniform,
       .size  = sizeof(ubo_vs),
     });
