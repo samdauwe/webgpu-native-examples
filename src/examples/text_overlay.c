31b82d7a253fd2df3845c53f0cd5bd700a7f4bc3
samdauwe <sam.dauwe@gmail.com>
2023-11-09 20:49:49 +0100

    Updated mesh fragment shader label

--- src/examples/text_overlay.c
+++ src/examples/text_overlay.c
@@ -215,7 +215,7 @@ static void prepare_pipelines(wgpu_context_t* wgpu_context)
             wgpu_context, &(wgpu_fragment_state_t){
             .shader_desc = (wgpu_shader_desc_t){
               // Fragment shader SPIR-V
-              .label = "mesh_fragment_shader",
+              .label = "Mesh fragment shader",
               .file  = "shaders/text_overlay/mesh.frag.spv",
             },
             .target_count = 1,
