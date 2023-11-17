1df9de00d5bf04ea588422381c4529bef2995442
samdauwe <sam.dauwe@gmail.com>
2023-11-17 20:49:31 +0100

    Added instance buffer bind group label

--- src/examples/terrain_mesh.c
+++ src/examples/terrain_mesh.c
@@ -512,6 +512,7 @@ static void setup_bind_groups(wgpu_context_t* wgpu_context)
       },
     };
     WGPUBindGroupDescriptor bg_desc = {
+      .label      = "Instance buffer bind group",
       .layout     = bind_group_layouts.instance_buffer,
       .entryCount = (uint32_t)ARRAY_SIZE(bg_entries),
       .entries    = bg_entries,
