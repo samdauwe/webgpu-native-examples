03e2957e7122b61e84d197e82bb9032d6b9cab19
samdauwe <sam.dauwe@gmail.com>
2023-11-24 21:23:04 +0100

    Updated cube mesh render pipeline

--- src/examples/text_overlay.c
+++ src/examples/text_overlay.c
@@ -232,7 +232,7 @@ static void prepare_pipelines(wgpu_context_t* wgpu_context)
   // Create rendering pipeline using the specified states
   pipeline = wgpuDeviceCreateRenderPipeline(
     wgpu_context->device, &(WGPURenderPipelineDescriptor){
-                            .label        = "cube_mesh_render_pipeline",
+                            .label        = "Cube mesh render pipeline",
                             .layout       = pipeline_layout,
                             .primitive    = primitive_state,
                             .vertex       = vertex_state,
