bbf2b9379f1c4de6103c77ec1a8c4ccc11a60c48
samdauwe <sam.dauwe@gmail.com>
2023-11-30 19:45:35 +0100

    Updated vertex buffer label

--- src/examples/textured_quad.c
+++ src/examples/textured_quad.c
@@ -107,7 +107,7 @@ static void generate_quad(wgpu_context_t* wgpu_context)
   };
   vertices = wgpu_create_buffer(
     wgpu_context, &(wgpu_buffer_desc_t){
-                    .label = "Vertex buffer",
+                    .label = "Quad vertex buffer",
                     .usage = WGPUBufferUsage_CopyDst | WGPUBufferUsage_Vertex,
                     .size  = sizeof(vertices_data),
                     .count = (uint32_t)ARRAY_SIZE(vertices_data),
