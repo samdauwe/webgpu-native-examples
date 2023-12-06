dfcea56fe4f5fddf9da7c1c8e7bfb96397d96967
samdauwe <sam.dauwe@gmail.com>
2023-12-06 20:52:11 +0100

    Added cube vertices buffer

--- src/examples/two_cubes.c
+++ src/examples/two_cubes.c
@@ -85,6 +85,7 @@ static void prepare_vertex_buffer(wgpu_context_t* wgpu_context)
 {
   vertices = wgpu_create_buffer(
     wgpu_context, &(wgpu_buffer_desc_t){
+                    .label = "Cube vertices buffer",
                     .usage = WGPUBufferUsage_CopyDst | WGPUBufferUsage_Vertex,
                     .size  = sizeof(cube_mesh.vertex_array),
                     .initial.data = cube_mesh.vertex_array,
