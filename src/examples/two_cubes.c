df776a1d6672775236f56cafcf621f096b212550
samdauwe <sam.dauwe@gmail.com>
2023-12-07 20:36:14 +0100

    Enabled V-Sync

--- src/examples/two_cubes.c
+++ src/examples/two_cubes.c
@@ -456,8 +456,9 @@ void example_two_cubes(int argc, char* argv[])
   // clang-format off
   example_run(argc, argv, &(refexport_t){
     .example_settings = (wgpu_example_settings_t){
-     .title   = example_title,
-     .overlay = true,
+      .title   = example_title,
+      .overlay = true,
+      .vsync   = true,
     },
     .example_initialize_func = &example_initialize,
     .example_render_func     = &example_render,
