4302d624529ff8b1b2bff9b7392053af6d536d95
samdauwe <sam.dauwe@gmail.com>
2023-12-14 20:50:10 +0100

    Updated vertex shader label

--- src/examples/bind_groups.c
+++ src/examples/bind_groups.c
@@ -300,7 +300,7 @@ static void prepare_pipelines(wgpu_context_t* wgpu_context)
             wgpu_context, &(wgpu_vertex_state_t){
             .shader_desc = (wgpu_shader_desc_t){
               // Vertex shader WGSL
-              .label            = "Cube vertex shader",
+              .label            = "Cube vertex shader WGSL",
               .wgsl_code.source = bind_groups_vertex_shader_wgsl,
               .entry            = "main",
             },
