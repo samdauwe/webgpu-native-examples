8ca51ee0ade03e91a025a6a69611bd7c0994b233
samdauwe <sam.dauwe@gmail.com>
2024-01-03 20:32:20 +0100

    Updated graphics bind group layout label.

--- src/examples/conway.c
+++ src/examples/conway.c
@@ -256,7 +256,7 @@ static void setup_pipeline_layouts(wgpu_context_t* wgpu_context)
       }
     };
     WGPUBindGroupLayoutDescriptor bgl_desc = {
-      .label      = "Rendering pipeline main layout",
+      .label      = "Graphics bind group layout",
       .entryCount = (uint32_t)ARRAY_SIZE(bgl_entries),
       .entries    = bgl_entries,
     };
