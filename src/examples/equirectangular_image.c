331022e03383b07972b212bb20df450dc79c43d2
samdauwe <sam.dauwe@gmail.com>
2024-01-09 20:46:04 +0100

    Updated fragment shader label.

--- src/examples/equirectangular_image.c
+++ src/examples/equirectangular_image.c
@@ -293,7 +293,7 @@ static void prepare_pipelines(wgpu_context_t* wgpu_context)
                     wgpu_context, &(wgpu_fragment_state_t){
                     .shader_desc = (wgpu_shader_desc_t){
                       // Fragment shader SPIR-V
-                      .label = "Fragment shader",
+                      .label = "Fragment shader SPIR-V",
                       .file  = "shaders/equirectangular_image/main.frag.spv",
                     },
                     .target_count = 1,
