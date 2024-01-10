0cd1c5bec0c53896602f8fde1fe52409a75e9903
samdauwe <sam.dauwe@gmail.com>
2024-01-10 21:24:51 +0100

    Updated vertex shader label

--- src/examples/compute_shader.c
+++ src/examples/compute_shader.c
@@ -393,7 +393,7 @@ static void prepare_pipelines(wgpu_context_t* wgpu_context)
                     wgpu_context, &(wgpu_vertex_state_t){
                     .shader_desc = (wgpu_shader_desc_t){
                       // Vertex shader SPIR-V
-                      .label = "Vertex shader",
+                      .label = "Vertex shader SPIR-V",
                       .file  = "shaders/compute_shader/texture.vert.spv",
                     },
                     .buffer_count = 1,
