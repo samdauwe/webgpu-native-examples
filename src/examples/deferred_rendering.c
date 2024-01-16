5625c439a9e53cb025a3855f11ba1ec6b4aa65eb
samdauwe <sam.dauwe@gmail.com>
2024-01-16 21:13:21 +0100

    Added vertex texture quad label.

--- src/examples/deferred_rendering.c
+++ src/examples/deferred_rendering.c
@@ -671,6 +671,7 @@ static void prepare_gbuffers_debug_view_pipeline(wgpu_context_t* wgpu_context)
         wgpu_context, &(wgpu_vertex_state_t){
         .shader_desc = (wgpu_shader_desc_t){
           // Vertex shader WGSL
+          .label            = "Vertex texture quad WGSL",
           .wgsl_code.source = vertex_texture_quad_wgsl,
           .entry            = "main",
         },
