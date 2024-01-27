88a0cd156454eb08488613c8f291e589bbccc261
samdauwe <sam.dauwe@gmail.com>
2024-01-27 19:43:48 +0100

    Updated fragment shader label.

--- src/examples/immersive_video.c
+++ src/examples/immersive_video.c
@@ -352,7 +352,7 @@ static void prepare_pipelines(wgpu_context_t* wgpu_context)
                     wgpu_context, &(wgpu_fragment_state_t){
                     .shader_desc = (wgpu_shader_desc_t){
                       // Fragment shader SPIR-V
-                      .label = "Main fragment shader",
+                      .label = "Main fragment shader SPIR-V",
                       .file  = "shaders/immersive_video/main.frag.spv",
                     },
                     .target_count = 1,
