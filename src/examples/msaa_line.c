26e71a25a82bfe935b6091e93c5310340f3eadac
samdauwe <sam.dauwe@gmail.com>
2024-01-31 21:19:49 +0100

    Updated fragment shader label.

--- src/examples/msaa_line.c
+++ src/examples/msaa_line.c
@@ -224,7 +224,7 @@ static void prepare_pipelines(wgpu_context_t* wgpu_context)
                     wgpu_context, &(wgpu_fragment_state_t){
                     .shader_desc = (wgpu_shader_desc_t){
                       // Fragment shader WGSL
-                      .label            = "Fragment shader",
+                      .label            = "Fragment shader WGSL",
                       .wgsl_code.source = fragment_shader_wgsl,
                       .entry            = "main",
                     },
