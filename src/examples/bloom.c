158414c217c0584228c48f0db61b34ace49c79d0
samdauwe <sam.dauwe@gmail.com>
2024-02-03 20:40:21 +0100

    Updated phong pass vertex shader label.

--- src/examples/bloom.c
+++ src/examples/bloom.c
@@ -769,7 +769,7 @@ static void prepare_pipelines(wgpu_context_t* wgpu_context)
               wgpu_context, &(wgpu_vertex_state_t){
               .shader_desc = (wgpu_shader_desc_t){
                 // Vertex shader SPIR-V
-                .label = "Phong pass vertex shader",
+                .label = "Phong pass vertex shader SPIR-V",
                 .file  = "shaders/bloom/phongpass.vert.spv",
               },
               .buffer_count = 1,
