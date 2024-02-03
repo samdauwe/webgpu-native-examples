e1406853d76c1eec8a63e8567ad536037bc70d92
samdauwe <sam.dauwe@gmail.com>
2024-02-03 20:39:43 +0100

    Updated horizontal gaussian blur fragment shader label.

--- src/examples/bloom.c
+++ src/examples/bloom.c
@@ -713,7 +713,7 @@ static void prepare_pipelines(wgpu_context_t* wgpu_context)
                 wgpu_context, &(wgpu_fragment_state_t){
                 .shader_desc = (wgpu_shader_desc_t){
                   // Fragment shader SPIR-V
-                  .label = "Horizontal gaussian blur fragment shader",
+                  .label = "Horizontal gaussian blur fragment shader SPIR-V",
                   .file  = "shaders/bloom/gaussblur_horz.frag.spv",
                 },
                 .target_count = 1,
