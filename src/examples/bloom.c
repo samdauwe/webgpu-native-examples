6f9e4febc4d6789cf5716fc88f55fed8d81e16ba
samdauwe <sam.dauwe@gmail.com>
2024-02-05 20:00:26 +0100

    Updated phong pass fragment shader label.

--- src/examples/bloom.c
+++ src/examples/bloom.c
@@ -782,7 +782,7 @@ static void prepare_pipelines(wgpu_context_t* wgpu_context)
               wgpu_context, &(wgpu_fragment_state_t){
               .shader_desc = (wgpu_shader_desc_t){
                 // Fragment shader SPIR-V
-                .label = "Phong pass fragment shader",
+                .label = "Phong pass fragment shader SPIR-V",
                 .file  = "shaders/bloom/phongpass.frag.spv",
               },
               .target_count = 1,
