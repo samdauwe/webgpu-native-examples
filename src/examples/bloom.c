892ea7cf86f6f8db905095779562b94c6a1d7794
samdauwe <sam.dauwe@gmail.com>
2024-02-06 20:12:59 +0100

    Updated skybox vertex shader label.

--- src/examples/bloom.c
+++ src/examples/bloom.c
@@ -877,7 +877,7 @@ static void prepare_pipelines(wgpu_context_t* wgpu_context)
               wgpu_context, &(wgpu_vertex_state_t){
               .shader_desc = (wgpu_shader_desc_t){
                 // Vertex shader SPIR-V
-                .label = "skybox_vertex_shader",
+                .label = "Skybox vertex shader SPIR-V",
                 .file  = "shaders/bloom/skybox.vert.spv",
               },
               .buffer_count = 1,
