5d08b04db8998cd224987f9bb3abfa67011ea580
samdauwe <sam.dauwe@gmail.com>
2024-02-09 19:44:19 +0100

    Updated mesh fragment shader label.

--- src/examples/occlusion_query.c
+++ src/examples/occlusion_query.c
@@ -314,7 +314,7 @@ static void prepare_pipelines(wgpu_context_t* wgpu_context)
               wgpu_context, &(wgpu_fragment_state_t){
               .shader_desc = (wgpu_shader_desc_t){
                 // Fragment shader SPIR-V
-                .label = "Mesh fragment shader",
+                .label = "Mesh fragment shader SPIR-V",
                 .file  = "shaders/occlusion_query/mesh.frag.spv",
               },
               .target_count = 1,
