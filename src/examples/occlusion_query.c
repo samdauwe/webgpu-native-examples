3d6104f355ed0cabaa40e14dce392f9ba42c8450
samdauwe <sam.dauwe@gmail.com>
2024-02-09 19:43:24 +0100

    Updated mesh vertex shader label.

--- src/examples/occlusion_query.c
+++ src/examples/occlusion_query.c
@@ -302,7 +302,7 @@ static void prepare_pipelines(wgpu_context_t* wgpu_context)
               wgpu_context, &(wgpu_vertex_state_t){
               .shader_desc = (wgpu_shader_desc_t){
                 // Vertex shader SPIR-V
-                .label = "Mesh vertex shader",
+                .label = "Mesh vertex shader SPIR-V",
                 .file  = "shaders/occlusion_query/mesh.vert.spv",
               },
               .buffer_count = 1,
