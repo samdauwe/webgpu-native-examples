9f690a391beb7dffc960e61d1a0543c47cf14245
samdauwe <sam.dauwe@gmail.com>
2024-02-10 17:58:01 +0100

    Updated simple fragment shader label.

--- src/examples/occlusion_query.c
+++ src/examples/occlusion_query.c
@@ -352,7 +352,7 @@ static void prepare_pipelines(wgpu_context_t* wgpu_context)
               wgpu_context, &(wgpu_fragment_state_t){
               .shader_desc = (wgpu_shader_desc_t){
                 // Fragment shader SPIR-V
-                .label = "simple_fragment_shader",
+                .label = "Simple fragment shader SPIR-V",
                 .file  = "shaders/occlusion_query/simple.frag.spv",
               },
               .target_count = 1,
