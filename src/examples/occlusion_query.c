89c914fc29d030386b2a68c9ca02e6af6f0ac486
samdauwe <sam.dauwe@gmail.com>
2024-02-11 19:45:09 +0100

    Added occluder uniform buffer label.

--- src/examples/occlusion_query.c
+++ src/examples/occlusion_query.c
@@ -465,6 +465,7 @@ static void prepare_uniform_buffers(wgpu_example_context_t* context)
   uniform_buffers.occluder = wgpu_create_buffer(
     context->wgpu_context,
     &(wgpu_buffer_desc_t){
+      .label = "Occluder uniform buffer",
       .usage = WGPUBufferUsage_CopyDst | WGPUBufferUsage_Uniform,
       .size  = sizeof(ubo_vs),
     });
