f639d4f135e9afa594a489343ace261979b6749a
samdauwe <sam.dauwe@gmail.com>
2024-02-11 19:40:49 +0100

    Updated occluder vertex shader label.

--- src/examples/occlusion_query.c
+++ src/examples/occlusion_query.c
@@ -387,7 +387,7 @@ static void prepare_pipelines(wgpu_context_t* wgpu_context)
               wgpu_context, &(wgpu_vertex_state_t){
               .shader_desc = (wgpu_shader_desc_t){
                 // Vertex shader SPIR-V
-                .label = "occluder_vertex_shader",
+                .label = "Occluder vertex shader SPIR-V",
                 .file  = "shaders/occlusion_query/occluder.vert.spv",
               },
               .buffer_count = 1,
