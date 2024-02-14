e82a154543c01c1e26087e0ec8043554977cfd57
samdauwe <sam.dauwe@gmail.com>
2024-02-14 20:45:43 +0100

    Added depth stencil texture view label.

--- src/examples/offscreen_rendering.c
+++ src/examples/offscreen_rendering.c
@@ -200,6 +200,7 @@ static void prepare_offscreen(wgpu_context_t* wgpu_context)
 
     // Create the texture view
     WGPUTextureViewDescriptor texture_view_dec = {
+      .label           = "Depth stencil texture view",
       .dimension       = WGPUTextureViewDimension_2D,
       .format          = texture_desc.format,
       .baseMipLevel    = 0,
