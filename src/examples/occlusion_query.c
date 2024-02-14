f13f6be15450f481cfa39267d89b52bcbb348427
samdauwe <sam.dauwe@gmail.com>
2024-02-14 20:43:55 +0100

    Using define for exit code.

--- src/examples/occlusion_query.c
+++ src/examples/occlusion_query.c
@@ -724,13 +724,13 @@ static int example_draw(wgpu_example_context_t* context)
   // Submit frame
   submit_frame(context);
 
-  return 0;
+  return EXIT_SUCCESS;
 }
 
 static int example_render(wgpu_example_context_t* context)
 {
   if (!prepared) {
-    return 1;
+    return EXIT_FAILURE;
   }
   return example_draw(context);
 }
