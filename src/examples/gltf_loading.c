a646c02ac7ce12217372dceff67230574ca24fff
samdauwe <sam.dauwe@gmail.com>
2024-02-18 18:25:06 +0100

    Enabled V-Sync

--- src/examples/gltf_loading.c
+++ src/examples/gltf_loading.c
@@ -1187,8 +1187,9 @@ void example_gltf_loading(int argc, char* argv[])
   // clang-format off
   example_run(argc, argv, &(refexport_t){
     .example_settings = (wgpu_example_settings_t){
-     .title  = example_title,
+     .title   = example_title,
      .overlay = true,
+     .vsync   = true,
     },
     .example_initialize_func      = &example_initialize,
     .example_render_func          = &example_render,
