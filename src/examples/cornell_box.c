136db8ad44b57b74a1a280d2ad9789a71b7eb7f7
samdauwe <sam.dauwe@gmail.com>
2024-02-23 17:21:42 +0100

    Updated radiosity comp shader label.

--- src/examples/cornell_box.c
+++ src/examples/cornell_box.c
@@ -956,7 +956,7 @@ static void radiosity_create(radiosity_t* this, wgpu_context_t* wgpu_context,
     wgpu_shader_t radiosity_comp_shader = wgpu_shader_create(
       wgpu_context, &(wgpu_shader_desc_t){
                       // Compute shader WGSL
-                      .label           = "radiosity_comp_shader",
+                      .label           = "Radiosity comp shader",
                       .wgsl_code       = {wgsl_code},
                       .entry           = "radiosity",
                       .constants.count = (uint32_t)ARRAY_SIZE(constant_entries),
