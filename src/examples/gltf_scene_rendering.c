95b27ddc671299448722e368629611e78ee498db
samdauwe <sam.dauwe@gmail.com>
2024-03-05 20:33:21 +0100

    Added material bind group layout label.

--- src/examples/gltf_scene_rendering.c
+++ src/examples/gltf_scene_rendering.c
@@ -186,6 +186,7 @@ static void setup_pipeline_layout(wgpu_context_t* wgpu_context)
       },
     };
     WGPUBindGroupLayoutDescriptor bgl_desc = {
+      .label      = "Material bind group layout",
       .entryCount = (uint32_t)ARRAY_SIZE(bgl_entries),
       .entries    = bgl_entries,
     };
