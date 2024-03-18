05ff8adc8dcfe3f2d6f566ae16793c972c262c3e
samdauwe <sam.dauwe@gmail.com>
2024-03-18 20:34:28 +0100

    Updated upscale rendering pipeline label.

--- src/examples/conservative_raster.c
+++ src/examples/conservative_raster.c
@@ -404,7 +404,7 @@ static void prepare_pipeline_upscale(wgpu_context_t* wgpu_context)
   // Create rendering pipeline using the specified states
   render_pipelines.upscale = wgpuDeviceCreateRenderPipeline(
     wgpu_context->device, &(WGPURenderPipelineDescriptor){
-                            .label       = "Upscale",
+                            .label       = "Upscale rendering pipeline",
                             .layout      = pipeline_layout,
                             .primitive   = primitive_state,
                             .vertex      = vertex_state,
