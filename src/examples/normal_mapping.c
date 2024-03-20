fbc51e4c56e8adcf27d25ade9facfdcab7d5b1bc
samdauwe <sam.dauwe@gmail.com>
2024-03-20 20:12:31 +0100

    Updated normal map fragment shader label.

--- src/examples/normal_mapping.c
+++ src/examples/normal_mapping.c
@@ -1120,7 +1120,7 @@ static void prepare_normal_map_pipeline(wgpu_context_t* wgpu_context)
     wgpu_context, &(wgpu_fragment_state_t){
                     .shader_desc = (wgpu_shader_desc_t){
                       // Fragment shader WGSL
-                      .label            = "normal_map_fragment_shader_wgsl",
+                      .label            = "Normal map fragment shader WGSL",
                       .wgsl_code.source = normal_map_fragment_shader_wgsl,
                       .entry            = "main",
                     },
