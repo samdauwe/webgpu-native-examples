d0926746d6d8dd9d6f4c49c9767bab38722a47d6
samdauwe <sam.dauwe@gmail.com>
2024-03-22 20:21:51 +0100

    Updated commenting style.

--- src/examples/text_overlay.c
+++ src/examples/text_overlay.c
@@ -356,42 +356,42 @@ static WGPUCommandBuffer build_command_buffer(wgpu_example_context_t* context)
 {
   wgpu_context_t* wgpu_context = context->wgpu_context;
 
-  // Set target frame buffer
+  /* Set target frame buffer */
   render_pass.color_attachments[0].view = wgpu_context->swap_chain.frame_buffer;
 
-  // Create command encoder
+  /* Create command encoder */
   wgpu_context->cmd_enc
     = wgpuDeviceCreateCommandEncoder(wgpu_context->device, NULL);
 
-  // Create render pass encoder for encoding drawing commands
+  /* Create render pass encoder for encoding drawing commands */
   wgpu_context->rpass_enc = wgpuCommandEncoderBeginRenderPass(
     wgpu_context->cmd_enc, &render_pass.descriptor);
 
-  // Set viewport
+  /* Set viewport */
   wgpuRenderPassEncoderSetViewport(
     wgpu_context->rpass_enc, 0.0f, 0.0f, (float)wgpu_context->surface.width,
     (float)wgpu_context->surface.height, 0.0f, 1.0f);
 
-  // Set scissor rectangle
+  /* Set scissor rectangle */
   wgpuRenderPassEncoderSetScissorRect(wgpu_context->rpass_enc, 0u, 0u,
                                       wgpu_context->surface.width,
                                       wgpu_context->surface.height);
 
-  // Bind the rendering pipeline
+  /* Bind the rendering pipeline */
   wgpuRenderPassEncoderSetPipeline(wgpu_context->rpass_enc, pipeline);
 
-  // Set the bind group
+  /* Set the bind group */
   wgpuRenderPassEncoderSetBindGroup(wgpu_context->rpass_enc, 0, bind_group, 0,
                                     0);
 
-  // Draw model
+  /* Draw model */
   wgpu_gltf_model_draw(model, (wgpu_gltf_model_render_options_t){0});
 
-  // End render pass
+  /* End render pass */
   wgpuRenderPassEncoderEnd(wgpu_context->rpass_enc);
   WGPU_RELEASE_RESOURCE(RenderPassEncoder, wgpu_context->rpass_enc)
 
-  // Draw text overlay on frame
+  /* Draw text overlay on frame */
   if (text_overlay.visible) {
     if (text_overlay.redraw_needed) {
       text_overlay.redraw_needed = false;
@@ -401,7 +401,7 @@ static WGPUCommandBuffer build_command_buffer(wgpu_example_context_t* context)
                             render_pass.color_attachments[0].view);
   }
 
-  // Get command buffer
+  // Get command buffer */
   WGPUCommandBuffer command_buffer
     = wgpu_get_command_buffer(wgpu_context->cmd_enc);
   WGPU_RELEASE_RESOURCE(CommandEncoder, wgpu_context->cmd_enc)
