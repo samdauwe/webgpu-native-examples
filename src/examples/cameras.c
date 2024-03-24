d0c20f813445023129eaecd458178937d8f7878c
samdauwe <sam.dauwe@gmail.com>
2024-03-24 19:20:52 +0100

    Added texture sampler label.

--- src/examples/cameras.c
+++ src/examples/cameras.c
@@ -959,6 +959,7 @@ static void prepare_texture(wgpu_context_t* wgpu_context)
   {
     textures.sampler = wgpuDeviceCreateSampler(
       wgpu_context->device, &(WGPUSamplerDescriptor){
+                              .label         = "Texture sampler",
                               .addressModeU  = WGPUAddressMode_ClampToEdge,
                               .addressModeV  = WGPUAddressMode_ClampToEdge,
                               .addressModeW  = WGPUAddressMode_ClampToEdge,
