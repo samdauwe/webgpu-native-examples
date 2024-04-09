248aea24cfb7dab9d627c28d1846374b0156cda6
samdauwe <sam.dauwe@gmail.com>
2024-04-09 20:27:31 +0200

    Added compute pipeline layout label.

--- src/examples/compute_particles_easing.c
+++ src/examples/compute_particles_easing.c
@@ -191,6 +191,7 @@ static void prepare_compute(wgpu_context_t* wgpu_context)
   ASSERT(compute.bind_group_layout != NULL)
 
   WGPUPipelineLayoutDescriptor compute_pipeline_layout_desc = {
+    .label                = "Compute pipeline layout",
     .bindGroupLayoutCount = 1,
     .bindGroupLayouts     = &compute.bind_group_layout,
   };
