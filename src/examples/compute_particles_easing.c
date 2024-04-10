2b16bded021d1a6a8d3962044a971e0a333f38b8
samdauwe <sam.dauwe@gmail.com>
2024-04-10 20:45:35 +0200

    Added bind group label.

--- src/examples/compute_particles_easing.c
+++ src/examples/compute_particles_easing.c
@@ -313,6 +313,7 @@ static void setup_bind_groups(wgpu_context_t* wgpu_context)
   };
 
   WGPUBindGroupDescriptor bg_desc = {
+    .label      = "Bind group",
     .layout     = graphics.uniforms_bind_group_layout,
     .entryCount = (uint32_t)ARRAY_SIZE(bg_entries),
     .entries    = bg_entries,
