722a12a6c2058405cc83611925e47295710e7f3e
samdauwe <sam.dauwe@gmail.com>
2024-04-17 20:56:12 +0200

    Added bind group layout label.

--- src/examples/n_body_simulation.c
+++ src/examples/n_body_simulation.c
@@ -232,6 +232,7 @@ static void setup_compute_pipeline_layout(wgpu_context_t* wgpu_context)
     },
   };
   WGPUBindGroupLayoutDescriptor bgl_desc = {
+    .label      = "Bind group layout",
     .entryCount = (uint32_t)ARRAY_SIZE(bgl_entries),
     .entries    = bgl_entries,
   };
