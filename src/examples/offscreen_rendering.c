4a22abef3f93071291c5716568ce6b038fd05672
samdauwe <sam.dauwe@gmail.com>
2024-04-18 21:07:51 +0200

    Added mirror bind group label.

--- src/examples/offscreen_rendering.c
+++ src/examples/offscreen_rendering.c
@@ -355,6 +355,7 @@ static void setup_bind_groups(wgpu_context_t* wgpu_context)
 
     bind_groups.mirror = wgpuDeviceCreateBindGroup(
       wgpu_context->device, &(WGPUBindGroupDescriptor){
+                              .label      = "Mirror bind group",
                               .layout     = bind_group_layouts.textured,
                               .entryCount = (uint32_t)ARRAY_SIZE(bg_entries),
                               .entries    = bg_entries,
