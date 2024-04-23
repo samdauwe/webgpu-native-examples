53741aae2f184b04a16da49d20ca6036b028fd9c
samdauwe <sam.dauwe@gmail.com>
2024-04-23 21:06:11 +0200

    Added parameters uniform buffer label.

--- src/examples/hdr.c
+++ src/examples/hdr.c
@@ -1024,6 +1024,7 @@ static void prepare_uniform_buffers(wgpu_example_context_t* context)
   uniform_buffers.params = wgpu_create_buffer(
     context->wgpu_context,
     &(wgpu_buffer_desc_t){
+      .label = "Parameters uniform buffer",
       .usage = WGPUBufferUsage_CopyDst | WGPUBufferUsage_Uniform,
       .size  = sizeof(ubo_matrices),
     });
