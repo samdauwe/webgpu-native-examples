d9fbb538637f1e6486490e714c8a21b8effa262a
samdauwe <sam.dauwe@gmail.com>
2024-04-28 19:05:37 +0200

    Updated offscreen color texure 1 label.

--- src/examples/hdr.c
+++ src/examples/hdr.c
@@ -255,7 +255,7 @@ static void prepare_offscreen(wgpu_context_t* wgpu_context)
 
     /* Two floating point color buffers */
     create_attachment(
-      wgpu_context, "offscreen_color_tex_1", WGPUTextureFormat_RGBA8Unorm,
+      wgpu_context, "Offscreen color texure 1", WGPUTextureFormat_RGBA8Unorm,
       WGPU_RENDER_PASS_COLOR_ATTACHMENT_TYPE, &offscreen_pass.color[0]);
     create_attachment(
       wgpu_context, "offscreen_color_tex_2", WGPUTextureFormat_RGBA8Unorm,
