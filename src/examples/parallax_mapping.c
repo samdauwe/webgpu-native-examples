5d08d68911efe2ee77a76370028ef5fea19371d2
samdauwe <sam.dauwe@gmail.com>
2024-05-06 20:06:51 +0200

    Enabled V-Sync.

--- src/examples/parallax_mapping.c
+++ src/examples/parallax_mapping.c
@@ -544,6 +544,7 @@ void example_parallax_mapping(int argc, char* argv[])
     .example_settings = (wgpu_example_settings_t){
       .title   = example_title,
       .overlay = true,
+      .vsync   = true,
     },
     .example_initialize_func      = &example_initialize,
     .example_render_func          = &example_render,
