24b95f29b273517c08f956f0a5bc87dee716c4b7
samdauwe <sam.dauwe@gmail.com>
2024-05-08 20:19:05 +0200

    Updated render pipeline label.

--- src/examples/equirectangular_image.c
+++ src/examples/equirectangular_image.c
@@ -310,7 +310,7 @@ static void prepare_pipelines(wgpu_context_t* wgpu_context)
   // Create rendering pipeline using the specified states
   pipeline = wgpuDeviceCreateRenderPipeline(
     wgpu_context->device, &(WGPURenderPipelineDescriptor){
-                            .label  = "Equirectangular image render pipeline",
+                            .label  = "Equirectangular image - render pipeline",
                             .layout = pipeline_layout,
                             .primitive   = primitive_state,
                             .vertex      = vertex_state,
