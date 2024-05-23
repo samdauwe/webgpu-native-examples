4ea133b1163bcdd3969b3f5e72d4d92aa89194c7
samdauwe <sam.dauwe@gmail.com>
2024-05-23 20:22:05 +0200

    Updated commenting style.

--- src/examples/texture_mipmap_gen.c
+++ src/examples/texture_mipmap_gen.c
@@ -446,7 +446,7 @@ static void prepare_pipelines(wgpu_context_t* wgpu_context)
   WGPUVertexState vertex_state = wgpu_create_vertex_state(
             wgpu_context, &(wgpu_vertex_state_t){
             .shader_desc = (wgpu_shader_desc_t){
-              // Vertex shader SPIR-V
+              /* Vertex shader SPIR-V */
               .label = "Texture vertex shader SPIR-V",
               .file  = "shaders/texture_mipmap_gen/texture.vert.spv",
             },
@@ -458,7 +458,7 @@ static void prepare_pipelines(wgpu_context_t* wgpu_context)
   WGPUFragmentState fragment_state = wgpu_create_fragment_state(
             wgpu_context, &(wgpu_fragment_state_t){
             .shader_desc = (wgpu_shader_desc_t){
-              // Fragment shader SPIR-V
+              /* Fragment shader SPIR-V */
               .label = "Texture fragment shader SPIR-V",
               .file  = "shaders/texture_mipmap_gen/texture.frag.spv",
             },
