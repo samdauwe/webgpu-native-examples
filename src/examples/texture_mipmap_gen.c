1a67938b45c668beafc6b0789c463176eb08358b
samdauwe <sam.dauwe@gmail.com>
2024-05-24 20:27:51 +0200

    Updated mipmap gen render pipeline label.

--- src/examples/texture_mipmap_gen.c
+++ src/examples/texture_mipmap_gen.c
@@ -476,7 +476,7 @@ static void prepare_pipelines(wgpu_context_t* wgpu_context)
   // Create rendering pipeline using the specified states
   pipeline = wgpuDeviceCreateRenderPipeline(
     wgpu_context->device, &(WGPURenderPipelineDescriptor){
-                            .label     = "texture_mipmap_gen_render_pipeline",
+                            .label     = "Texture mipmap gen render pipeline",
                             .layout    = pipeline_layout,
                             .primitive = primitive_state,
                             .vertex    = vertex_state,
