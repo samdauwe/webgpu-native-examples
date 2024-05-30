187b20588c2501732133f5909edef98f1c7524ca
samdauwe <sam.dauwe@gmail.com>
2024-05-30 20:47:36 +0200

    Updated bind group layout label.

--- src/examples/tile_map.c
+++ src/examples/tile_map.c
@@ -245,7 +245,7 @@ static void tile_map_renderer_create(tile_map_renderer_t* this,
     };
     this->bind_group_layout = wgpuDeviceCreateBindGroupLayout(
       wgpu_context->device, &(WGPUBindGroupLayoutDescriptor){
-                              .label      = "Tile map bind group layout",
+                              .label      = "Tile map - bind group layout",
                               .entryCount = (uint32_t)ARRAY_SIZE(bgl_entries),
                               .entries    = bgl_entries,
                             });
