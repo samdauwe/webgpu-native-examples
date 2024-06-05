fa5cfffabd153fa1e7f8d58ae4f372dae1546195
samdauwe <sam.dauwe@gmail.com>
2024-06-05 21:10:13 +0200

    Updated vertex shader label.

--- src/examples/triangle.c
+++ src/examples/triangle.c
@@ -289,7 +289,7 @@ static void prepare_pipelines(wgpu_context_t* wgpu_context)
     wgpu_context, &(wgpu_vertex_state_t){
     .shader_desc = (wgpu_shader_desc_t){
       // Vertex shader WGSL
-      .label             = "Triangle vertex shader WGSL",
+      .label             = "Triangle - Vertex shader WGSL",
       .wgsl_code.source  = triangle_vertex_shader_wgsl,
       .entry             = "main",
     },
