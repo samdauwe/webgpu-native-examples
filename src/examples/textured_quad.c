0583d5fe2bac1ea88d565e106746adc7316e5a78
samdauwe <sam.dauwe@gmail.com>
2024-06-05 21:05:03 +0200

    Updated render pipeline label.

--- src/examples/textured_quad.c
+++ src/examples/textured_quad.c
@@ -344,7 +344,7 @@ static void prepare_pipelines(wgpu_context_t* wgpu_context)
   /* Create rendering pipeline using the specified states */
   pipeline = wgpuDeviceCreateRenderPipeline(
     wgpu_context->device, &(WGPURenderPipelineDescriptor){
-                            .label        = "Textured quad render pipeline",
+                            .label        = "Textured quad - Render pipeline",
                             .layout       = pipeline_layout,
                             .primitive    = primitive_state,
                             .vertex       = vertex_state,
