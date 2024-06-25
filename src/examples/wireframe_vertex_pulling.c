dec8bfc971ad50707f6120a7ea7cab858274fca3
samdauwe <sam.dauwe@gmail.com>
2024-06-25 20:46:28 +0200

    Updated fragment shader label.

--- src/examples/wireframe_vertex_pulling.c
+++ src/examples/wireframe_vertex_pulling.c
@@ -525,7 +525,7 @@ static void prepare_pipelines(wgpu_context_t* wgpu_context)
        wgpu_context, &(wgpu_fragment_state_t){
        .shader_desc = (wgpu_shader_desc_t){
          /* Vertex shader WGSL */
-         .label            = "Render wireframe thick fragment shader",
+         .label            = "Render wireframe thick fragment shader WGSL",
          .wgsl_code.source = render_wireframe_thick_wgsl,
          .entry            = "main_fragment",
        },
