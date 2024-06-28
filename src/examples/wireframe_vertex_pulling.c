3280af2b56215f92144a8cc1346788bcf6e8e52b
samdauwe <sam.dauwe@gmail.com>
2024-06-28 21:04:03 +0200

    Updated vertex shader label.

--- src/examples/wireframe_vertex_pulling.c
+++ src/examples/wireframe_vertex_pulling.c
@@ -464,7 +464,7 @@ static void prepare_pipelines(wgpu_context_t* wgpu_context)
        wgpu_context, &(wgpu_vertex_state_t){
        .shader_desc = (wgpu_shader_desc_t){
          /* Vertex shader WGSL */
-         .label            = "Render wireframe vertex shader WGSL",
+         .label            = "Render wireframe - Vertex shader WGSL",
          .wgsl_code.source = render_wireframe_wgsl,
          .entry            = "main_vertex",
        },
