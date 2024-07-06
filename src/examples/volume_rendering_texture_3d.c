e8479fed2acaf52cf2344b809e0f2e6617607022
samdauwe <sam.dauwe@gmail.com>
2024-07-06 18:17:43 +0200

    Updated fragment shader label.

--- src/examples/volume_rendering_texture_3d.c
+++ src/examples/volume_rendering_texture_3d.c
@@ -386,7 +386,7 @@ static void prepare_pipeline(wgpu_context_t* wgpu_context)
     wgpu_context, &(wgpu_fragment_state_t){
                     .shader_desc = (wgpu_shader_desc_t){
                       /* Fragment shader WGSL */
-                      .label            = "Volume texture 3d - Fragment shader",
+                      .label            = "Volume texture 3d - Fragment shader WGSL",
                       .wgsl_code.source = volume_shader_wgsl,
                       .entry            = "fragment_main",
                     },
