4078c930f00212ea8116c999819a32b0514f88d8
samdauwe <sam.dauwe@gmail.com>
2024-07-08 20:14:47 +0200

    Updated commenting style.

--- src/examples/msaa_line.c
+++ src/examples/msaa_line.c
@@ -149,7 +149,7 @@ static void setup_render_pass(wgpu_context_t* wgpu_context)
 {
   UNUSED_VAR(wgpu_context);
 
-  // Color attachment
+  /* Color attachment */
   rp_color_att_descriptors[0] = (WGPURenderPassColorAttachment) {
       .view          = NULL, /* Assigned later */
       .resolveTarget = NULL,
@@ -164,7 +164,7 @@ static void setup_render_pass(wgpu_context_t* wgpu_context)
       },
   };
 
-  // Render pass descriptor
+  /* Render pass descriptor */
   render_pass_desc = (WGPURenderPassDescriptor){
     .label                = "Render pass descriptor",
     .colorAttachmentCount = 1,
