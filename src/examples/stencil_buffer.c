95001d57e12c122116b2140ae36ff4a524641924
samdauwe <sam.dauwe@gmail.com>
2024-07-18 21:37:03 +0200

    Updated vertex shader label.

--- src/examples/stencil_buffer.c
+++ src/examples/stencil_buffer.c
@@ -274,7 +274,7 @@ static void prepare_pipelines(wgpu_context_t* wgpu_context)
           wgpu_context, &(wgpu_vertex_state_t){
             .shader_desc = (wgpu_shader_desc_t){
               // Vertex shader SPIR-V
-              .label = "Outline vertex shader",
+              .label = "Outline vertex shader SPIR-V",
               .file  = "shaders/stencil_buffer/outline.vert.spv",
             },
             .buffer_count = 1,
