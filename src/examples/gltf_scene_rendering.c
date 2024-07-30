fb8b64b0d39cde1f0eed85fad0a60fcd5c9052f9
samdauwe <sam.dauwe@gmail.com>
2024-07-30 20:54:35 +0200

    Updated scene matrices bind group.

--- src/examples/gltf_scene_rendering.c
+++ src/examples/gltf_scene_rendering.c
@@ -227,7 +227,7 @@ static void setup_bind_groups(wgpu_context_t* wgpu_context)
     };
     bind_groups.ubo_scene = wgpuDeviceCreateBindGroup(
       wgpu_context->device, &(WGPUBindGroupDescriptor){
-                              .label      = "Scene matrices bind group",
+                              .label      = "Scene matrices - Bind group",
                               .layout     = bind_group_layouts.ubo_scene,
                               .entryCount = 1,
                               .entries    = &bg_entry,
