f035cf04c60e715ad094bdf72f73fbcbc4da5504
samdauwe <sam.dauwe@gmail.com>
2024-08-04 17:29:38 +0200

    Updated fragment shader label.

--- src/examples/pbr_basic.c
+++ src/examples/pbr_basic.c
@@ -357,7 +357,7 @@ static void prepare_pipelines(wgpu_context_t* wgpu_context)
             wgpu_context, &(wgpu_fragment_state_t){
             .shader_desc = (wgpu_shader_desc_t){
               // Vertex shader WGSL
-              .label            = "PBR Basic fragment WGSL",
+              .label            = "PBR Basic - Fragment shader WGSL",
               .wgsl_code.source = pbr_fragment_shader_wgsl,
               .entry            = "main",
             },
