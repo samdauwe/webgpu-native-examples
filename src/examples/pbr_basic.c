4612a9bde36197e9c695381e9ac6d2d5cd99aa31
samdauwe <sam.dauwe@gmail.com>
2024-08-04 17:28:56 +0200

    Updated vertex shader label.

--- src/examples/pbr_basic.c
+++ src/examples/pbr_basic.c
@@ -344,7 +344,7 @@ static void prepare_pipelines(wgpu_context_t* wgpu_context)
             wgpu_context, &(wgpu_vertex_state_t){
             .shader_desc = (wgpu_shader_desc_t){
               // Vertex shader WGSL
-              .label            = "PBR Basic vertex WGSL",
+              .label            = "PBR Basic - Vertex shader WGSL",
               .wgsl_code.source = pbr_vertex_shader_wgsl,
               .entry            = "main",
             },
