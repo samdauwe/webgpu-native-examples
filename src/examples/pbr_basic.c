20d69efa4ebcd72d714c2c0fa0e76441afb02fd2
samdauwe <sam.dauwe@gmail.com>
2024-08-05 20:00:47 +0200

    Updated render pipeline label.

--- src/examples/pbr_basic.c
+++ src/examples/pbr_basic.c
@@ -375,7 +375,7 @@ static void prepare_pipelines(wgpu_context_t* wgpu_context)
   // Create rendering pipeline using the specified states
   pipeline = wgpuDeviceCreateRenderPipeline(
     wgpu_context->device, &(WGPURenderPipelineDescriptor){
-                            .label        = "PBR render pipeline",
+                            .label        = "PBR Basic - Render pipeline",
                             .layout       = pipeline_layout,
                             .primitive    = primitive_state,
                             .vertex       = vertex_state,
