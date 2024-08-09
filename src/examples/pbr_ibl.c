b60ae5642088714b626872e9aa2a6ac03ca04c1c
samdauwe <sam.dauwe@gmail.com>
2024-08-09 17:33:46 +0200

    Updated skybox fragment shader label.

--- src/examples/pbr_ibl.c
+++ src/examples/pbr_ibl.c
@@ -648,7 +648,7 @@ static void prepare_pipelines(wgpu_context_t* wgpu_context)
             wgpu_context, &(wgpu_fragment_state_t){
             .shader_desc = (wgpu_shader_desc_t){
               // Fragment shader WGSL
-              .label            = "Skybox fragment shader",
+              .label            = "Skybox - Fragment shader WGSL",
               .wgsl_code.source = skybox_fragment_shader_wgsl,
               .entry            = "main",
             },
