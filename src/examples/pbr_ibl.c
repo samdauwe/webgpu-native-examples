2b7c7bc1c6069b9a097bbcd38107db4e7b4b5a31
samdauwe <sam.dauwe@gmail.com>
2024-08-09 17:33:10 +0200

    Updated skybox vertex shader label.

--- src/examples/pbr_ibl.c
+++ src/examples/pbr_ibl.c
@@ -635,7 +635,7 @@ static void prepare_pipelines(wgpu_context_t* wgpu_context)
             wgpu_context, &(wgpu_vertex_state_t){
             .shader_desc = (wgpu_shader_desc_t){
               // Vertex shader WGSL
-              .label            = "Skybox vertex shader",
+              .label            = "Skybox - Vertex shader WGSL",
               .wgsl_code.source = skybox_vertex_shader_wgsl,
               .entry            = "main",
             },
