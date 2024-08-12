a98f32c3076cd05d2e19aefb14516c1f71231bd7
samdauwe <sam.dauwe@gmail.com>
2024-08-12 17:47:38 +0200

    Updated PBR IBL fragment shader label.

--- src/examples/pbr_ibl.c
+++ src/examples/pbr_ibl.c
@@ -702,7 +702,7 @@ static void prepare_pipelines(wgpu_context_t* wgpu_context)
             wgpu_context, &(wgpu_fragment_state_t){
             .shader_desc = (wgpu_shader_desc_t){
               /* Fragment shader WGSL */
-              .label            = "PBR IBL - Fragment shader",
+              .label            = "PBR IBL - Fragment shader WGSL",
               .wgsl_code.source = fragment_shader_wgsl,
               .entry            = "main",
             },
