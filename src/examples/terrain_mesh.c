74e7d468b38c1848f010d48007d882f0738ad68e
samdauwe <sam.dauwe@gmail.com>
2024-08-20 20:12:50 +0200

    Updated frame constants bind group label.

--- src/examples/terrain_mesh.c
+++ src/examples/terrain_mesh.c
@@ -492,7 +492,7 @@ static void setup_bind_groups(wgpu_context_t* wgpu_context)
       }
     };
     WGPUBindGroupDescriptor bg_desc = {
-      .label      = "Frame constants bind group",
+      .label      = "Frame constants - Bind group",
       .layout     = bind_group_layouts.frame_constants,
       .entryCount = (uint32_t)ARRAY_SIZE(bg_entries),
       .entries    = bg_entries,
