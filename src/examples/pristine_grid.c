1dddd2f3bc29ae360e0f8908b0cebf66c89213a0
samdauwe <sam.dauwe@gmail.com>
2024-08-23 19:50:55 +0200

    Updated commenting style.

--- src/examples/pristine_grid.c
+++ src/examples/pristine_grid.c
@@ -791,14 +791,14 @@ static void setup_render_pass(void)
 
 static void prepare_render_pipeline(wgpu_context_t* wgpu_context)
 {
-  // Primitive state
+  /* Primitive state */
   WGPUPrimitiveState primitive_state = {
     .topology  = WGPUPrimitiveTopology_TriangleList,
     .frontFace = WGPUFrontFace_CCW,
     .cullMode  = WGPUCullMode_None,
   };
 
-  // Color target state
+  /* Color target state */
   WGPUBlendState blend_state              = wgpu_create_blend_state(false);
   WGPUColorTargetState color_target_state = (WGPUColorTargetState){
     .format    = wgpu_context->swap_chain.format,
@@ -806,7 +806,7 @@ static void prepare_render_pipeline(wgpu_context_t* wgpu_context)
     .writeMask = WGPUColorWriteMask_All,
   };
 
-  // Depth stencil state
+  /* Depth stencil state */
   WGPUDepthStencilState depth_stencil_state
     = wgpu_create_depth_stencil_state(&(create_depth_stencil_state_desc_t){
       .format              = depth_format,
@@ -814,19 +814,19 @@ static void prepare_render_pipeline(wgpu_context_t* wgpu_context)
     });
   depth_stencil_state.depthCompare = WGPUCompareFunction_LessEqual;
 
-  // Vertex buffer layout
+  /* Vertex buffer layout */
   WGPU_VERTEX_BUFFER_LAYOUT(
     triangle, 20,
-    // Attribute location 0: Position
+    /* Attribute location 0: Position */
     WGPU_VERTATTR_DESC(0, WGPUVertexFormat_Float32x3, 0),
-    // Attribute location 1: UV
+    /* Attribute location 1: UV */
     WGPU_VERTATTR_DESC(1, WGPUVertexFormat_Float32x2, sizeof(float) * 3))
 
-  // Vertex state
+  /* Vertex state */
   WGPUVertexState vertex_state = wgpu_create_vertex_state(
     wgpu_context, &(wgpu_vertex_state_t){
     .shader_desc = (wgpu_shader_desc_t){
-      // Vertex shader WGSL
+      /* Vertex shader WGSL */
       .label             = "Grid vertex shader WGSL",
       .wgsl_code.source  = grid_shader_wgsl,
       .entry             = "vertexMain",
@@ -835,11 +835,11 @@ static void prepare_render_pipeline(wgpu_context_t* wgpu_context)
     .buffers      = &triangle_vertex_buffer_layout,
   });
 
-  // Fragment state
+  /* Fragment state */
   WGPUFragmentState fragment_state = wgpu_create_fragment_state(
     wgpu_context, &(wgpu_fragment_state_t){
     .shader_desc = (wgpu_shader_desc_t){
-      // Fragment shader WGSL
+      /* Fragment shader WGSL */
       .label             = "Grid fragment shader WGSL",
       .wgsl_code.source  = grid_shader_wgsl,
       .entry             = "fragmentMain",
@@ -848,14 +848,14 @@ static void prepare_render_pipeline(wgpu_context_t* wgpu_context)
     .targets      = &color_target_state,
   });
 
-  // Multisample state
+  /* Multisample state */
   WGPUMultisampleState multisample_state
     = wgpu_create_multisample_state_descriptor(
       &(create_multisample_state_desc_t){
         .sample_count = msaa_sample_count,
       });
 
-  // Create rendering pipeline using the specified states
+  /* Create rendering pipeline using the specified states */
   pipeline = wgpuDeviceCreateRenderPipeline(
     wgpu_context->device, &(WGPURenderPipelineDescriptor){
                             .label        = "Pristine Grid render pipeline",
@@ -867,7 +867,7 @@ static void prepare_render_pipeline(wgpu_context_t* wgpu_context)
                             .multisample  = multisample_state,
                           });
 
-  // Partial cleanup
+  /* Partial cleanup */
   WGPU_RELEASE_RESOURCE(ShaderModule, vertex_state.module);
   WGPU_RELEASE_RESOURCE(ShaderModule, fragment_state.module);
 }
