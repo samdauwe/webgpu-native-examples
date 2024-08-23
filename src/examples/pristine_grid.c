b0c6ed6f95ff2c1cde5e580ddf877adbd24a941f
samdauwe <sam.dauwe@gmail.com>
2024-08-23 19:48:38 +0200

    Updated frame bind group label.

--- src/examples/pristine_grid.c
+++ src/examples/pristine_grid.c
@@ -637,7 +637,7 @@ static void setup_bind_groups(wgpu_context_t* wgpu_context)
     frame_bind_group = wgpuDeviceCreateBindGroup(
       wgpu_context->device,
       &(WGPUBindGroupDescriptor) {
-        .label      = "Frame bind group",
+        .label      = "Frame - Bind group",
         .layout     = frame_bind_group_layout,
         .entryCount = 1,
         .entries    = &(WGPUBindGroupEntry) {
