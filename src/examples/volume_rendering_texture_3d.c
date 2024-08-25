57071f03f17f1f3c9e9b0f162f664eb69a8f88eb
samdauwe <sam.dauwe@gmail.com>
2024-08-25 17:30:12 +0200

    Updated vertex shader uniform buffer block label.

--- src/examples/volume_rendering_texture_3d.c
+++ src/examples/volume_rendering_texture_3d.c
@@ -150,7 +150,7 @@ static void prepare_uniform_buffer(wgpu_example_context_t* context)
   uniform_buffer_vs = wgpu_create_buffer(
     context->wgpu_context,
     &(wgpu_buffer_desc_t){
-      .label = "Vertex shader uniform buffer block",
+      .label = "Vertex shader - Uniform buffer block",
       .usage = WGPUBufferUsage_Uniform | WGPUBufferUsage_CopyDst,
       .size  = sizeof(ubo_vs),
     });
