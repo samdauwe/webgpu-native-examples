72592a2ec1008a79f6103ae60d3d73d95d86e33a
samdauwe <sam.dauwe@gmail.com>
2024-08-29 20:11:37 +0200

    Updated cube uniform buffer bind group label.

--- src/examples/textured_cube.c
+++ src/examples/textured_cube.c
@@ -247,7 +247,7 @@ static void setup_bind_groups(wgpu_context_t* wgpu_context)
     }
   };
   WGPUBindGroupDescriptor bg_desc = {
-    .label      = "Cube uniform buffer bind group",
+    .label      = "Cube uniform - Buffer bind group",
     .layout     = cube.bind_group_layout,
     .entryCount = (uint32_t)ARRAY_SIZE(bg_entries),
     .entries    = bg_entries,
