dd52ddba5198426e57b0de1cbed782c26a47bc20
samdauwe <sam.dauwe@gmail.com>
2024-09-01 18:02:48 +0200

    Updated 3D object bind group label.

--- src/examples/texture_cubemap.c
+++ src/examples/texture_cubemap.c
@@ -211,7 +211,7 @@ static void setup_bind_groups(wgpu_context_t* wgpu_context)
     };
 
     WGPUBindGroupDescriptor bg_desc = {
-      .label      = "Skybox - Bind group",
+      .label      = "3D object - Bind group",
       .layout     = bind_group_layout,
       .entryCount = (uint32_t)ARRAY_SIZE(bg_entries),
       .entries    = bg_entries,
