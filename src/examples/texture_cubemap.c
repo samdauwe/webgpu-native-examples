1aca3ff70b6b24b38579e3aec4b443719c9393c1
samdauwe <sam.dauwe@gmail.com>
2024-09-03 20:57:25 +0200

    Updated reflect vertex shader label.

--- src/examples/texture_cubemap.c
+++ src/examples/texture_cubemap.c
@@ -354,7 +354,7 @@ static void prepare_pipelines(wgpu_context_t* wgpu_context)
             wgpu_context, &(wgpu_vertex_state_t){
             .shader_desc = (wgpu_shader_desc_t){
               // Vertex shader SPIR-V
-              .label = "Reflect vertex shader",
+              .label = "Reflect - Vertex shader SPIR-V",
               .file  = "shaders/texture_cubemap/reflect.vert.spv",
             },
             .buffer_count = 1,
