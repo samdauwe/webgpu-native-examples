a10c28fdc60833fbd7808edb5a622293408ef1eb
samdauwe <sam.dauwe@gmail.com>
2024-09-04 20:07:50 +0200

    Updated commenting style.

--- src/examples/texture_cubemap.c
+++ src/examples/texture_cubemap.c
@@ -394,7 +394,7 @@ static void prepare_pipelines(wgpu_context_t* wgpu_context)
 
 static void setup_render_pass(wgpu_context_t* wgpu_context)
 {
-  // Color attachment
+  /* Color attachment */
   render_pass.color_attachments[0] = (WGPURenderPassColorAttachment) {
       .view       = NULL, /* Assigned later */
       .depthSlice = ~0,
@@ -408,10 +408,10 @@ static void setup_render_pass(wgpu_context_t* wgpu_context)
       },
   };
 
-  // Depth attachment
+  /* Depth attachment */
   wgpu_setup_deph_stencil(wgpu_context, NULL);
 
-  // Render pass descriptor
+  /* Render pass descriptor */
   render_pass.descriptor = (WGPURenderPassDescriptor){
     .label                  = "Render pass descriptor",
     .colorAttachmentCount   = 1,
