4872f6ea665b4f7829f06754e8d3850b719bae4d
samdauwe <sam.dauwe@gmail.com>
2024-09-13 19:50:37 +0200

    Updated terrain mesh vertex shader label.

--- src/examples/terrain_mesh.c
+++ src/examples/terrain_mesh.c
@@ -563,7 +563,7 @@ static void prepare_pipelines(wgpu_context_t* wgpu_context)
                     wgpu_context, &(wgpu_vertex_state_t){
                     .shader_desc = (wgpu_shader_desc_t){
                       // Vertex shader SPIR-V
-                      .label = "Terrain mesh vertex shader",
+                      .label = "Terrain mesh - Vertex shader",
                       .file  = "shaders/terrain_mesh/shader.vert.spv",
                     },
                     .buffer_count = 1,
