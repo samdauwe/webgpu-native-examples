3fd604acbc8ddfab374902258efb1d2da04ea1e7
samdauwe <sam.dauwe@gmail.com>
2024-09-15 17:36:46 +0200

    Enabled V-Sync

--- src/examples/terrain_mesh.c
+++ src/examples/terrain_mesh.c
@@ -719,6 +719,7 @@ void example_terrain_mesh(int argc, char* argv[])
   example_run(argc, argv, &(refexport_t){
     .example_settings = (wgpu_example_settings_t){
       .title = example_title,
+      .vsync = true,
     },
     .example_initialize_func = &example_initialize,
     .example_render_func     = &example_render,
