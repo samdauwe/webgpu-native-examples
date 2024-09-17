8b71512680d08540b2d717e70b7b18a79e5fc75b
samdauwe <sam.dauwe@gmail.com>
2024-09-17 19:47:52 +0200

    Updated stencil render pipeline label.

--- src/examples/stencil_buffer.c
+++ src/examples/stencil_buffer.c
@@ -244,7 +244,7 @@ static void prepare_pipelines(wgpu_context_t* wgpu_context)
     // Create rendering pipeline using the specified states
     pipelines.stencil = wgpuDeviceCreateRenderPipeline(
       wgpu_context->device, &(WGPURenderPipelineDescriptor){
-                              .label        = "Stencil render pipeline",
+                              .label        = "Stencil - Render pipeline",
                               .layout       = pipeline_layout,
                               .primitive    = primitive_state,
                               .vertex       = vertex_state,
