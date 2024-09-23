e0c99793908da61fcffadacd646adb0610c1c91c
samdauwe <sam.dauwe@gmail.com>
2024-09-23 20:55:34 +0200

    Updated instance model matrix label.

--- src/examples/post_processing.c
+++ src/examples/post_processing.c
@@ -906,7 +906,7 @@ static void generate_gpu_buffers_from_instanced_geometry(
   /* Instance model matrix */
   gpu_buffers->normal_matrix = wgpu_create_buffer(
     wgpu_context, &(wgpu_buffer_desc_t){
-                    .label = "Instance model matrix",
+                    .label = "Instance - Model matrix",
                     .usage = WGPUBufferUsage_CopyDst | WGPUBufferUsage_Vertex,
                     .size  = sizeof(geometry->normal_matrix_data),
                     .initial.data = geometry->normal_matrix_data,
