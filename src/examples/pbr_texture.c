34d76d397162eae16dbadfc5a3b1c33a3528ccde
samdauwe <sam.dauwe@gmail.com>
2024-10-10 20:19:20 +0200

    Updated object bind group layout label.

--- src/examples/pbr_texture.c
+++ src/examples/pbr_texture.c
@@ -357,7 +357,7 @@ static void setup_bind_group_layouts(wgpu_context_t* wgpu_context)
     };
     bind_group_layouts.object = wgpuDeviceCreateBindGroupLayout(
       wgpu_context->device, &(WGPUBindGroupLayoutDescriptor){
-                              .label      = "object_bind_group_layout",
+                              .label      = "Object - Bind group layout",
                               .entryCount = (uint32_t)ARRAY_SIZE(bgl_entries),
                               .entries    = bgl_entries,
                             });
