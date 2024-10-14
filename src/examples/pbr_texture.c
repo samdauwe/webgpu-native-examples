e5e3e75b72a64ce07dc26b03b86ec2485587a1c4
samdauwe <sam.dauwe@gmail.com>
2024-10-14 20:01:09 +0200

    Updated PBR texture vertex shader label.

--- src/examples/pbr_texture.c
+++ src/examples/pbr_texture.c
@@ -732,7 +732,7 @@ static void prepare_pipelines(wgpu_context_t* wgpu_context)
             wgpu_context, &(wgpu_vertex_state_t){
             .shader_desc = (wgpu_shader_desc_t){
               // Vertex shader WGSL
-              .label            = "PBR texture vertex shader",
+              .label            = "PBR texture - Vertex shader",
               .wgsl_code.source = pbr_texture_vertex_shader_wgsl,
               .entry            = "main",
             },
