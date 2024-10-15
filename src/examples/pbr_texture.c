f50cd58fde33a5229866f32166e5cb272cd237a5
samdauwe <sam.dauwe@gmail.com>
2024-10-15 20:12:32 +0200

    Updated PBR texture fragment shader label.

--- src/examples/pbr_texture.c
+++ src/examples/pbr_texture.c
@@ -748,7 +748,7 @@ static void prepare_pipelines(wgpu_context_t* wgpu_context)
             wgpu_context, &(wgpu_fragment_state_t){
             .shader_desc = (wgpu_shader_desc_t){
               // Fragment shader WGSL
-              .label            = "PBR texture fragment shader",
+              .label            = "PBR texture - Fragment shader",
               .wgsl_code.source = fragment_shader_wgsl,
               .entry            = "main",
             },
