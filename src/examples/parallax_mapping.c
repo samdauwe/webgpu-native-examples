50043b935973ce02b2d87ba61a55bda886fa7fc7
samdauwe <sam.dauwe@gmail.com>
2024-10-19 19:50:25 +0200

    Updated fragment shader uniform buffer label.

--- src/examples/parallax_mapping.c
+++ src/examples/parallax_mapping.c
@@ -400,7 +400,7 @@ static void prepare_uniform_buffers(wgpu_example_context_t* context)
   uniform_buffers.fragment_shader = wgpu_create_buffer(
     context->wgpu_context,
     &(wgpu_buffer_desc_t){
-      .label = "Fragment shader - uniform buffer",
+      .label = "Fragment shader - Uniform buffer",
       .usage = WGPUBufferUsage_CopyDst | WGPUBufferUsage_Uniform,
       .size  = sizeof(ubos.fragment_shader),
     });
