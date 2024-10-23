a8b9b699b28e927df05f2d723c4ee18ea4e410f9
samdauwe <sam.dauwe@gmail.com>
2024-10-23 20:02:41 +0200

    Added offscreen bind group label.

--- src/examples/offscreen_rendering.c
+++ src/examples/offscreen_rendering.c
@@ -393,6 +393,7 @@ static void setup_bind_groups(wgpu_context_t* wgpu_context)
     };
     bind_groups.offscreen = wgpuDeviceCreateBindGroup(
       wgpu_context->device, &(WGPUBindGroupDescriptor){
+                              .label      = "Offscreen - Bind group",
                               .layout     = bind_group_layouts.shaded,
                               .entryCount = 1,
                               .entries    = &bg_entry,
