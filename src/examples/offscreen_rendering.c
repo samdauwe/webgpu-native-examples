74facfad690b1b8c4a6ffc09cde1e3d311bcfb21
samdauwe <sam.dauwe@gmail.com>
2024-10-24 21:00:01 +0200

    Updated quad fragment shader label.

--- src/examples/offscreen_rendering.c
+++ src/examples/offscreen_rendering.c
@@ -497,7 +497,7 @@ static void prepare_pipelines(wgpu_context_t* wgpu_context)
               wgpu_context, &(wgpu_fragment_state_t){
               .shader_desc = (wgpu_shader_desc_t){
                 // Fragment shader SPIR-V
-                .label = "quad_fragment_shader",
+                .label = "Quad - Fragment shader",
                 .file  = "shaders/offscreen_rendering/quad.frag.spv",
               },
               .target_count = 1,
