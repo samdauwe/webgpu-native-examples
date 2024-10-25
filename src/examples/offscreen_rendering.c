a9ea600fa6c1aad19bc71fb7adaa4a84cb32a484
samdauwe <sam.dauwe@gmail.com>
2024-10-25 19:53:26 +0200

    Updated mirror vertex shader label.

--- src/examples/offscreen_rendering.c
+++ src/examples/offscreen_rendering.c
@@ -523,7 +523,7 @@ static void prepare_pipelines(wgpu_context_t* wgpu_context)
               wgpu_context, &(wgpu_vertex_state_t){
               .shader_desc = (wgpu_shader_desc_t){
                 // Vertex shader SPIR-V
-                .label = "mirror_vertex_shader",
+                .label = "Mirror - Vertex shader",
                 .file  = "shaders/offscreen_rendering/mirror.vert.spv",
               },
               .buffer_count = 1,
