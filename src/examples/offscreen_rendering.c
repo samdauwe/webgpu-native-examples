c4e94402c58cf812ca487af07eb6d302c11079a4
samdauwe <sam.dauwe@gmail.com>
2024-10-26 19:16:29 +0200

    Updated phong fragment shader label.

--- src/examples/offscreen_rendering.c
+++ src/examples/offscreen_rendering.c
@@ -576,7 +576,7 @@ static void prepare_pipelines(wgpu_context_t* wgpu_context)
               wgpu_context, &(wgpu_fragment_state_t){
               .shader_desc = (wgpu_shader_desc_t){
                 // Fragment shader SPIR-V
-                .label = "phong_fragment_shader",
+                .label = "Phong - Fragment shader",
                 .file  = "shaders/offscreen_rendering/phong.frag.spv",
               },
               .target_count = 1,
