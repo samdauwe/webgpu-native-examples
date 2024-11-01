a9d9ac1b26d3dc003f9300593b5f99751e7f2676
samdauwe <sam.dauwe@gmail.com>
2024-11-01 19:32:27 +0100

    Updated torus knot tangents buffer label.

--- src/examples/normal_mapping.c
+++ src/examples/normal_mapping.c
@@ -655,7 +655,7 @@ static void prepare_buffers(wgpu_context_t* wgpu_context)
   /* Tangent buffer */
   buffers.torus_knot.tangent = wgpu_create_buffer(
     wgpu_context, &(wgpu_buffer_desc_t){
-                    .label = "Torus knot tangents buffer",
+                    .label = "Torus knot - Tangents buffer",
                     .usage = WGPUBufferUsage_CopyDst | WGPUBufferUsage_Vertex,
                     .size  = sizeof(torus_knot_mesh.tangents),
                     .initial.data = torus_knot_mesh.tangents,
