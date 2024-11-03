f49008916f83fbf157b9fb870744867a82f38718
samdauwe <sam.dauwe@gmail.com>
2024-11-03 19:19:47 +0100

    Updated plane tangents buffer label.

--- src/examples/normal_mapping.c
+++ src/examples/normal_mapping.c
@@ -712,7 +712,7 @@ static void prepare_buffers(wgpu_context_t* wgpu_context)
   /* Tangent buffer */
   buffers.plane.tangent = wgpu_create_buffer(
     wgpu_context, &(wgpu_buffer_desc_t){
-                    .label = "Plane tangents buffer",
+                    .label = "Plane - Tangents buffer",
                     .usage = WGPUBufferUsage_CopyDst | WGPUBufferUsage_Vertex,
                     .size  = sizeof(plane_mesh.tangents),
                     .initial.data = plane_mesh.tangents,
