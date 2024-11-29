a45a4a1dd4d6f8c73d4cf5d7bf0e20e9f2de0b03
samdauwe <sam.dauwe@gmail.com>
2024-11-29 20:45:45 +0100

    Updated instanced cube render pipeline label.

--- src/examples/instanced_cube.c
+++ src/examples/instanced_cube.c
@@ -297,7 +297,7 @@ static void prepare_pipeline(wgpu_context_t* wgpu_context)
   // Create rendering pipeline using the specified states
   pipeline = wgpuDeviceCreateRenderPipeline(
     wgpu_context->device, &(WGPURenderPipelineDescriptor){
-                            .label        = "Instanced cube render pipeline",
+                            .label        = "Instanced cube - Render pipeline",
                             .primitive    = primitive_state,
                             .vertex       = vertex_state,
                             .fragment     = &fragment_state,
