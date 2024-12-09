39bdfc23cb01842bebbc2865b0b60c15d91ccf54
samdauwe <sam.dauwe@gmail.com>
2024-12-09 20:57:10 +0100

    Updated vertex shader label.

--- src/examples/image_blur.c
+++ src/examples/image_blur.c
@@ -358,7 +358,7 @@ static void prepare_pipelines(wgpu_context_t* wgpu_context)
                   wgpu_context, &(wgpu_vertex_state_t){
                   .shader_desc = (wgpu_shader_desc_t){
                     /* Vertex shader WGSL */
-                    .label            = "Fullscreen textured quad WGSL",
+                    .label            = "Fullscreen textured quad - Vertex shader WGSL",
                     .wgsl_code.source = fullscreen_textured_quad_wgsl,
                     .entry            = "vert_main"
                   },
