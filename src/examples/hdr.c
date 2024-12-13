a7c303c5a0e2861c38fc71f61167d3cc2de8b8b5
samdauwe <sam.dauwe@gmail.com>
2024-12-13 20:28:20 +0100

    Updated models bind group layout label.

--- src/examples/hdr.c
+++ src/examples/hdr.c
@@ -441,7 +441,7 @@ static void setup_pipeline_layout(wgpu_context_t* wgpu_context)
     // Create the bind group layout
     bind_group_layouts.models = wgpuDeviceCreateBindGroupLayout(
       wgpu_context->device, &(WGPUBindGroupLayoutDescriptor){
-                              .label      = "Models bind group layout",
+                              .label      = "Models - Bind group layout",
                               .entryCount = (uint32_t)ARRAY_SIZE(bgl_entries),
                               .entries    = bgl_entries,
                             });
