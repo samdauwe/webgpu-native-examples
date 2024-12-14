5f995620ccf7a9a7bd1ba8b88c9f1ff12dd2278b
samdauwe <sam.dauwe@gmail.com>
2024-12-14 19:27:26 +0100

    Updated G-Buffer composition pipeline layout label.

--- src/examples/hdr.c
+++ src/examples/hdr.c
@@ -551,7 +551,7 @@ static void setup_pipeline_layout(wgpu_context_t* wgpu_context)
       pipeline_layouts.composition = wgpuDeviceCreatePipelineLayout(
         wgpu_context->device,
         &(WGPUPipelineLayoutDescriptor){
-          .label                = "G-Buffer composition pipeline layout",
+          .label                = "G-Buffer composition - Pipeline layout",
           .bindGroupLayoutCount = 1,
           .bindGroupLayouts     = &bind_group_layouts.composition,
         });
