80676e95a9ebfec3b0f73572edf6ce467c1f4803
samdauwe <sam.dauwe@gmail.com>
2024-12-16 17:00:17 +0100

    Updated bloom filter bind group label.

--- src/examples/hdr.c
+++ src/examples/hdr.c
@@ -658,7 +658,7 @@ static void setup_bind_groups(wgpu_context_t* wgpu_context)
     };
 
     WGPUBindGroupDescriptor bg_desc = {
-      .label      = "Bloom filter bind group",
+      .label      = "Bloom filter - Bind group",
       .layout     = bind_group_layouts.bloom_filter,
       .entryCount = (uint32_t)ARRAY_SIZE(bg_entries),
       .entries    = bg_entries,
