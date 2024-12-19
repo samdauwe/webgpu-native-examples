5af2e2df0fe2c33762a5f3aa0edfebf9c7bf44e3
samdauwe <sam.dauwe@gmail.com>
2024-12-19 19:15:54 +0100

    Updated G-Buffer fragment shader label.

--- src/examples/hdr.c
+++ src/examples/hdr.c
@@ -926,7 +926,7 @@ static void prepare_pipelines(wgpu_context_t* wgpu_context)
           wgpu_context, &(wgpu_fragment_state_t){
             .shader_desc = (wgpu_shader_desc_t){
               // Fragment shader SPIR-V
-              .label = "G-Buffer fragment shader SPIR-V",
+              .label = "G-Buffer - Fragment shader SPIR-V",
               .file = "shaders/hdr/gbuffer.frag.spv",
             },
             .target_count = 2,
