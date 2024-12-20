0e2af47d344fb3895ffaf2e6fd75f717365f41e3
samdauwe <sam.dauwe@gmail.com>
2024-12-20 17:35:14 +0100

    Updated reflect render pipeline label.

--- src/examples/hdr.c
+++ src/examples/hdr.c
@@ -962,7 +962,7 @@ static void prepare_pipelines(wgpu_context_t* wgpu_context)
       // Create rendering pipeline using the specified states
       pipelines.reflect = wgpuDeviceCreateRenderPipeline(
         wgpu_context->device, &(WGPURenderPipelineDescriptor){
-                                .label        = "Reflect render pipeline",
+                                .label        = "Reflect - Render pipeline",
                                 .layout       = pipeline_layouts.models,
                                 .primitive    = primitive_state_desc,
                                 .vertex       = vertex_state_desc,
