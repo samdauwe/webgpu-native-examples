b21376c30c3b1a4d67848a2a3fe573d1f2b4d4be
samdauwe <sam.dauwe@gmail.com>
2024-12-22 17:53:14 +0100

    Updated UBO Scene bind group layout label.

--- src/examples/gltf_skinning.c
+++ src/examples/gltf_skinning.c
@@ -107,7 +107,7 @@ static void setup_pipeline_layout(wgpu_context_t* wgpu_context)
         },
     };
     WGPUBindGroupLayoutDescriptor bgl_desc = {
-      .label      = "UBOScene bind group layout",
+      .label      = "UBOScene - Bind group layout",
       .entryCount = (uint32_t)ARRAY_SIZE(bgl_entries),
       .entries    = bgl_entries,
     };
