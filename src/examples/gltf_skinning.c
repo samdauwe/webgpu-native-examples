154e3ad7f92055c7faeffb613833dde28008ed36
samdauwe <sam.dauwe@gmail.com>
2024-12-26 19:25:50 +0100

    Updated GLTF skinned model vertex label.

--- src/examples/gltf_skinning.c
+++ src/examples/gltf_skinning.c
@@ -363,7 +363,7 @@ static void prepare_pipelines(wgpu_context_t* wgpu_context)
             wgpu_context, &(wgpu_vertex_state_t){
             .shader_desc = (wgpu_shader_desc_t){
               // Vertex shader WGSL
-              .label            = "GLTF skinned model vertex WGSL",
+              .label            = "GLTF skinned model - Vertex shader WGSL",
               .wgsl_code.source = skinned_model_vertex_shader_wgsl,
               .entry            = "main",
             },
