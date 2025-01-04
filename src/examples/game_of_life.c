ceb2ebe052737d9432460fa06f71902c3a2c2692
samdauwe <sam.dauwe@gmail.com>
2025-01-04 13:51:11 +0100

    Updated commenting style.

--- src/examples/game_of_life.c
+++ src/examples/game_of_life.c
@@ -551,19 +551,19 @@ static WGPUCommandBuffer build_command_buffer(wgpu_context_t* wgpu_context)
 
 static int example_draw(wgpu_example_context_t* context)
 {
-  // Prepare frame
+  /* Prepare frame */
   prepare_frame(context);
 
-  // Command buffer to be submitted to the queue
+  /* Command buffer to be submitted to the queue */
   wgpu_context_t* wgpu_context                   = context->wgpu_context;
   wgpu_context->submit_info.command_buffer_count = 1;
   wgpu_context->submit_info.command_buffers[0]
     = build_command_buffer(context->wgpu_context);
 
-  // Submit command buffer to queue
+  /* Submit command buffer to queue */
   submit_command_buffers(context);
 
-  // Submit frame
+  /* Submit frame */
   submit_frame(context);
 
   return EXIT_SUCCESS;
