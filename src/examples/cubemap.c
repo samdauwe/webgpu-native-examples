5a4dcdc2ecafea8002456cc1c232b874b70e4f02
samdauwe <sam.dauwe@gmail.com>
2025-01-25 18:26:35 +0100

    Updated cubemap vertex shader label.

--- src/examples/cubemap.c
+++ src/examples/cubemap.c
@@ -335,7 +335,7 @@ static void prepare_pipelines(wgpu_context_t* wgpu_context)
          wgpu_context, &(wgpu_vertex_state_t){
          .shader_desc = (wgpu_shader_desc_t){
             // Vertex shader WGSL
-            .label            = "Cubemap vertex shader WGSL",
+            .label            = "Cubemap - Vertex shader WGSL",
             .wgsl_code.source = basic_vertex_shader_wgsl,
             .entry            = "main",
          },
