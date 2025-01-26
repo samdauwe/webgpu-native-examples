cbe52fd2310227b79e4247ef6d51cbb30c06d90d
samdauwe <sam.dauwe@gmail.com>
2025-01-26 18:09:52 +0100

    Updated cubemap render pipeline label.

--- src/examples/cubemap.c
+++ src/examples/cubemap.c
@@ -366,7 +366,7 @@ static void prepare_pipelines(wgpu_context_t* wgpu_context)
   // Create rendering pipeline using the specified states
   pipeline = wgpuDeviceCreateRenderPipeline(
     wgpu_context->device, &(WGPURenderPipelineDescriptor){
-                            .label        = "Cubemap render pipeline",
+                            .label        = "Cubemap - Render pipeline",
                             .layout       = pipeline_layout,
                             .primitive    = primitive_state,
                             .vertex       = vertex_state,
