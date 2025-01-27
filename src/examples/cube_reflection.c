705e4845cc5a22c96e750dc8583ea33baf7f2cad
samdauwe <sam.dauwe@gmail.com>
2025-01-27 20:00:40 +0100

    Updated cube vertex shader label.

--- src/examples/cube_reflection.c
+++ src/examples/cube_reflection.c
@@ -311,7 +311,7 @@ static void prepare_pipelines(wgpu_context_t* wgpu_context)
             wgpu_context, &(wgpu_vertex_state_t){
             .shader_desc = (wgpu_shader_desc_t){
               /* Vertex shader SPIR-V */
-              .label = "Cube vertex shader SPIR-V",
+              .label = "Cube - Vertex shader SPIR-V",
               .file  = "shaders/cube_reflection/shader.vert.spv",
             },
             .buffer_count = 1,
