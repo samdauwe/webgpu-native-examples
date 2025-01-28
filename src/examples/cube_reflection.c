dce4e6679249ff509f7fad64c16e40fc10aa659e
samdauwe <sam.dauwe@gmail.com>
2025-01-28 20:07:03 +0100

    Updated camera data buffer label.

--- src/examples/cube_reflection.c
+++ src/examples/cube_reflection.c
@@ -140,7 +140,7 @@ static void prepare_uniform_buffers(wgpu_context_t* wgpu_context)
   {
     camera_buffer
       = wgpu_create_buffer(wgpu_context, &(wgpu_buffer_desc_t){
-                                           .label = "Camera data buffer",
+                                           .label = "Camera - Data buffer",
                                            .usage = WGPUBufferUsage_CopyDst
                                                     | WGPUBufferUsage_Uniform,
                                            .size = sizeof(camera_data),
