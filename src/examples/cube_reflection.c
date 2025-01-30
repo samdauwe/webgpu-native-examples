a01f03e4bca2a66026fbc6b7200f8223eb30e779
samdauwe <sam.dauwe@gmail.com>
2025-01-30 20:26:27 +0100

    Updated cube render pipeline label.

--- src/examples/cube_reflection.c
+++ src/examples/cube_reflection.c
@@ -360,7 +360,7 @@ static void prepare_pipelines(wgpu_context_t* wgpu_context)
     /* Create rendering pipeline using the specified states */
     pipeline = wgpuDeviceCreateRenderPipeline(
       wgpu_context->device, &(WGPURenderPipelineDescriptor){
-                              .label        = "Cube render pipeline",
+                              .label        = "Cube - Render pipeline",
                               .layout       = pipeline_layout,
                               .primitive    = primitive_state,
                               .vertex       = vertex_state,
