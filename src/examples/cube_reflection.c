ea1e97ce0292b97e95f0c4c08f17521f0c2074b7
samdauwe <sam.dauwe@gmail.com>
2025-01-31 20:03:39 +0100

    Updated uniform bind group label.

--- src/examples/cube_reflection.c
+++ src/examples/cube_reflection.c
@@ -270,7 +270,7 @@ static void setup_bind_groups(wgpu_context_t* wgpu_context)
       },
     };
     WGPUBindGroupDescriptor bg_desc = {
-      .label      = "Bind group",
+      .label      = "Uniform - Bind group",
       .layout     = bind_group_layout,
       .entryCount = (uint32_t)ARRAY_SIZE(bg_entries),
       .entries    = bg_entries,
