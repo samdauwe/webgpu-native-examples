f34a724e468ef74590d7c0e5a94e639ec1ee83b3
samdauwe <sam.dauwe@gmail.com>
2025-02-05 19:56:03 +0100

    Updated radiosity bind group layout label.

--- src/examples/cornell_box.c
+++ src/examples/cornell_box.c
@@ -878,7 +878,7 @@ static void radiosity_create(radiosity_t* this, wgpu_context_t* wgpu_context,
     };
     this->bind_group_layout = wgpuDeviceCreateBindGroupLayout(
       wgpu_context->device, &(WGPUBindGroupLayoutDescriptor){
-                              .label      = "Radiosity bind group layout",
+                              .label      = "Radiosity - Bind group layout",
                               .entryCount = (uint32_t)ARRAY_SIZE(bgl_entries),
                               .entries    = bgl_entries,
                             });
