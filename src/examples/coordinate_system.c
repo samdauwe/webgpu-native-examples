48867836bac237b02dd82cff69f0cc049a117d5a
samdauwe <sam.dauwe@gmail.com>
2025-02-24 20:05:35 +0100

    Updated quad vertex shader label.

--- src/examples/coordinate_system.c
+++ src/examples/coordinate_system.c
@@ -320,7 +320,7 @@ static void prepare_pipelines(wgpu_context_t* wgpu_context)
                     wgpu_context, &(wgpu_vertex_state_t){
                     .shader_desc = (wgpu_shader_desc_t){
                       // Vertex shader WGSL
-                      .label            = "Quad vertex shader WGSL",
+                      .label            = "Quad - Vertex shader WGSL",
                       .wgsl_code.source = quad_vertex_shader_wgsl,
                       .entry            = "main",
                     },
