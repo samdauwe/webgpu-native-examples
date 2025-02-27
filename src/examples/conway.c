f6c456138803274d75aae31d047cc31c01dda784
samdauwe <sam.dauwe@gmail.com>
2025-02-27 20:08:30 +0100

    Updated graphics bind group layout label.

--- src/examples/conway.c
+++ src/examples/conway.c
@@ -257,7 +257,7 @@ static void setup_pipeline_layouts(wgpu_context_t* wgpu_context)
       }
     };
     WGPUBindGroupLayoutDescriptor bgl_desc = {
-      .label      = "Graphics bind group layout",
+      .label      = "Graphics - Bind group layout",
       .entryCount = (uint32_t)ARRAY_SIZE(bgl_entries),
       .entries    = bgl_entries,
     };
