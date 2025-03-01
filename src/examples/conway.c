7aa24c0a21f121534afa2c5c64461159eb491c19
samdauwe <sam.dauwe@gmail.com>
2025-03-01 18:33:42 +0100

    Updated graphics vertex shader label.

--- src/examples/conway.c
+++ src/examples/conway.c
@@ -412,7 +412,7 @@ static void prepare_pipelines(wgpu_context_t* wgpu_context)
           wgpu_context, &(wgpu_vertex_state_t){
             .shader_desc = (wgpu_shader_desc_t){
               // Vertex shader WGSL
-              .label            = "Graphics vertex shader WGSL",
+              .label            = "Graphics - Vertex shader WGSL",
               .wgsl_code.source = graphics_vertex_shader_wgsl,
               .entry            = "main",
             },
