d868755ec5441a86616933b9b6d3fb758ca46329
samdauwe <sam.dauwe@gmail.com>
2025-03-14 19:48:06 +0100

    Updated full resolution render pass label.

--- src/examples/conservative_raster.c
+++ src/examples/conservative_raster.c
@@ -482,7 +482,7 @@ static WGPUCommandBuffer build_command_buffer(wgpu_context_t* wgpu_context)
   {
     /* Render pass descriptor */
     WGPURenderPassDescriptor render_pass_desc = (WGPURenderPassDescriptor){
-      .label                  = "Full resolution render pass",
+      .label                  = "Full resolution - Render pass",
       .colorAttachmentCount   = 1,
       .colorAttachments       = &(WGPURenderPassColorAttachment) {
         .view       = view,
