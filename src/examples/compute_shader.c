0093c716d468d488b44911836745d3b213764c41
samdauwe <sam.dauwe@gmail.com>
2025-03-18 20:28:37 +0100

    Updated graphics render pipeline label.

--- src/examples/compute_shader.c
+++ src/examples/compute_shader.c
@@ -422,7 +422,7 @@ static void prepare_pipelines(wgpu_context_t* wgpu_context)
   // Create rendering pipeline using the specified states
   graphics.pipeline = wgpuDeviceCreateRenderPipeline(
     wgpu_context->device, &(WGPURenderPipelineDescriptor){
-                            .label        = "Graphics render pipeline",
+                            .label        = "Graphics - Render pipeline",
                             .layout       = graphics.pipeline_layout,
                             .primitive    = primitive_state,
                             .vertex       = vertex_state,
