3ec1f0ba5c773b28f8fda20dda2be40f2e243fb4
samdauwe <sam.dauwe@gmail.com>
2025-03-22 19:24:12 +0100

    Added graphics bind group layout label.

--- src/examples/compute_ray_tracing.c
+++ src/examples/compute_ray_tracing.c
@@ -296,6 +296,7 @@ static void setup_pipeline_layout(wgpu_context_t* wgpu_context)
     }
   };
   WGPUBindGroupLayoutDescriptor bgl_desc = {
+    .label      = "Graphics - Bind group layout",
     .entryCount = (uint32_t)ARRAY_SIZE(bgl_entries),
     .entries    = bgl_entries,
   };
