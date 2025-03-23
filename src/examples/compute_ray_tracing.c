1d13f42704051c38bcdc9d127345a6b5c4144473
samdauwe <sam.dauwe@gmail.com>
2025-03-23 17:47:38 +0100

    Updated fragment vertex shader label.

--- src/examples/compute_ray_tracing.c
+++ src/examples/compute_ray_tracing.c
@@ -382,7 +382,7 @@ static void prepare_pipelines(wgpu_context_t* wgpu_context)
                     wgpu_context, &(wgpu_fragment_state_t){
                     .shader_desc = (wgpu_shader_desc_t){
                       // Fragment shader SPIR-V
-                      .label = "Fragment vertex shader SPIR-V",
+                      .label = "Fragment - Vertex shader SPIR-V",
                       .file = "shaders/compute_ray_tracing/texture.frag.spv",
                     },
                     .target_count = 1,
