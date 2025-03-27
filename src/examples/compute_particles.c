adbf93c77ba0285260d65f924e76cbd8d417b454
samdauwe <sam.dauwe@gmail.com>
2025-03-27 20:24:02 +0100

    Updated particle vertex shader label.

--- src/examples/compute_particles.c
+++ src/examples/compute_particles.c
@@ -290,7 +290,7 @@ static void prepare_pipelines(wgpu_context_t* wgpu_context)
                 wgpu_context, &(wgpu_vertex_state_t){
                 .shader_desc = (wgpu_shader_desc_t){
                   // Vertex shader SPIR-V
-                  .label = "Particle vertex shader SPIR-V",
+                  .label = "Particle - Vertex shader SPIR-V",
                   .file  = "shaders/compute_particles/particle.vert.spv",
                 },
                 .buffer_count = 1,
