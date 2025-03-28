23d4b98086f056f92ee88d8747872dec920327bc
samdauwe <sam.dauwe@gmail.com>
2025-03-28 20:10:44 +0100

    Updated particle color and gradient bind group label.

--- src/examples/compute_particles.c
+++ src/examples/compute_particles.c
@@ -360,7 +360,7 @@ static void setup_bind_groups(wgpu_context_t* wgpu_context)
   };
 
   WGPUBindGroupDescriptor bg_desc = {
-    .label      = "Particle color and gradient bind group",
+    .label      = "Particle color and gradient - Bind group",
     .layout     = graphics.bind_group_layout,
     .entryCount = (uint32_t)ARRAY_SIZE(bg_entries),
     .entries    = bg_entries,
