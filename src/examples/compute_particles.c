b694b885995749ad6e326f00fc7ac9828bfc086b
samdauwe <sam.dauwe@gmail.com>
2025-03-29 18:06:54 +0100

    Updated graphics bind group layout label.

--- src/examples/compute_particles.c
+++ src/examples/compute_particles.c
@@ -226,7 +226,7 @@ static void setup_pipeline_layout(wgpu_context_t* wgpu_context)
     }
   };
   WGPUBindGroupLayoutDescriptor bgl_desc = {
-    .label      = "Bind group layout",
+    .label      = "Graphics - Bind group layout",
     .entryCount = (uint32_t)ARRAY_SIZE(bgl_entries),
     .entries    = bgl_entries,
   };
