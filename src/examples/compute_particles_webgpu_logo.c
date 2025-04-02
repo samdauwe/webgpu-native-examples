4b4f6d8c5ad9a1c0e66e0fd8cda4f5e642f9fc22
samdauwe <sam.dauwe@gmail.com>
2025-04-02 20:09:59 +0200

    Added uniform buffer label.

--- src/examples/compute_particles_webgpu_logo.c
+++ src/examples/compute_particles_webgpu_logo.c
@@ -269,6 +269,7 @@ static void prepare_uniform_buffer(wgpu_context_t* wgpu_context)
 {
   uniform_buffer_vs.buffer = wgpu_create_buffer(
     wgpu_context, &(wgpu_buffer_desc_t){
+                    .label = "Uniform buffer",
                     .usage = WGPUBufferUsage_CopyDst | WGPUBufferUsage_Uniform,
                     .size  = sizeof(uniform_buffer_vs.data),
                   });
