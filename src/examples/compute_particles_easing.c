331c8700070ebd5353425f147cad6dac07814fc3
samdauwe <sam.dauwe@gmail.com>
2025-04-17 20:02:34 +0200

    Updated graphics uniforms bind group label.

--- src/examples/compute_particles_easing.c
+++ src/examples/compute_particles_easing.c
@@ -315,7 +315,7 @@ static void setup_bind_groups(wgpu_context_t* wgpu_context)
   };
 
   WGPUBindGroupDescriptor bg_desc = {
-    .label      = "Bind group",
+    .label      = "Graphics uniforms - Bind group",
     .layout     = graphics.uniforms_bind_group_layout,
     .entryCount = (uint32_t)ARRAY_SIZE(bg_entries),
     .entries    = bg_entries,
