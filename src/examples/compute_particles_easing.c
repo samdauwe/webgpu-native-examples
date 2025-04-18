45a8e64cda12346ef182c08f53a149687b67f6c6
samdauwe <sam.dauwe@gmail.com>
2025-04-18 20:14:49 +0200

    Updated particle fragment shader label.

--- src/examples/compute_particles_easing.c
+++ src/examples/compute_particles_easing.c
@@ -480,7 +480,7 @@ static void prepare_graphics_pipeline(wgpu_context_t* wgpu_context)
                 wgpu_context, &(wgpu_fragment_state_t){
                 .shader_desc = (wgpu_shader_desc_t){
                   /* Fragment shader SPIR-V */
-                  .label = "Particle fragment shader SPIR-V",
+                  .label = "Particle - Fragment shader SPIR-V",
                   .file  = "shaders/compute_particles_easing/particle.frag.spv",
                 },
                 .target_count = 1,
