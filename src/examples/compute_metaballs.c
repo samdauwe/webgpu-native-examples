e82005fe7a38738b4c1bb51aef1f1f4fe6ba8c49
samdauwe <sam.dauwe@gmail.com>
2025-05-12 20:33:38 +0200

    Updated metaballs vertex shader label.

--- src/examples/compute_metaballs.c
+++ src/examples/compute_metaballs.c
@@ -3674,7 +3674,7 @@ static void metaballs_init(metaballs_t* this)
         this->renderer->wgpu_context, &(wgpu_vertex_state_t){
         .shader_desc = (wgpu_shader_desc_t){
             // Vertex shader WGSL
-            .label = "metaballs vertex shader wgsl",
+            .label = "Metaballs - Vertex shader wgsl",
             .file  = "shaders/compute_metaballs/metaballs_vertex_shader.wgsl",
             .entry = "main",
           },
