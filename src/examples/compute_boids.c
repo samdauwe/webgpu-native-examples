d3ab38533ad1be184a7a2f80cb8b1d9c56fc61c7
samdauwe <sam.dauwe@gmail.com>
2025-06-10 20:27:25 +0200

    Updated sprite vertex shader label.

--- src/examples/compute_boids.c
+++ src/examples/compute_boids.c
@@ -351,7 +351,7 @@ static void prepare_pipelines(wgpu_context_t* wgpu_context)
             wgpu_context, &(wgpu_vertex_state_t){
             .shader_desc = (wgpu_shader_desc_t){
               // Vertex shader WGSL
-              .label            = "Sprite vertex shader WGSL",
+              .label            = "Sprite - Vertex shader WGSL",
               .wgsl_code.source = sprite_vertex_shader_wgsl,
               .entry            = "vert_main",
             },
