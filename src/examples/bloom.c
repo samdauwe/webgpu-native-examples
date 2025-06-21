0377148f671691311536005db7d942bc3ef0a206
samdauwe <sam.dauwe@gmail.com>
2025-06-21 18:00:56 +0200

    Updated offscreen pass sampler label.

--- src/examples/bloom.c
+++ src/examples/bloom.c
@@ -289,7 +289,7 @@ static void prepare_offscreen(wgpu_context_t* wgpu_context)
   // Create sampler to sample from the color attachments
   offscreen_pass.sampler = wgpuDeviceCreateSampler(
     wgpu_context->device, &(WGPUSamplerDescriptor){
-                            .label         = "Offscreen pass sampler",
+                            .label         = "Offscreen pass - Sampler",
                             .addressModeU  = WGPUAddressMode_ClampToEdge,
                             .addressModeV  = WGPUAddressMode_ClampToEdge,
                             .addressModeW  = WGPUAddressMode_ClampToEdge,
