7e65bdc504b1398a811f7dcfd81f4b06b60d07df
samdauwe <sam.dauwe@gmail.com>
2025-06-23 20:11:07 +0200

    Updated scene bind group layout label.

--- src/examples/bloom.c
+++ src/examples/bloom.c
@@ -385,7 +385,7 @@ static void setup_pipeline_layout(wgpu_context_t* wgpu_context)
     // Create the bind group layout
     bind_group_layouts.scene = wgpuDeviceCreateBindGroupLayout(
       wgpu_context->device, &(WGPUBindGroupLayoutDescriptor){
-                              .label      = "Scene bind group layout",
+                              .label      = "Scene - Bind group layout",
                               .entryCount = (uint32_t)ARRAY_SIZE(bgl_entries),
                               .entries    = bgl_entries,
                             });
