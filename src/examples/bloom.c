b0cf4e376b2c9a9fa88f58e37c5e67553d5c4fd7
samdauwe <sam.dauwe@gmail.com>
2025-06-27 20:02:03 +0200

    Updated vertical gaussian blur fragment shader label.

--- src/examples/bloom.c
+++ src/examples/bloom.c
@@ -668,7 +668,7 @@ static void prepare_pipelines(wgpu_context_t* wgpu_context)
                 wgpu_context, &(wgpu_fragment_state_t){
                 .shader_desc = (wgpu_shader_desc_t){
                   // Fragment shader SPIR-V
-                  .label = "Vertical gaussian blur fragment shader SPIR-V",
+                  .label = "Vertical gaussian blur - Fragment shader SPIR-V",
                   .file  = "shaders/bloom/gaussblur_vert.frag.spv",
                 },
                 .target_count = 1,
