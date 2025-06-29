a5e86c2ba3031b83c385bdf677441550a5e98195
samdauwe <sam.dauwe@gmail.com>
2025-06-29 18:02:46 +0200

    Updated phong render pipeline label.

--- src/examples/bloom.c
+++ src/examples/bloom.c
@@ -794,7 +794,7 @@ static void prepare_pipelines(wgpu_context_t* wgpu_context)
     /* Create rendering pipeline using the specified states */
     pipelines.phong_pass = wgpuDeviceCreateRenderPipeline(
       wgpu_context->device, &(WGPURenderPipelineDescriptor){
-                              .label        = "Phong render pipeline",
+                              .label        = "Phong - Render pipeline",
                               .layout       = pipeline_layouts.scene,
                               .primitive    = primitive_state,
                               .vertex       = vertex_state,
