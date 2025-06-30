2c5ca9afaa2346afa57537d0004d30c2d65cc62e
samdauwe <sam.dauwe@gmail.com>
2025-06-30 20:16:33 +0200

    Updated color pass vertex shader label.

--- src/examples/bloom.c
+++ src/examples/bloom.c
@@ -824,7 +824,7 @@ static void prepare_pipelines(wgpu_context_t* wgpu_context)
               wgpu_context, &(wgpu_vertex_state_t){
               .shader_desc = (wgpu_shader_desc_t){
                 /* Vertex shader SPIR-V */
-                .label = "Color pass vertex shader SPIR-V",
+                .label = "Color pass - Vertex shader SPIR-V",
                 .file  = "shaders/bloom/colorpass.vert.spv",
               },
               .buffer_count = 1,
