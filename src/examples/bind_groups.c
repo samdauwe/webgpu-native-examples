3c763171868087c8033378768a2fb0643dc6afad
samdauwe <sam.dauwe@gmail.com>
2025-07-11 19:45:39 +0200

    Updated cube fragment shader label.

--- src/examples/bind_groups.c
+++ src/examples/bind_groups.c
@@ -314,7 +314,7 @@ static void prepare_pipelines(wgpu_context_t* wgpu_context)
             wgpu_context, &(wgpu_fragment_state_t){
             .shader_desc = (wgpu_shader_desc_t){
               // Fragment shader WGSL
-              .label            = "Cube fragment shader WGSL",
+              .label            = "Cube - Fragment shader WGSL",
               .wgsl_code.source = bind_groups_fragment_shader_wgsl,
               .entry            = "main",
             },
