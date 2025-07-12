6f0e337553d50e3126f47bfbdf82430dd3fb78d5
samdauwe <sam.dauwe@gmail.com>
2025-07-12 17:30:03 +0200

    Updated cube render pipeline label.

--- src/examples/bind_groups.c
+++ src/examples/bind_groups.c
@@ -332,7 +332,7 @@ static void prepare_pipelines(wgpu_context_t* wgpu_context)
   /* Create rendering pipeline using the specified states */
   pipeline = wgpuDeviceCreateRenderPipeline(
     wgpu_context->device, &(WGPURenderPipelineDescriptor){
-                            .label        = "Cube render pipeline",
+                            .label        = "Cube - Render pipeline",
                             .layout       = pipeline_layout,
                             .primitive    = primitive_state_desc,
                             .vertex       = vertex_state_desc,
