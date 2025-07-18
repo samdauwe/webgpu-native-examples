869b914f8ef9c2623c6c54ef6b3d12d824a9d826
samdauwe <sam.dauwe@gmail.com>
2025-07-18 20:14:22 +0200

    Updated time bind group layout label.

--- src/examples/animometer.c
+++ src/examples/animometer.c
@@ -139,7 +139,7 @@ static void setup_pipeline_layout(wgpu_context_t* wgpu_context)
     },
   };
   WGPUBindGroupLayoutDescriptor bgl_desc = {
-    .label      = "Time bind group layout",
+    .label      = "Time - Bind group layout",
     .entryCount = (uint32_t)ARRAY_SIZE(bgl_entries),
     .entries    = bgl_entries,
   };
