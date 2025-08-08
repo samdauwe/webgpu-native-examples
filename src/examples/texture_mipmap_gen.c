17626b513c1b74ea27403d8eaa24d6a8886058e9
samdauwe <sam.dauwe@gmail.com>
2025-08-08 19:11:04 +0200

    Updated bind group label.

--- src/examples/texture_mipmap_gen.c
+++ src/examples/texture_mipmap_gen.c
@@ -370,7 +370,7 @@ static void setup_bind_group(wgpu_context_t* wgpu_context)
 
   bind_group = wgpuDeviceCreateBindGroup(
     wgpu_context->device, &(WGPUBindGroupDescriptor){
-                            .label      = "Bind group",
+                            .label      = "Texture Mipmap - Bind group",
                             .layout     = bind_group_layout,
                             .entryCount = (uint32_t)ARRAY_SIZE(bg_entries),
                             .entries    = bg_entries,
