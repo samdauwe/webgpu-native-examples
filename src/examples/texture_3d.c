5553d34b56b168b08e0d5f88a839d8865eb14634
samdauwe <sam.dauwe@gmail.com>
2025-08-13 19:16:00 +0200

    Updated texture 3d fragment shader label.

--- src/examples/texture_3d.c
+++ src/examples/texture_3d.c
@@ -604,7 +604,7 @@ static void prepare_pipelines(wgpu_context_t* wgpu_context)
                 wgpu_context, &(wgpu_fragment_state_t){
                 .shader_desc = (wgpu_shader_desc_t){
                   // Fragment shader SPIR-V
-                  .label = "Texture 3d - fragment shader",
+                  .label = "Texture 3D - Fragment shader",
                   .file  = "shaders/texture_3d/texture_3d.frag.spv",
                 },
                 .target_count = 1,
