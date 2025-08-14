3b404fa1f6fd6528ac6c0da4608964eccd2969ba
samdauwe <sam.dauwe@gmail.com>
2025-08-14 15:41:34 +0200

    Updated and enabled square example.

--- src/examples/square.c
+++ src/examples/square.c
@@ -1,6 +1,6 @@
-#include "example_base.h"
+#include "webgpu/wgpu_common.h"
 
-#include <string.h>
+#include <stdio.h>
 
 /* -------------------------------------------------------------------------- *
  * WebGPU Example - Square
@@ -23,24 +23,28 @@ static const char* fragment_shader_wgsl;
  * Square example
  * -------------------------------------------------------------------------- */
 
-// Vertex buffers
 static struct {
-  wgpu_buffer_t positions; /* Positions */
-  wgpu_buffer_t colors;    /* Colors */
-} square = {0};
-
-// Pipeline
-static WGPURenderPipeline pipeline;
-
-// Render pass descriptor for frame buffer writes
-static struct {
-  WGPURenderPassColorAttachment color_attachments[1];
-  WGPURenderPassDescriptor descriptor;
-} render_pass = {0};
-
-// Other variables
-static const char* example_title = "Square";
-static bool prepared             = false;
+  struct {
+    wgpu_buffer_t positions; /* Positions */
+    wgpu_buffer_t colors;    /* Colors */
+  } square;
+  WGPURenderPipeline pipeline;
+  WGPURenderPassColorAttachment color_attachment;
+  WGPURenderPassDescriptor render_pass_dscriptor;
+  bool prepared;
+} state = {
+  .color_attachment = {
+    .loadOp     = WGPULoadOp_Clear,
+    .storeOp    = WGPUStoreOp_Store,
+    .clearValue = {0.0, 0.0, 0.0, 1.0},
+    .depthSlice = WGPU_DEPTH_SLICE_UNDEFINED,
+  },
+  .render_pass_dscriptor = {
+    .label                = STRVIEW("Render pass descriptor"),
+    .colorAttachmentCount = 1,
+    .colorAttachments     = &state.color_attachment,
+  },
+};
 
 static void prepare_vertex_buffers(wgpu_context_t* wgpu_context)
 {
@@ -68,7 +72,7 @@ static void prepare_vertex_buffers(wgpu_context_t* wgpu_context)
      0.5f, -0.5f, 0.0f, // v3
   };
   // clang-format on
-  square.positions = wgpu_create_buffer(
+  state.square.positions = wgpu_create_buffer(
     wgpu_context, &(wgpu_buffer_desc_t){
                     .usage = WGPUBufferUsage_CopyDst | WGPUBufferUsage_Vertex,
                     .size  = sizeof(positions),
@@ -82,7 +86,7 @@ static void prepare_vertex_buffers(wgpu_context_t* wgpu_context)
     0.0f, 0.0f, 1.0f, 1.0f, /* v2 */
     1.0f, 1.0f, 0.0f, 1.0f  /* v3 */
   };
-  square.colors = wgpu_create_buffer(
+  state.square.colors = wgpu_create_buffer(
     wgpu_context, &(wgpu_buffer_desc_t){
                     .label = "Colored square vertex buffer",
                     .usage = WGPUBufferUsage_CopyDst | WGPUBufferUsage_Vertex,
@@ -92,238 +96,150 @@ static void prepare_vertex_buffers(wgpu_context_t* wgpu_context)
                   });
 }
 
-static void setup_render_pass(wgpu_context_t* wgpu_context)
+static void prepare_pipeline(wgpu_context_t* wgpu_context)
 {
-  UNUSED_VAR(wgpu_context);
+  WGPUShaderModule vert_shader_module
+    = wgpu_create_shader_module(wgpu_context->device, vertex_shader_wgsl);
+  WGPUShaderModule frag_shader_module
+    = wgpu_create_shader_module(wgpu_context->device, fragment_shader_wgsl);
 
-  // Color attachment
-  render_pass.color_attachments[0] = (WGPURenderPassColorAttachment) {
-      .view       = NULL, /* Assigned later */
-      .depthSlice = ~0,
-      .loadOp     = WGPULoadOp_Clear,
-      .storeOp    = WGPUStoreOp_Store,
-      .clearValue = (WGPUColor) {
-        .r = 0.0f,
-        .g = 0.0f,
-        .b = 0.0f,
-        .a = 1.0f,
+  /* Vertex buffer layout */
+  WGPUVertexBufferLayout vertex_buffer_layouts[2] = {
+    [0] = {
+      .arrayStride    = sizeof(float) * 3,
+      .stepMode       = WGPUVertexStepMode_Vertex,
+      .attributeCount = 1,
+      .attributes     = &(WGPUVertexAttribute) {
+        /* Shader location 0 : vertex position */
+        .shaderLocation = 0,
+        .offset         = 0,
+        .format         = WGPUVertexFormat_Float32x3,
       },
-  };
-
-  // Render pass descriptor
-  render_pass.descriptor = (WGPURenderPassDescriptor){
-    .label                = "Render pass descriptor",
-    .colorAttachmentCount = 1,
-    .colorAttachments     = render_pass.color_attachments,
-  };
-}
-
-static void prepare_pipelines(wgpu_context_t* wgpu_context)
-{
-  // Primitive state
-  WGPUPrimitiveState primitive_state = {
-    .topology         = WGPUPrimitiveTopology_TriangleStrip,
-    .stripIndexFormat = WGPUIndexFormat_Uint32,
-    .frontFace        = WGPUFrontFace_CCW,
-    .cullMode         = WGPUCullMode_Front,
-  };
-
-  // Color target state
-  WGPUBlendState blend_state              = wgpu_create_blend_state(false);
-  WGPUColorTargetState color_target_state = (WGPUColorTargetState){
-    .format    = wgpu_context->swap_chain.format,
-    .blend     = &blend_state,
-    .writeMask = WGPUColorWriteMask_All,
-  };
-
-  // Vertex buffer layout
-  WGPUVertexBufferLayout vertex_buffer_layouts[2] = {0};
-  {
-    WGPUVertexAttribute attribute = {
-      // Shader location 0 : vertex position
-      .shaderLocation = 0,
-      .offset         = 0,
-      .format         = WGPUVertexFormat_Float32x3,
-    };
-    vertex_buffer_layouts[0] = (WGPUVertexBufferLayout){
-      .arrayStride    = 3 * 4,
+    },
+    [1] = {
+      .arrayStride    = sizeof(float) * 4,
       .stepMode       = WGPUVertexStepMode_Vertex,
       .attributeCount = 1,
-      .attributes     = &attribute,
-    };
-  }
-  {
-    WGPUVertexAttribute attribute = {
-      // Shader location 1 : vertex color
-      .shaderLocation = 1,
-      .offset         = 0,
-      .format         = WGPUVertexFormat_Float32x4,
-    };
-    vertex_buffer_layouts[1] = (WGPUVertexBufferLayout){
-      .arrayStride    = 4 * 4,
-      .stepMode       = WGPUVertexStepMode_Vertex,
-      .attributeCount = 1,
-      .attributes     = &attribute,
-    };
-  }
+      .attributes     = &(WGPUVertexAttribute) {
+        /* Shader location 1 : vertex color */
+        .shaderLocation = 1,
+        .offset         = 0,
+        .format         = WGPUVertexFormat_Float32x4,
+      },
+    }
+  };
 
-  // Vertex state
-  WGPUVertexState vertex_state = wgpu_create_vertex_state(
-                    wgpu_context, &(wgpu_vertex_state_t){
-                    .shader_desc = (wgpu_shader_desc_t){
-                      // Vertex shader WGSL
-                      .label            = "Square vertex shader wgsl",
-                      .wgsl_code.source = vertex_shader_wgsl,
-                    },
-                    .buffer_count = (uint32_t) ARRAY_SIZE(vertex_buffer_layouts),
-                    .buffers      = vertex_buffer_layouts,
-                  });
+  WGPURenderPipelineDescriptor rp_desc = {
+    .label = STRVIEW("Square - Render pipeline"),
+    .vertex = {
+      .module      = vert_shader_module,
+      .entryPoint  = STRVIEW("main"),
+      .bufferCount = (uint32_t)ARRAY_SIZE(vertex_buffer_layouts),
+      .buffers     = vertex_buffer_layouts,
+    },
+    .fragment = &(WGPUFragmentState) {
+      .entryPoint  = STRVIEW("main"),
+      .module      = frag_shader_module,
+      .targetCount = 1,
+      .targets = &(WGPUColorTargetState) {
+        .format    = wgpu_context->render_format,
+        .writeMask = WGPUColorWriteMask_All,
+      },
+    },
+    .primitive = {
+      .topology         = WGPUPrimitiveTopology_TriangleStrip,
+      .stripIndexFormat = WGPUIndexFormat_Uint32,
+      .frontFace        = WGPUFrontFace_CCW,
+      .cullMode         = WGPUCullMode_Front,
+    },
+    .multisample = {
+      .count = 1,
+      .mask  = 0xffffffff
+    },
+  };
 
-  // Fragment state
-  WGPUFragmentState fragment_state = wgpu_create_fragment_state(
-                    wgpu_context, &(wgpu_fragment_state_t){
-                    .shader_desc = (wgpu_shader_desc_t){
-                      // Fragment shader WGSL
-                      .label            = "Square fragment shader wgsl",
-                      .wgsl_code.source = fragment_shader_wgsl,
-                    },
-                    .target_count = 1,
-                    .targets      = &color_target_state,
-                  });
+  state.pipeline
+    = wgpuDeviceCreateRenderPipeline(wgpu_context->device, &rp_desc);
+  ASSERT(state.pipeline != NULL);
 
-  // Multisample state
-  WGPUMultisampleState multisample_state
-    = wgpu_create_multisample_state_descriptor(
-      &(create_multisample_state_desc_t){
-        .sample_count = 1,
-      });
-
-  // Create rendering pipeline using the specified states
-  pipeline = wgpuDeviceCreateRenderPipeline(wgpu_context->device,
-                                            &(WGPURenderPipelineDescriptor){
-                                              .label = "Square render pipeline",
-                                              .primitive   = primitive_state,
-                                              .vertex      = vertex_state,
-                                              .fragment    = &fragment_state,
-                                              .multisample = multisample_state,
-                                            });
-  ASSERT(pipeline != NULL);
-
-  // Partial cleanup
-  WGPU_RELEASE_RESOURCE(ShaderModule, vertex_state.module);
-  WGPU_RELEASE_RESOURCE(ShaderModule, fragment_state.module);
+  wgpuShaderModuleRelease(vert_shader_module);
+  wgpuShaderModuleRelease(frag_shader_module);
 }
 
-static int example_initialize(wgpu_example_context_t* context)
+static int init(struct wgpu_context_t* wgpu_context)
 {
-  if (context) {
-    prepare_vertex_buffers(context->wgpu_context);
-    prepare_pipelines(context->wgpu_context);
-    setup_render_pass(context->wgpu_context);
-    prepared = true;
+  if (wgpu_context) {
+    prepare_vertex_buffers(wgpu_context);
+    prepare_pipeline(wgpu_context);
+    state.prepared = true;
     return EXIT_SUCCESS;
   }
 
   return EXIT_FAILURE;
 }
 
-static WGPUCommandBuffer build_command_buffer(wgpu_context_t* wgpu_context)
+static int frame(struct wgpu_context_t* wgpu_context)
 {
-  // Set target frame buffer
-  render_pass.color_attachments[0].view = wgpu_context->swap_chain.frame_buffer;
+  if (!state.prepared) {
+    return EXIT_FAILURE;
+  }
 
-  // Create command encoder
-  wgpu_context->cmd_enc
-    = wgpuDeviceCreateCommandEncoder(wgpu_context->device, NULL);
+  WGPUDevice device = wgpu_context->device;
+  WGPUQueue queue   = wgpu_context->queue;
 
-  // Create render pass encoder for encoding drawing commands
-  wgpu_context->rpass_enc = wgpuCommandEncoderBeginRenderPass(
-    wgpu_context->cmd_enc, &render_pass.descriptor);
+  state.color_attachment.view = wgpu_context->swapchain_view;
 
-  // Bind the rendering pipeline
-  wgpuRenderPassEncoderSetPipeline(wgpu_context->rpass_enc, pipeline);
+  WGPUCommandEncoder cmd_enc = wgpuDeviceCreateCommandEncoder(device, NULL);
+  WGPURenderPassEncoder rpass_enc
+    = wgpuCommandEncoderBeginRenderPass(cmd_enc, &state.render_pass_dscriptor);
 
-  // Set viewport
-  wgpuRenderPassEncoderSetViewport(
-    wgpu_context->rpass_enc, 0.0f, 0.0f, (float)wgpu_context->surface.width,
-    (float)wgpu_context->surface.height, 0.0f, 1.0f);
+  /* Record render commands. */
+  wgpuRenderPassEncoderSetPipeline(rpass_enc, state.pipeline);
+  wgpuRenderPassEncoderSetViewport(rpass_enc, 0.0f, 0.0f,
+                                   (float)wgpu_context->width,
+                                   (float)wgpu_context->height, 0.0f, 1.0f);
+  wgpuRenderPassEncoderSetScissorRect(rpass_enc, 0u, 0u, wgpu_context->width,
+                                      wgpu_context->height);
 
-  // Set scissor rectangle
-  wgpuRenderPassEncoderSetScissorRect(wgpu_context->rpass_enc, 0u, 0u,
-                                      wgpu_context->surface.width,
-                                      wgpu_context->surface.height);
-
-  // Bind vertex buffers (contain position & colors)
   wgpuRenderPassEncoderSetVertexBuffer(
-    wgpu_context->rpass_enc, 0, square.positions.buffer, 0, WGPU_WHOLE_SIZE);
-  wgpuRenderPassEncoderSetVertexBuffer(
-    wgpu_context->rpass_enc, 1, square.colors.buffer, 0, WGPU_WHOLE_SIZE);
+    rpass_enc, 0, state.square.positions.buffer, 0, WGPU_WHOLE_SIZE);
+  wgpuRenderPassEncoderSetVertexBuffer(rpass_enc, 1, state.square.colors.buffer,
+                                       0, WGPU_WHOLE_SIZE);
+  wgpuRenderPassEncoderDraw(rpass_enc, state.square.positions.count, 1, 0, 0);
+  wgpuRenderPassEncoderEnd(rpass_enc);
+  WGPUCommandBuffer cmd_buffer = wgpuCommandEncoderFinish(cmd_enc, NULL);
 
-  // Draw quad
-  wgpuRenderPassEncoderDraw(wgpu_context->rpass_enc, square.positions.count, 1,
-                            0, 0);
+  /* Submit and present. */
+  wgpuQueueSubmit(queue, 1, &cmd_buffer);
 
-  // End render pass
-  wgpuRenderPassEncoderEnd(wgpu_context->rpass_enc);
-  WGPU_RELEASE_RESOURCE(RenderPassEncoder, wgpu_context->rpass_enc)
-
-  // Get command buffer
-  WGPUCommandBuffer command_buffer
-    = wgpu_get_command_buffer(wgpu_context->cmd_enc);
-  WGPU_RELEASE_RESOURCE(CommandEncoder, wgpu_context->cmd_enc)
-
-  return command_buffer;
-}
-
-static int example_draw(wgpu_example_context_t* context)
-{
-  wgpu_context_t* wgpu_context = context->wgpu_context;
-
-  // Prepare frame
-  prepare_frame(context);
-
-  // Command buffer to be submitted to the queue
-  wgpu_context->submit_info.command_buffer_count = 1;
-  wgpu_context->submit_info.command_buffers[0]
-    = build_command_buffer(context->wgpu_context);
-
-  // Submit to queue
-  submit_command_buffers(context);
-
-  // Submit frame
-  submit_frame(context);
+  /* Cleanup */
+  wgpuRenderPassEncoderRelease(rpass_enc);
+  wgpuCommandBufferRelease(cmd_buffer);
+  wgpuCommandEncoderRelease(cmd_enc);
 
   return EXIT_SUCCESS;
 }
 
-static int example_render(wgpu_example_context_t* context)
+static void shutdown(struct wgpu_context_t* wgpu_context)
 {
-  if (!prepared) {
-    return EXIT_FAILURE;
-  }
-  return example_draw(context);
+  UNUSED_VAR(wgpu_context);
+  WGPU_RELEASE_RESOURCE(Buffer, state.square.positions.buffer)
+  WGPU_RELEASE_RESOURCE(Buffer, state.square.colors.buffer)
+  WGPU_RELEASE_RESOURCE(RenderPipeline, state.pipeline)
 }
 
-static void example_destroy(wgpu_example_context_t* context)
+int main(void)
 {
-  camera_release(context->camera);
-  WGPU_RELEASE_RESOURCE(Buffer, square.positions.buffer)
-  WGPU_RELEASE_RESOURCE(Buffer, square.colors.buffer)
-  WGPU_RELEASE_RESOURCE(RenderPipeline, pipeline)
-}
-
-void example_square(int argc, char* argv[])
-{
-  // clang-format off
-  example_run(argc, argv, &(refexport_t){
-    .example_settings = (wgpu_example_settings_t){
-     .title  = example_title,
-    },
-    .example_initialize_func = &example_initialize,
-    .example_render_func     = &example_render,
-    .example_destroy_func    = &example_destroy,
+  wgpu_start(&(wgpu_desc_t){
+    .width       = 1280,
+    .height      = 720,
+    .title       = "Square",
+    .init_cb     = init,
+    .frame_cb    = frame,
+    .shutdown_cb = shutdown,
   });
-  // clang-format on
+
+  return EXIT_SUCCESS;
 }
 
 /* -------------------------------------------------------------------------- *
