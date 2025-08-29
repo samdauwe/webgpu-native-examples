8a7d36356b95fd1d917fe1f31cad207f270b22c8
samdauwe <sam.dauwe@gmail.com>
2025-08-29 21:27:30 +0200

    Using macro for exit code.

--- src/examples/shadertoy.c
+++ src/examples/shadertoy.c
@@ -277,10 +277,10 @@ static int example_initialize(wgpu_example_context_t* context)
     prepare_pipelines(context->wgpu_context);
     setup_render_pass(context->wgpu_context);
     prepared = true;
-    return 0;
+    return EXIT_SUCCESS;
   }
 
-  return 1;
+  return EXIT_FAILURE;
 }
 
 static WGPUCommandBuffer build_command_buffer(wgpu_context_t* wgpu_context)
