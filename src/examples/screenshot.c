ffd29a83585add83602414b896d73172947ac4ea
samdauwe <sam.dauwe@gmail.com>
2025-09-12 20:43:29 +0200

    Updated bind group layout label.

--- src/examples/screenshot.c
+++ src/examples/screenshot.c
@@ -276,7 +276,7 @@ static void setup_pipeline_layout(wgpu_context_t* wgpu_context)
   /* Create the bind group layout */
   bind_group_layout = wgpuDeviceCreateBindGroupLayout(
     wgpu_context->device, &(WGPUBindGroupLayoutDescriptor){
-                            .label      = "Bind group layout",
+                            .label      = "Screenshot - Bind group layout",
                             .entryCount = (uint32_t)ARRAY_SIZE(bgl_entries),
                             .entries    = bgl_entries,
                           });
