848680e87523c638790a49b5fdb2c8860f18f88d
samdauwe <sam.dauwe@gmail.com>
2025-09-15 20:40:27 +0200

    Updated vertex shader label.

--- src/examples/shadertoy.c
+++ src/examples/shadertoy.c
@@ -226,7 +226,7 @@ static void prepare_pipelines(wgpu_context_t* wgpu_context)
                     wgpu_context, &(wgpu_vertex_state_t){
                     .shader_desc = (wgpu_shader_desc_t){
                       // Vertex shader SPIR-V
-                      .label = "main_vertex_shader",
+                      .label = "Main - Vertex shader",
                       .file  = "shaders/shadertoy/main.vert.spv",
                     },
                     .buffer_count = 0,
