4ddd7141638f555eec0074e05677f3f2eba7b0a5
samdauwe <sam.dauwe@gmail.com>
2025-09-15 20:41:03 +0200

    Updated fragment shader label.

--- src/examples/shadertoy.c
+++ src/examples/shadertoy.c
@@ -238,7 +238,7 @@ static void prepare_pipelines(wgpu_context_t* wgpu_context)
                     wgpu_context, &(wgpu_fragment_state_t){
                     .shader_desc = (wgpu_shader_desc_t){
                       // Fragment shader SPIR-V
-                      .label = "main_fragment_shader",
+                      .label = "Main - Fragment shader",
                       .file  = "shaders/shadertoy/main.frag.spv",
                     },
                     .target_count = 1,
