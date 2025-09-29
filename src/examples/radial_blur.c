868d49d58eacae94ad3dada564cc064aefacb6c0
samdauwe <sam.dauwe@gmail.com>
2025-09-29 20:02:22 +0200

    Updated scene rendering bind group label.

--- src/examples/radial_blur.c
+++ src/examples/radial_blur.c
@@ -399,7 +399,7 @@ static void setup_bind_groups(wgpu_context_t* wgpu_context)
 
     bind_groups.scene = wgpuDeviceCreateBindGroup(
       wgpu_context->device, &(WGPUBindGroupDescriptor){
-                              .label      = "Scene rendering bind group",
+                              .label      = "Scene rendering - Bind group",
                               .layout     = bind_group_layouts.scene,
                               .entryCount = (uint32_t)ARRAY_SIZE(bg_entries),
                               .entries    = bg_entries,
