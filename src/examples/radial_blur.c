818d686704b5ff52292405b545f6853a6399bc1d
samdauwe <sam.dauwe@gmail.com>
2025-10-03 19:52:33 +0200

    Updated color pass fragment shader label.

--- src/examples/radial_blur.c
+++ src/examples/radial_blur.c
@@ -696,7 +696,7 @@ static void prepare_pipelines(wgpu_context_t* wgpu_context)
               wgpu_context, &(wgpu_fragment_state_t){
               .shader_desc = (wgpu_shader_desc_t){
                 // Fragment shader SPIR-V
-                .label = "Color pass fragment shader",
+                .label = "Color pass - Fragment shader",
                 .file  = "shaders/radial_blur/colorpass.frag.spv",
               },
               .target_count = 1,
