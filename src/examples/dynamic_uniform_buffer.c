844b6be222cfbd633d3be61434e6aeb2e56901ab
samdauwe <sam.dauwe@gmail.com>
2025-10-06 20:23:08 +0200

    Updated bind group layout label.

--- src/examples/dynamic_uniform_buffer.c
+++ src/examples/dynamic_uniform_buffer.c
@@ -174,7 +174,7 @@ static void setup_pipeline_layout(wgpu_context_t* wgpu_context)
   };
   bind_group_layout = wgpuDeviceCreateBindGroupLayout(
     wgpu_context->device, &(WGPUBindGroupLayoutDescriptor){
-                            .label      = "Bind group layout",
+                            .label      = "Render - Bind group layout",
                             .entryCount = (uint32_t)ARRAY_SIZE(bgl_entries),
                             .entries    = bgl_entries,
                           });
