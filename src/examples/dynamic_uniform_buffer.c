e4d2891e60e0d8c0194ab30969cedc1fa49ccaaa
samdauwe <sam.dauwe@gmail.com>
2025-10-07 20:42:44 +0200

    Updated vertex shader label.

--- src/examples/dynamic_uniform_buffer.c
+++ src/examples/dynamic_uniform_buffer.c
@@ -257,7 +257,7 @@ static void prepare_pipeline(wgpu_context_t* wgpu_context)
                 wgpu_context, &(wgpu_vertex_state_t){
                 .shader_desc = (wgpu_shader_desc_t){
                   // Vertex shader WGSL
-                  .label            = "Vertex shader WGSL",
+                  .label            = "Dynamic uniform buffer - Vertex shader WGSL",
                   .wgsl_code.source = vertex_shader_wgsl,
                   .entry            = "main",
                 },
