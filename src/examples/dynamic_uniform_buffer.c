2c9b5d0c97a708cc736e4ad8914a521822c6e0cf
samdauwe <sam.dauwe@gmail.com>
2025-10-08 20:30:29 +0200

    Updated checkbox label.

--- src/examples/dynamic_uniform_buffer.c
+++ src/examples/dynamic_uniform_buffer.c
@@ -501,7 +501,7 @@ static void example_on_update_ui_overlay(wgpu_example_context_t* context)
 {
   if (imgui_overlay_header("Settings")) {
     imgui_overlay_checkBox(context->imgui_overlay, "Paused", &context->paused);
-    imgui_overlay_checkBox(context->imgui_overlay, "Render bundles",
+    imgui_overlay_checkBox(context->imgui_overlay, "Render Bundles",
                            &render_bundles);
   }
 }
