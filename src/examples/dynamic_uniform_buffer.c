14ed1894b17f7ef7ffa91f333ddaa6263bca6f18
samdauwe <sam.dauwe@gmail.com>
2025-10-08 20:31:10 +0200

    Enabled V-Sync.

--- src/examples/dynamic_uniform_buffer.c
+++ src/examples/dynamic_uniform_buffer.c
@@ -598,6 +598,7 @@ void example_dynamic_uniform_buffer(int argc, char* argv[])
     .example_settings = (wgpu_example_settings_t){
       .title   = example_title,
       .overlay = true,
+      .vsync   = true,
     },
     .example_initialize_func      = &example_initialize,
     .example_render_func          = &example_render,
