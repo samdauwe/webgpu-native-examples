313d6627bc92263cac120f0517bba73fda0a474b
samdauwe <sam.dauwe@gmail.com>
2025-10-24 20:26:35 +0200

    Updated commenting style.

--- src/examples/compute_ray_tracing.c
+++ src/examples/compute_ray_tracing.c
@@ -245,7 +245,7 @@ static void setup_render_pass(wgpu_context_t* wgpu_context)
 {
   UNUSED_VAR(wgpu_context);
 
-  // Color attachment
+  /* Color attachment */
   render_pass.color_attachments[0] = (WGPURenderPassColorAttachment) {
       .view       = NULL, /* Assigned later */
       .depthSlice = ~0,
@@ -259,10 +259,10 @@ static void setup_render_pass(wgpu_context_t* wgpu_context)
       },
   };
 
-  // Depth attachment
+  /* Depth attachment */
   wgpu_setup_deph_stencil(wgpu_context, NULL);
 
-  // Render pass descriptor
+  /* Render pass descriptor */
   render_pass.descriptor = (WGPURenderPassDescriptor){
     .label = "Compute Shader Ray Tracing - Render pass descriptor",
     .colorAttachmentCount   = 1,
