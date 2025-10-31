c9703b89e9ee25b525f3ab30e554f05d2add0da8
samdauwe <sam.dauwe@gmail.com>
2025-10-31 20:02:02 +0100

    Updated vertex shader label.

--- src/examples/out_of_bounds_viewport.c
+++ src/examples/out_of_bounds_viewport.c
@@ -222,7 +222,7 @@ static void prepare_pipelines(wgpu_context_t* wgpu_context)
                     wgpu_context, &(wgpu_vertex_state_t){
                     .shader_desc = (wgpu_shader_desc_t){
                       /* Vertex shader WGSL */
-                      .label            = "Vertex shader WGSL",
+                      .label            = "Out-of-bounds Viewport - Vertex shader WGSL",
                       .wgsl_code.source = vertex_shader_wgsl,
                       .entry            = "vertex_main",
                     },
