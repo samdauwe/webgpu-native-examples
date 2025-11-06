7026b5e7f1e060ad902db04e650fc8914e21820e
samdauwe <sam.dauwe@gmail.com>
2025-11-06 20:49:59 +0100

    Updated opaque render pipeline label.

--- src/examples/a_buffer.c
+++ src/examples/a_buffer.c
@@ -458,7 +458,7 @@ static void prepare_opaque_render_pass(wgpu_context_t* wgpu_context)
     // Create rendering pipeline using the specified states
     opaque_render_pass.pipeline = wgpuDeviceCreateRenderPipeline(
       wgpu_context->device, &(WGPURenderPipelineDescriptor){
-                              .label        = "Opaque render pipeline",
+                              .label        = "Opaque - Render pipeline",
                               .primitive    = primitive_state,
                               .vertex       = vertex_state,
                               .fragment     = &fragment_state,
