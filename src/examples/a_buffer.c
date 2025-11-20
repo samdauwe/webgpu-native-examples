db28804d44d39f158eba4044459fc3ac3180d3d8
samdauwe <sam.dauwe@gmail.com>
2025-11-20 20:09:07 +0100

    Updated composite fragment shader label.

--- src/examples/a_buffer.c
+++ src/examples/a_buffer.c
@@ -875,7 +875,7 @@ static void prepare_composite_render_pass(wgpu_context_t* wgpu_context)
       wgpu_context, &(wgpu_fragment_state_t){
                       .shader_desc = (wgpu_shader_desc_t){
                         // Vertex shader WGSL
-                        .label            = "Composite fragment shader WGSL",
+                        .label            = "Composite - Fragment shader WGSL",
                         .wgsl_code.source = composite_shader_wgsl,
                         .entry            = "main_fs",
                       },
