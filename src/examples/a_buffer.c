6c928f3f3fd0865ece8c2b438c09e985ce3a3d2c
samdauwe <sam.dauwe@gmail.com>
2025-11-21 20:29:37 +0100

    Updated render pass descriptor label.

--- src/examples/a_buffer.c
+++ src/examples/a_buffer.c
@@ -920,7 +920,7 @@ static void prepare_composite_render_pass(wgpu_context_t* wgpu_context)
 
     /* Pass descriptor */
     composite_render_pass.pass_desc.descriptor = (WGPURenderPassDescriptor){
-      .label                = "Composite render pass descriptor",
+      .label                = "Composite - Render pass descriptor",
       .colorAttachmentCount = 1,
       .colorAttachments     = composite_render_pass.pass_desc.color_attachments,
     };
