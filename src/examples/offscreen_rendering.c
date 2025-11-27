751aca37cccf1cd116f0533f87331d9c929bd237
samdauwe <sam.dauwe@gmail.com>
2025-11-27 21:24:35 +0100

    Updated vertex shader comment.

--- src/examples/offscreen_rendering.c
+++ src/examples/offscreen_rendering.c
@@ -563,7 +563,7 @@ static void prepare_pipelines(wgpu_context_t* wgpu_context)
     WGPUVertexState vertex_state = wgpu_create_vertex_state(
               wgpu_context, &(wgpu_vertex_state_t){
               .shader_desc = (wgpu_shader_desc_t){
-                /* Vertex shader SPIR-V */
+                /* Vertex shader */
                 .label = "Phong - Vertex shader",
                 .file  = "shaders/offscreen_rendering/phong.vert.spv",
               },
