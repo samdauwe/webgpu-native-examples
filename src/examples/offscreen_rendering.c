3899d415a4605d58b938cd2c3f259a96cc923e94
samdauwe <sam.dauwe@gmail.com>
2025-11-27 21:24:07 +0100

    Updated fragment shader comment.

--- src/examples/offscreen_rendering.c
+++ src/examples/offscreen_rendering.c
@@ -575,7 +575,7 @@ static void prepare_pipelines(wgpu_context_t* wgpu_context)
     WGPUFragmentState fragment_state = wgpu_create_fragment_state(
               wgpu_context, &(wgpu_fragment_state_t){
               .shader_desc = (wgpu_shader_desc_t){
-                /* Fragment shader SPIR-V */
+                /* Fragment shader */
                 .label = "Phong - Fragment shader",
                 .file  = "shaders/offscreen_rendering/phong.frag.spv",
               },
