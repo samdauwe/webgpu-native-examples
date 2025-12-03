66b43941b09bd05e95474cf34749d3d0722ef8f0
samdauwe <sam.dauwe@gmail.com>
2025-12-03 20:33:29 +0100

    Updated fragment shader comment.

--- src/examples/textured_quad.c
+++ src/examples/textured_quad.c
@@ -325,7 +325,7 @@ static void prepare_pipelines(wgpu_context_t* wgpu_context)
   WGPUFragmentState fragment_state = wgpu_create_fragment_state(
                 wgpu_context, &(wgpu_fragment_state_t){
                 .shader_desc = (wgpu_shader_desc_t){
-                  /* Fragment shader SPIR-V */
+                  /* Fragment shader */
                   .label = "Texture quad - Fragment shader SPIR-V",
                   .file  = "shaders/textured_quad/texture.frag.spv",
                 },
