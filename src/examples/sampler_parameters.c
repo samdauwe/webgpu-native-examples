856f2d4c2520abaa47516877aff1a99589711698
samdauwe <sam.dauwe@gmail.com>
2025-12-11 20:40:19 +0100

    Updated show texture render pipeline label.

--- src/examples/sampler_parameters.c
+++ src/examples/sampler_parameters.c
@@ -461,7 +461,7 @@ static void prepare_debug_view_render_pipeline(wgpu_context_t* wgpu_context)
   /* Create rendering pipeline using the specified states */
   show_texture_render_pipeline = wgpuDeviceCreateRenderPipeline(
     wgpu_context->device, &(WGPURenderPipelineDescriptor){
-                            .label       = "Show texture render pipeline",
+                            .label       = "Show texture - Render pipeline",
                             .primitive   = primitive_state,
                             .vertex      = vertex_state,
                             .fragment    = &fragment_state,
