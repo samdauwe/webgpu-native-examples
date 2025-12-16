66ccd1e698cc3e47944632d653635f580bbba90d
samdauwe <sam.dauwe@gmail.com>
2025-12-16 16:48:04 +0100

    Updated vertex shader comment.

--- src/examples/reversed_z.c
+++ src/examples/reversed_z.c
@@ -300,7 +300,7 @@ static void prepare_precision_pass_render_pipeline(wgpu_context_t* wgpu_context)
   WGPUVertexState vertex_state = wgpu_create_vertex_state(
                 wgpu_context, &(wgpu_vertex_state_t){
                 .shader_desc = (wgpu_shader_desc_t){
-                  // Vertex shader WGSL
+                  // Vertex shader
                   .label = "vertex_precision_error_pass_vertex_shader",
                   .file  = "shaders/reversed_z/vertexPrecisionErrorPass.wgsl",
                   .entry = "main",
