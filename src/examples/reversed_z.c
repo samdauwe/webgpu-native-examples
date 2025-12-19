7f5c28f242da6d3c5abfef789ff4c8007c3f8e07
samdauwe <sam.dauwe@gmail.com>
2025-12-19 17:28:38 +0100

    Updated fragment shader comment.

--- src/examples/reversed_z.c
+++ src/examples/reversed_z.c
@@ -497,7 +497,7 @@ prepare_texture_quad_pass_render_pipeline(wgpu_context_t* wgpu_context)
   WGPUFragmentState fragment_state = wgpu_create_fragment_state(
             wgpu_context, &(wgpu_fragment_state_t){
             .shader_desc = (wgpu_shader_desc_t){
-              // Fragment shader SPIR-V
+              // Fragment shader
               .label = "fragment_texture_quad_fragment_shader",
               .file  = "shaders/reversed_z/fragmentTextureQuad.wgsl",
               .entry = "main",
