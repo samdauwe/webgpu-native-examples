80888ccbb90a675e80ee8410bff4963333cbfd9b
samdauwe <sam.dauwe@gmail.com>
2025-12-29 18:18:08 +0100

    Updated vertex shader label.

--- src/examples/radial_blur.c
+++ src/examples/radial_blur.c
@@ -632,7 +632,7 @@ static void prepare_pipelines(wgpu_context_t* wgpu_context)
               wgpu_context, &(wgpu_vertex_state_t){
               .shader_desc = (wgpu_shader_desc_t){
                 // Vertex shader
-                .label = "Phongpass - Vertex shader SPIR-V",
+                .label = "Phongpass - Vertex shader",
                 .file  = "shaders/radial_blur/phongpass.vert.spv",
               },
               .buffer_count = 1,
