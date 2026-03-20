25dbbb1a4f23daad0c610d3e56b6d42639462554
samdauwe <sam.dauwe@gmail.com>
2026-03-20 21:53:10 +0100

    Updated GBuffer Depth MS label.

--- src/examples/deferred_multi_sampling.c
+++ src/examples/deferred_multi_sampling.c
@@ -287,7 +287,7 @@ static void init_gbuffer_textures(struct wgpu_context_t* wgpu_context)
   /* Depth: Depth24PlusStencil8, multisampled */
   {
     WGPUTextureDescriptor desc = {
-      .label         = STRVIEW("GBuffer Depth MS"),
+      .label         = STRVIEW("GBuffer Depth MS - Texture"),
       .usage         = WGPUTextureUsage_RenderAttachment,
       .dimension     = WGPUTextureDimension_2D,
       .size          = {GBUFFER_DIM, GBUFFER_DIM, 1},
