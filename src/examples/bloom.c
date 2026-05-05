bf321ddba018d272092a36193255789a09c4e2b2
samdauwe <sam.dauwe@gmail.com>
2026-05-05 20:49:28 +0200

    Updated scene UBO label.

--- src/examples/bloom.c
+++ src/examples/bloom.c
@@ -582,7 +582,7 @@ static void init_uniform_buffers(struct wgpu_context_t* wgpu_context)
   /* Scene UBO: projection + view + model (3 × mat4 = 192 bytes) */
   state.scene_ubo = wgpuDeviceCreateBuffer(
     device, &(WGPUBufferDescriptor){
-              .label = STRVIEW("Scene UBO"),
+              .label = STRVIEW("Scene - UBO"),
               .usage = WGPUBufferUsage_Uniform | WGPUBufferUsage_CopyDst,
               .size  = 3 * sizeof(mat4),
             });
