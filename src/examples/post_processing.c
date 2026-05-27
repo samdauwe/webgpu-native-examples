901a97417f709c7b07f2a68f7d4e6c024b90559a
samdauwe <sam.dauwe@gmail.com>
2026-05-27 21:07:18 +0200

    Made example compatible with WebAssembly.

--- src/examples/post_processing.c
+++ src/examples/post_processing.c
@@ -4,6 +4,18 @@
 #include <cglm/cglm.h>
 #include <string.h>
 
+#ifdef __WAJIC__
+#define WAJIC_SFETCH_IMPL
+#include <wajic_sfetch.h>
+#define WAJIC_TIME_IMPL
+#include <wajic_time.h>
+/* WAjic WebGPU handles are uint32_t, not pointers; redefine NULL to plain 0
+ * so WGPU handle assignments compile without pointer-to-integer errors. */
+#ifdef NULL
+#undef NULL
+#define NULL 0
+#endif
+#else
 #define SOKOL_FETCH_IMPL
 #include <sokol_fetch.h>
 
@@ -12,6 +24,7 @@
 
 #define SOKOL_TIME_IMPL
 #include <sokol_time.h>
+#endif
 
 #ifdef __GNUC__
 #pragma GCC diagnostic push
@@ -1434,7 +1447,9 @@ static int init(struct wgpu_context_t* ctx)
     .max_requests = 1,
     .num_channels = 1,
     .num_lanes    = 1,
-    .logger.func  = slog_func,
+#ifndef __WAJIC__
+    .logger.func = slog_func,
+#endif
   });
 
   setup_cameras(ctx);
