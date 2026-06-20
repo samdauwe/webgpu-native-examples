5e4381c96386e24ed9d5c5d9deb4373cab87db4e
samdauwe <sam.dauwe@gmail.com>
2026-06-20 19:10:02 +0200

    Updated face extract pipeline label.

--- src/examples/texture_cubemap.c
+++ src/examples/texture_cubemap.c
@@ -392,7 +392,7 @@ static void extract_cubemap_faces_from_cross(wgpu_context_t* wgpu_context)
   WGPURenderPipeline extract_pipeline = wgpuDeviceCreateRenderPipeline(
     wgpu_context->device,
     &(WGPURenderPipelineDescriptor){
-      .label  = STRVIEW("Face extract pipeline"),
+      .label  = STRVIEW("Face extract - Render pipeline"),
       .layout = extract_pl,
       .vertex = (WGPUVertexState){
         .module     = extract_shader,
