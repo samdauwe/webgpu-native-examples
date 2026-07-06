ae14f03adc3ef0d9ca8c7719380144d0b0784ad7
samdauwe <sam.dauwe@gmail.com>
2026-07-06 21:17:09 +0200

    Updated GBuffer Albedo label.

--- src/examples/deferred_shading.c
+++ src/examples/deferred_shading.c
@@ -270,7 +270,7 @@ static void init_gbuffer_textures(struct wgpu_context_t* wgpu_context)
   /* Albedo: RGBA8Unorm (RGB = diffuse color, A = specular intensity) */
   {
     WGPUTextureDescriptor desc = {
-      .label = STRVIEW("GBuffer Albedo"),
+      .label = STRVIEW("GBuffer - Albedo"),
       .usage
       = WGPUTextureUsage_RenderAttachment | WGPUTextureUsage_TextureBinding,
       .dimension     = WGPUTextureDimension_2D,
