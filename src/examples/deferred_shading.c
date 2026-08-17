2bce0804501261d6decea29209b790d2e8fbaefc
samdauwe <sam.dauwe@gmail.com>
2026-08-17 18:27:29 +0200

    Updated "Floor Bind Group" label.

--- src/examples/deferred_shading.c
+++ src/examples/deferred_shading.c
@@ -989,7 +989,7 @@ static void init_bind_groups(struct wgpu_context_t* wgpu_context)
 
     state.floor_bind_group = wgpuDeviceCreateBindGroup(
       device, &(WGPUBindGroupDescriptor){
-                .label      = STRVIEW("Floor Bind Group"),
+                .label      = STRVIEW("Floor - Bind Group"),
                 .layout     = state.offscreen_bgl,
                 .entryCount = 4,
                 .entries    = entries,
