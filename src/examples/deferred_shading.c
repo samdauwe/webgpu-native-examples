ec49eae7ee4d9ecd4435c59476938cfaf2387aaa
samdauwe <sam.dauwe@gmail.com>
2026-08-18 18:01:53 +0200

    Updated "Offscreen Pipeline Layout" label.

--- src/examples/deferred_shading.c
+++ src/examples/deferred_shading.c
@@ -1044,7 +1044,7 @@ static void init_pipeline_layouts(struct wgpu_context_t* wgpu_context)
   /* Offscreen pipeline layout */
   state.offscreen_pipeline_layout = wgpuDeviceCreatePipelineLayout(
     device, &(WGPUPipelineLayoutDescriptor){
-              .label                = STRVIEW("Offscreen Pipeline Layout"),
+              .label                = STRVIEW("Offscreen - Pipeline Layout"),
               .bindGroupLayoutCount = 1,
               .bindGroupLayouts     = &state.offscreen_bgl,
             });
